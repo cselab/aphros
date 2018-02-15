2aad447244f4551b2ece60e6ebe3bd87b1018dd2
Petr Karnakov <pkarnakov@gmail.com>
2018-02-15 02:06:03 +0100

    split suspender.h to cpp

---
+++ mpcf/code/hydro/suspender.cpp
@@ -0,0 +1,62 @@
+#include <cassert>
+
+#include "suspender.h"
+
+Suspender::Sem::Sem(Suspender& p, std::string name) 
+  : p(p), name_(name)
+{
+  auto& l = p.lu_;
+  auto& i = p.lui_;
+  if (std::next(i) == l.end()) {
+    l.emplace_back(0, 0);
+  }
+  ++i;
+  i->c = 0;
+}
+
+Suspender::Sem::~Sem() {
+  auto& l = p.lu_;
+  auto& i = p.lui_;
+
+  assert(!l.empty());
+  assert(i != l.end());
+  assert(i != l.begin());
+
+  auto ip = std::prev(i);
+
+  if (std::next(i) == l.end()) {
+    // all lower levels done, next stage
+    ++i->t;
+    if (i->c == i->t) { 
+      // all stages done, remove current level
+      // i->c keeps number of stages
+      l.pop_back();
+    }
+  } 
+  i = ip;
+}
+
+bool Suspender::Sem::operator()() {
+  auto& i = p.lui_;
+  return i->c++ == i->t;
+}
+
+Suspender::Suspender() 
+  : lu_(1, U(-1,-1)), lui_(lu_.begin()) 
+{}
+
+Suspender::Sem Suspender::GetSem(std::string name) {
+  return Sem(*this, name);
+}
+
+std::string Suspender::LeToStr() const {
+  std::stringstream b;
+  for (auto e : lu_) {
+    b << "(" << e.c << " " << e.t << ") ";
+  }
+  return b.str();
+}
+
+bool Suspender::Pending() const {
+  return lu_.size() != 1;
+}
--- mpcf/code/hydro/suspender.h
+++ mpcf/code/hydro/suspender.h
@@ -2,7 +2,6 @@
 
 #include <list>
 #include <string>
-#include <cassert>
 #include <sstream>
 
 // Suspendable functions.
@@ -21,17 +20,7 @@ class Suspender {
    public:
     // Constructor
     // Advance list iterator, add new counter if needed, reset counter
-    Sem(Suspender& p, std::string name="") 
-    : p(p), name_(name)
-    {
-      auto& l = p.lu_;
-      auto& i = p.lui_;
-      if (std::next(i) == l.end()) {
-        l.emplace_back(0, 0);
-      }
-      ++i;
-      i->c = 0;
-    }
+    Sem(Suspender& p, std::string name="");
     // Forbid copy
     Sem(Sem&) = delete;
     Sem& operator=(Sem&) = delete;
@@ -40,63 +29,24 @@ class Suspender {
     // Destructor
     // If all lower levels done, next stage.
     // If all stages on current level done, remove current level
-    ~Sem() {
-      auto& l = p.lu_;
-      auto& i = p.lui_;
-
-      assert(!l.empty());
-      assert(i != l.end());
-      assert(i != l.begin());
-
-      auto ip = std::prev(i);
-
-      if (std::next(i) == l.end()) {
-        // all lower levels done, next stage
-        ++i->t;
-        if (i->c == i->t) { 
-          // all stages done, remove current level
-          // i->c keeps number of stages
-          l.pop_back();
-        }
-      } 
-      i = ip;
-    }
+    ~Sem();
     // Returns true if current stage needs execution
     // and advances stage counter
-    bool operator()() {
-      auto& i = p.lui_;
-      return i->c++ == i->t;
-    }
-    std::string GetName() const {
-      return name_;
-    }
-    void SetName(std::string name) {
-      name_ = name;
-    }
+    bool operator()();
+    std::string GetName() const { return name_; }
+    void SetName(std::string name) { name_ = name; }
    private:
     Suspender& p; // parent
     std::string name_;
   };
   friend Sem;
   // Intializes list with auxiliary counter (-1,-1), sets iterator to it
-  Suspender() 
-    : lu_(1, U(-1,-1)), lui_(lu_.begin()) 
-  {}
-  Sem GetSem(std::string name="") {
-    return Sem(*this, name);
-  }
+  Suspender();
+  Sem GetSem(std::string name="");
   // Converts counter list to string
-  std::string LeToStr() const {
-    std::stringstream b;
-    for (auto e : lu_) {
-      b << "(" << e.c << " " << e.t << ") ";
-    }
-    return b.str();
-  }
+  std::string LeToStr() const;
   // Returns true if there are unfinished levels 
-  bool Pending() const {
-    return lu_.size() != 1;
-  }
+  bool Pending() const;
 
  private:
   using LU = std::list<U>;
