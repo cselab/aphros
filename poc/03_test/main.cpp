657d27c55def1519a77e3b232541c7d274f74224
Petr Karnakov <kpetr@ethz.ch>
2018-02-16 10:58:02 +0100

    add t1 t2

--- poc/03_test/main.cpp
+++ poc/03_test/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <cstdlib>
 
-int main() {
-  std::cout << "out" << std::endl;
-  std::cerr << "err" << std::endl;
+int main(int c, char** v) {
+  // parse arg
+  std::string a = v[1];
+  std::string b = v[2];
+  std::cout 
+      << "a=" << a << " "
+      << "b=" << b << " "
+      << std::endl;
+  std::cerr 
+      << "err a=" << a << " "
+      << "err b=" << b << " "
+      <<  std::endl;
+
+  if (a != "a") {
+    return 11;
+  }
+  if (b != "b") {
+    return 12;
+  }
+  return 0;
 }
