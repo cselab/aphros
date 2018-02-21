ada8a20de6a47b7787587014d0879fe5d37a8259
Petr Karnakov <pkarnakov@gmail.com>
2018-02-21 08:24:48 +0100

    add assert to test/mesh

--- test/mesh/main.cpp
+++ test/mesh/main.cpp
@@ -4,18 +4,30 @@
 
 #include "mesh.hpp"
 
+// Returns true if a < b (lex starting from end)
+template <class Vect>
+bool Cmp(Vect a, Vect b) {
+  int i = Vect::dim;
+  while (i--) {
+    if (a[i] != b[i]) {
+      return a[i] < b[i];
+    }
+  }
+  return false;
+}
 
 int main() {
   const int dim = 3;
 
   using MIdx = geom::MIdxGeneral<dim>;
   using IdxFace = geom::IdxFace;
+  using Dir = geom::Direction<dim>;
 
   const size_t hl = 1;
-  MIdx oi(0);
-  MIdx si(2);
-  MIdx oa= oi - MIdx(hl);
-  MIdx sa = si + MIdx(2 * hl);
+  MIdx oi(0); // origin inner
+  MIdx si(2); // size inner
+  MIdx oa= oi - MIdx(hl); // origin all
+  MIdx sa = si + MIdx(2 * hl); // size all
 
   geom::BlockFaces<dim> bi(oi, si);
   geom::BlockFaces<dim> ba(oa, sa);
@@ -23,10 +35,25 @@ int main() {
   geom::Range<IdxFace> ra(ba);
   geom::RangeInner<IdxFace, dim> ri(ba, bi);
 
+  const MIdx xp0 = oa - Dir(0);
+  MIdx xp = xp0;
+  Dir dp(0); // direction
+
+  // Check that whole inner block covered with ascending indices
   for (auto i : ri) {
-    std::cout 
-      << ba.GetMIdx(i) << " " 
-      << ba.GetDirection(i).GetLetter() << " "
-      << std::endl;
+    auto x = ba.GetMIdx(i);
+    auto d = ba.GetDirection(i);
+
+    // Next direction, reset xp
+    if (dp < d) {
+      xp = xp0;
+    }
+
+    // std::cerr << x << " " << d.GetLetter() << std::endl;
+    assert(Cmp(xp, x));
+    assert(oi <= x && x < oi + si + d);
+
+    xp = x;
+    dp = d;
   }
 }
