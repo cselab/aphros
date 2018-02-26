bad362b8d17f20d180843065ba9b9ae522b209de
Petr Karnakov <kpetr@ethz.ch>
2018-02-26 18:03:10 +0100

    add RunGrad to test/grad

--- test/grad/main.cpp
+++ test/grad/main.cpp
@@ -40,18 +40,6 @@ typename V::value_type Prod(const V& v) {
 }
 
 template <class Idx, class Mesh>
-typename Mesh::Scal Max(
-    const geom::GField<typename Mesh::Scal, Idx>& u,
-    const Mesh& m) {
-  using Scal = typename Mesh::Scal;
-  Scal r = 0;
-  for (auto i : m.template Get<Idx>()) {
-    r = std::max(r, u[i]);
-  }
-  return r;
-}
-
-template <class Idx, class Mesh>
 typename Mesh::Scal DiffMax(
     const geom::GField<typename Mesh::Scal, Idx>& u,
     const geom::GField<typename Mesh::Scal, Idx>& v,
@@ -63,6 +51,20 @@ typename Mesh::Scal DiffMax(
   }
   return r;
 }
+
+template <class Idx, class Mesh>
+typename Mesh::Scal DiffMax(
+    const geom::GField<typename Mesh::Vect, Idx>& u,
+    const geom::GField<typename Mesh::Vect, Idx>& v,
+    const Mesh& m) {
+  using Scal = typename Mesh::Scal;
+  Scal r = 0;
+  for (auto i : m.template Get<Idx>()) {
+    r = std::max(r, (u[i] - v[i]).norminf());
+  }
+  return r;
+}
+
 
 #define CMP(a, b) \
   assert(Cmp(a, b)); 
@@ -85,7 +87,7 @@ Mesh GetMesh(MIdx s /*size in cells*/) {
   return geom::InitUniformMesh<Mesh>(dom, b, s, hl);
 }
 
-Scal TestInterp0(std::function<Scal(Vect)> f, const Mesh& m) {
+Scal RunInterp(std::function<Scal(Vect)> f, const Mesh& m) {
   // Init field on all cells (including halos)
   geom::FieldCell<Scal> fc(m);
   for (auto i : m.AllCells()) {
@@ -96,21 +98,58 @@ Scal TestInterp0(std::function<Scal(Vect)> f, const Mesh& m) {
   // Empty bc
   geom::MapFace<std::shared_ptr<solver::ConditionFace>> mfc; 
 
-  // Inteatpolate to faces
+  // Interpolate to faces
   geom::FieldFace<Scal> ff = solver::Interpolate(fc, mfc, m);
 
   // Init reference on faces
   geom::FieldFace<Scal> ffr(m);  
-  for (auto i : m.AllFaces()) {
+  for (auto i : m.Faces()) {
     Vect x = m.GetCenter(i);
     ffr[i] = f(x);
   }
   return DiffMax(ff, ffr, m);
 }
 
+Scal RunGrad(std::function<Scal(Vect)> uf, 
+             std::function<Vect(Vect)> ugr,  // reference
+             const Mesh& m) {
+  // Init field on all cells (including halos)
+  geom::FieldCell<Scal> f(m);
+  for (auto i : m.AllCells()) {
+    Vect x = m.GetCenter(i);
+    f[i] = uf(x);
+  }
+
+  // Empty bc
+  geom::MapFace<std::shared_ptr<solver::ConditionFace>> bc; 
+
+  // Interpolate to faces
+  geom::FieldFace<Scal> ff = solver::Interpolate(f, bc, m);
+
+  // Gradient on cells
+  geom::FieldCell<Vect> g = solver::Gradient(ff, m);
+
+  // Init reference on cells
+  geom::FieldCell<Vect> gr(m);  
+  for (auto i : m.Cells()) {
+    Vect x = m.GetCenter(i);
+    gr[i] = ugr(x);
+  }
+  return DiffMax(g, gr, m);
+}
+
 void Single(std::function<Scal(Vect)> f) {
   auto m = GetMesh(MIdx(5, 4, 3));
-  auto e = TestInterp0(f, m);
+  auto e = RunInterp(f, m);
+  std::cerr << std::scientific
+      << "err: " << e
+      << std::endl;
+  CMP(e, 0.);
+}
+
+void SingleG(std::function<Scal(Vect)> f, std::function<Vect(Vect)> gr) {
+  auto m = GetMesh(MIdx(5, 4, 3));
+  auto e = RunGrad(f, gr, m);
   std::cerr << std::scientific
       << "err: " << e
       << std::endl;
@@ -118,15 +157,15 @@ void Single(std::function<Scal(Vect)> f) {
 }
 
 void Conv(std::function<Scal(Vect)> f) {
-  MIdx s(2, 3, 4); // initial mesh size
+  MIdx s(2, 3, 3); // initial mesh size
   Scal rh = 2; // refinement factor (for number of cells in one direction)
 
   Scal ep = -1;
   Scal ord = 0.;
   size_t n = 0;
-  while (Prod(s) < 1e5) {
+  while (s.prod() < 1e4) {
     auto m = GetMesh(s);
-    Scal e = TestInterp0(f, m); 
+    Scal e = RunInterp(f, m); 
     // e = C * h ^ ord
     // ep / e = rh ^ ord
     if (ep > 0) {
@@ -159,4 +198,19 @@ int main() {
   EE(Single([](Vect x) { return x[0]; }));
   EE(Single([](Vect x) { return x[0] * x[1] * x[2]; }));
   EE(Conv([](Vect x) { return std::sin(x[0] + sqr(x[1]) + cube(x[2])); }));
+
+  EE(SingleG([](Vect x) { return 1.; },
+             [](Vect x) { return Vect(0., 0., 0.); }));
+  EE(SingleG([](Vect x) { return x[0]; },
+             [](Vect x) { return Vect(1., 0., 0.); }));
+  EE(SingleG([](Vect x) { return sqr(x[0]); },
+             [](Vect x) { return Vect(2 * x[0], 0., 0.); }));
+  EE(SingleG([](Vect x) { return sqr(x[0]); },
+             [](Vect x) { return Vect(2 * x[0], 0., 0.); }));
+  EE(SingleG([](Vect x) { return sqr(x[0]) * sqr(x[1]) * sqr(x[2]); },
+             [](Vect x) { return Vect(
+                 2 * sqr(x[1]) * sqr(x[2]) * x[0], 
+                 2 * sqr(x[2]) * sqr(x[0]) * x[1], 
+                 2 * sqr(x[0]) * sqr(x[1]) * x[2]
+                 ); }));
 }
