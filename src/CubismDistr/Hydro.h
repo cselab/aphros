84b627325d2de9e3aee943ce517383f98d9fe628
Petr Karnakov <kpetr@ethz.ch>
2018-02-27 14:38:46 +0100

    move CreateMesh from Hydro class

--- src/CubismDistr/Hydro.h
+++ src/CubismDistr/Hydro.h
@@ -27,6 +27,33 @@
 
 
 template <class M>
+M CreateMesh(const MyBlockInfo& bi) {
+  using MIdx = typename M::MIdx;
+  using B = MyBlock;
+  using Vect = typename M::Vect;
+  using Rect = geom::Rect<Vect>;
+  B& b = *(B*)bi.ptrBlock;
+  int hl = bi.hl;
+  MIdx s(B::sx, B::sy, B::sz); // block size inner
+
+  Scal h = bi.h_gridpoint;
+  auto w = bi.index;   // block index
+  auto c = bi.origin; 
+  Vect d0(c[0], c[1], c[2]); // origin coord
+  Vect d1 = d0 + Vect(s) * h;      // end coord
+  Rect d(d0, d1);
+
+  MIdx o(w[0] * s[0], w[1] * s[1], w[2] * s[2]); // origin index
+  std::cout 
+      << "o=" << o 
+      << " dom=" << d0 << "," << d1 
+      << " h=" << h
+      << std::endl;
+  
+  return geom::InitUniformMesh<M>(d, o, s, hl);
+}
+
+template <class M>
 class Hydro : public Kernel {
  public:
   using Mesh = M;
@@ -51,8 +78,6 @@ class Hydro : public Kernel {
   M& GetMesh() { return m; }
 
  private:
-  M CreateMesh(const MyBlockInfo& bi);
-
   Vars& par;
   std::string name_;
   MyBlockInfo bi_;
@@ -92,32 +117,8 @@ void Grad(M& m) {
 
 
 template <class M>
-M Hydro<M>::CreateMesh(const MyBlockInfo& bi) {
-  using B = MyBlock;
-  B& b = *(B*)bi.ptrBlock;
-  int hl = bi.hl;
-  MIdx s(B::sx, B::sy, B::sz); // block size inner
-
-  Scal h = bi.h_gridpoint;
-  auto w = bi.index;   // block index
-  auto c = bi.origin; 
-  Vect d0(c[0], c[1], c[2]); // origin coord
-  Vect d1 = d0 + Vect(s) * h;      // end coord
-  Rect d(d0, d1);
-
-  MIdx o(w[0] * s[0], w[1] * s[1], w[2] * s[2]); // origin index
-  std::cout 
-      << "o=" << o 
-      << " dom=" << d0 << "," << d1 
-      << " h=" << h
-      << std::endl;
-  
-  return geom::InitUniformMesh<M>(d, o, s, hl);
-}
-
-template <class M>
 Hydro<M>::Hydro(Vars& par, const MyBlockInfo& bi) 
-  : par(par), bi_(bi), m(CreateMesh(bi))
+  : par(par), bi_(bi), m(CreateMesh<M>(bi))
 {
   name_ = 
       "[" + std::to_string(bi.index[0]) +
--- test/comm/main.cpp
+++ test/comm/main.cpp
@@ -8,38 +8,13 @@
 #include "CubismDistr/Kernel.h"
 #include "CubismDistr/Cubism.h"
 #include "CubismDistr/Local.h"
+#include "CubismDistr/Hydro.h"
 
 #include "hydro/suspender.h"
 #include "hydro/vect.hpp"
 #include "hydro/mesh3d.hpp"
 #include "hydro/solver.hpp"
 
-template <class M>
-M CreateMesh(const MyBlockInfo& bi) {
-  using MIdx = typename M::MIdx;
-  using B = MyBlock;
-  using Vect = typename M::Vect;
-  using Rect = geom::Rect<Vect>;
-  B& b = *(B*)bi.ptrBlock;
-  int hl = bi.hl;
-  MIdx s(B::sx, B::sy, B::sz); // block size inner
-
-  Scal h = bi.h_gridpoint;
-  auto w = bi.index;   // block index
-  auto c = bi.origin; 
-  Vect d0(c[0], c[1], c[2]); // origin coord
-  Vect d1 = d0 + Vect(s) * h;      // end coord
-  Rect d(d0, d1);
-
-  MIdx o(w[0] * s[0], w[1] * s[1], w[2] * s[2]); // origin index
-  std::cout 
-      << "o=" << o 
-      << " dom=" << d0 << "," << d1 
-      << " h=" << h
-      << std::endl;
-  
-  return geom::InitUniformMesh<M>(d, o, s, hl);
-}
 
 template <class M>
 class Simple : public Kernel {
