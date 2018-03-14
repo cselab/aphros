139137dedb2985d42cd219d0e5d5ea25b10dc5bd
Petr Karnakov <kpetr@ethz.ch>
2018-03-14 18:39:46 +0100

    Distr: add timer

--- src/hydro/metrics.hpp
+++ src/hydro/metrics.hpp
@@ -1,10 +1,3 @@
-/*
- * metrics.hpp
- *
- *  Created on: Mar 2, 2016
- *      Author: Petr Karnakov (petr.karnakov@gmail.com)
- */
-
 #pragma once
 
 #include <map>
@@ -16,42 +9,60 @@ class SingleTimer {
   Clock clock_;
   Clock::time_point start_;
  public:
-  SingleTimer() :
-    start_(clock_.now()) {}
+  SingleTimer() : start_(clock_.now()) {}
   double GetSeconds() const {
     return std::chrono::duration<double>(clock_.now() - start_).count();
   }
 };
 
-template <class Attr>
+template <class Key_>
 class MultiTimer {
-  using Clock =  std::chrono::steady_clock;
-  Clock clock_;
-  struct Timer {
-    const Attr attr_;
-    Clock::time_point start_;
-    Timer(const Attr& attr, Clock::time_point start)
-        : attr_(attr)
-        , start_(start)
-    {}
-  };
-  std::map<Attr, double> total_time_;
-  std::stack<Timer> stack_;
-
  public:
-  void Push(const Attr& attr) {
-    stack_.push(Timer(attr, clock_.now()));
-  }
-  void Pop() {
-    const Timer& timer = stack_.top();
-    if (!total_time_.count(timer.attr_)) {
-      total_time_[timer.attr_] = 0.;
-    }
-    total_time_[timer.attr_] +=
-        std::chrono::duration<double>(clock_.now() - timer.start_).count();
-    stack_.pop();
-  }
-  const std::map<Attr, double>& GetTotalTime() const {
-    return total_time_;
-  }
+  using Key = Key_;
+  using Value = double;
+
+  // Marks start of timer with given key
+  void Push(const Key& k);
+  void Push() { Push(""); }
+  // Marks stop of timer with last key.
+  // Overwrites the key if k != "".
+  // Appends to accumulated time for new key.
+  void Pop(const Key& k);
+  void Pop() { Pop(""); }
+  // Returns map of accumulated time
+  const std::map<Key, Value>& GetMap() const;
+
+ private:
+  using Clock = std::chrono::steady_clock;
+  Clock c_;
+  struct S { // start 
+    Key k;
+    Clock::time_point t;
+    S(const Key& k, Clock::time_point t) : k(k), t(t) {}
+  };
+  std::map<Key, Value> a_; // accumulated time
+  std::stack<S> ss_;        // stack of starts
 };
+
+template <class K>
+void MultiTimer<K>::Push(const Key& k) {
+  ss_.emplace(k, c_.now());
+}
+
+template <class K>
+void MultiTimer<K>::Pop(const Key& k) {
+  S& s = ss_.top();
+  if (k != "") {
+    s.k = k;   
+  }
+  if (!a_.count(s.k)) {
+    a_[s.k] = 0.;
+  }
+  a_[s.k] += std::chrono::duration<Value>(c_.now() - s.t).count();
+  ss_.pop();
+}
+
+template <class K>
+auto MultiTimer<K>::GetMap() const -> const std::map<Key, Value>&  {
+  return a_;
+}
