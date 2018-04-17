e0a74821a6704a02ab65c851fa7095719801aae8
Petr Karnakov <kpetr@ethz.ch>
2018-04-17 10:19:28 +0200

    test/reconst: plot grid

--- test/reconst/plot.py
+++ test/reconst/plot.py
@@ -6,9 +6,10 @@ matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import glob
 import os
+import re
 
 # Read uniform grid data
-# p -- path
+# p: path
 # Format:
 # <Nx> <Ny> <Nz>
 # <u[0,0,0]> <u[1,0,0]> ...
@@ -23,7 +24,7 @@ def Read(p):
         u = np.array(ll[1].split(), dtype=float)
         return u.reshape(s)
 
-# u -- numpy array (2d or 3d with shape[2]==1)
+# u: numpy array (2d or 3d with shape[2]==1)
 def Get2d(u):
     if len(u.shape) == 2:
         return u
@@ -32,25 +33,60 @@ def Get2d(u):
         assert len(s) == 3 and s[2] == 1
         return u.reshape((s[0], s[1]))
 
-# u -- 2d numpy array
-# po -- output path
-def Plot(u, po):
-    u = np.clip(u, 0., 1.)
+def PlotInit():
     fig, ax = plt.subplots(figsize=(5,5))
-    ax.imshow(np.flipud(u),
-               extent=(0, 1, 0, 1),
-               interpolation='nearest')
+    return fig, ax
+
+def PlotSave(fig, ax, po):
     ax.set_aspect('equal')
-    plt.tight_layout()
-    plt.savefig(po, dpi=300)
+    fig.tight_layout()
+    fig.savefig(po, dpi=300)
     plt.close()
 
+# u: 2d numpy array
+# po: output path
+def PlotField(ax, u):
+    u = np.clip(u, 0., 1.)
+    ax.imshow(np.flipud(u), extent=(0, 1, 0, 1), interpolation='nearest')
+
+# sx, sy: number of cells
+def PlotGrid(ax, x1, y1):
+    ax.set_xticks(x1)
+    ax.set_yticks(y1)
+    #ax.xaxis.set_ticklabels([])
+    #ax.yaxis.set_ticklabels([])
+    plt.setp(ax.get_xticklabels(), visible=False)
+    plt.setp(ax.get_yticklabels(), visible=False)
+    ax.tick_params(axis='both', which='both', length=0)
+    ax.grid(True)
+
+def PlotLines(ax, xa, xb, ya, yb):
+    ax.plot(xa, xb, ya, yb)
 
 
-pp = sorted(glob.glob("u*.dat"))
+pre = 'u'
+pp = sorted(glob.glob(pre + "*.dat"))
+pp = pp[1:2]
 for p in pp:
+    suf = re.findall(pre + "(.*)", p)[0]
     u = Read(p)
     u = Get2d(u)
+    [sx, sy] = u.shape
+    hx = 1. / sx
+    hy = 1. / sy
+    # nodes, 1 means 1d
+    xn1 = np.arange(sx + 1) * hx
+    yn1 = np.arange(sy + 1) * hy
+    # cell centers
+    x1 = (0.5 + np.arange(sx)) * hx
+    y1 = (0.5 + np.arange(sy)) * hy
+    x, y = np.meshgrid(x1, y1)
+    a = Read('a' + suf) # alpha
+
     po = os.path.splitext(p)[0] + ".pdf"
     print(po)
-    Plot(u, po)
+    fig, ax = PlotInit()
+    PlotGrid(ax, xn1, yn1)
+    PlotField(ax, u)
+    #PlotLines(ax, x, y, x, y)
+    PlotSave(fig, ax, po)
