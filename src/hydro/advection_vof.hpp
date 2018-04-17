ca0caf207507420eabc3e769b22555231bf16566
Petr Karnakov <kpetr@ethz.ch>
2018-04-17 20:04:00 +0200

    vof: add GetLineU

--- src/hydro/advection_vof.hpp
+++ src/hydro/advection_vof.hpp
@@ -22,12 +22,11 @@ inline void Clip(Scal& a) {
   Clip(a, 0., 1.);
 };
 
-// GetLineA() 
-// assuming 0 < u < 0.5, 0 < n[0] < n[1]
+// GetLineA() helper
+// assuming 0 < u < 0.5, 0 < nx < ny
 template <class Scal>
 inline Scal GetLineA0(Scal nx, Scal ny, Scal u) {
   Scal u1 = 0.5 * nx / ny;
-
   if (u <= u1) {
     return -0.5 * (nx + ny) + std::sqrt(2. * nx * ny * u);
   } else {
@@ -64,6 +63,22 @@ inline Scal GetLineA(const GVect<Scal, 3>& n, Scal u) {
   }
 }
 
+// GetLineU() helper
+// assuming a < 0, 0 < nx < ny
+template <class Scal>
+inline Scal GetLineU0(Scal nx, Scal ny, Scal a) {
+  Scal a1 = 0.5 * (nx - ny);
+  if (a <= a1) {
+    if (nx == 0.) { // TODO revise
+      return 0.;
+    } else {
+      return std::pow(a + 0.5 * (nx + ny), 2) / (2. * nx * ny);
+    }
+  } else {
+    return 0.5 + a / ny;
+  }
+}
+
 // Line constant to volume fraction
 // n : unit normal
 // a: line constant
@@ -71,8 +86,22 @@ inline Scal GetLineA(const GVect<Scal, 3>& n, Scal u) {
 // u: volume fraction
 // (see equation in GetLineA())
 template <class Scal>
-inline Scal GetLineU_Ge(const GVect<Scal, 3>& n, Scal a) {
+inline Scal GetLineU(const GVect<Scal, 3>& n, Scal a) {
   using Vect = GVect<Scal, 3>;
+
+  Scal nx = std::abs(n[0]);
+  Scal ny = std::abs(n[1]);
+  if (ny < nx) {
+    std::swap(nx, ny);
+  }
+  
+  Clip(a, -0.5 * (nx + ny), 0.5 * (nx + ny));
+
+  if (a < 0.) {
+    return GetLineU0(nx, ny, a);
+  } else {
+    return 1. - GetLineU0(nx, ny, -a);
+  }
 }
 
 template <class M>
@@ -129,7 +158,11 @@ class Vof : public AdvectionSolver<M> {
       }
 
       auto p = [&](Vect n, Scal u) {
-        std::cout << "n=" << n << " u=" << u << " a=" << GetLineA(n, u)
+        Scal a = GetLineA(n, u);
+        std::cout 
+            << "n=" << n << " u=" << u 
+            << " a=" << a
+            << " u(a)=" << GetLineU(n, a) 
             << std::endl;
       };
       p(Vect(1., 0., 0.), 0.);
@@ -144,7 +177,7 @@ class Vof : public AdvectionSolver<M> {
         fc_us_ = uc;
       }
       if (sem.Nested("smooth")) {
-        //Smoothen(fc_us_, mf_u_cond_, m, 1);
+        Smoothen(fc_us_, mf_u_cond_, m, 1);
       }
       if (sem("adv")) {
         auto us = fc_us_;
@@ -152,7 +185,7 @@ class Vof : public AdvectionSolver<M> {
         auto gc = Gradient(uf, m);
 
         // commit smooth
-        //uc = us;
+        uc = us;
 
         for (auto c : m.Cells()) {
           const Vect g = gc[c];
