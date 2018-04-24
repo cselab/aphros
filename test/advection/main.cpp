a84dcaa6cc43cc2d9332f33f92655bfb7a1bece7
Petr Karnakov <kpetr@ethz.ch>
2018-04-24 12:00:13 +0200

    test/advection: move DistrSolver to DistrSolver.h

--- test/advection/main.cpp
+++ test/advection/main.cpp
@@ -11,12 +11,8 @@
 
 #include "CubismDistr/Vars.h"
 #include "CubismDistr/Interp.h"
-#include "CubismDistr/Kernel.h"
-#include "CubismDistr/KernelMesh.h"
 #include "CubismDistr/KernelMeshPar.h"
-//#include "CubismDistr/ICubism.h"
-#include "CubismDistr/ILocal.h"
-#include "CubismDistr/Distr.h"
+#include "CubismDistr/DistrSolver.h"
 
 #include "hydro/suspender.h"
 #include "hydro/vect.hpp"
@@ -212,72 +208,19 @@ void Advection<M>::Run() {
   sem.LoopEnd();
 }
 
-template <class M_>
-class DistrSolver {
- public:
-  using M = M_;
-  static constexpr size_t dim = M::dim;
-  using Scal = typename M::Scal;
-  using Vect = typename M::Vect;
-
-  using K = Advection<M>;
-  using KF = KernelMeshParFactory<M, K>;
-  using D = DistrMesh<KernelMeshFactory<M>>; 
-  using Par = typename K::Par;
-
-  DistrSolver(MPI_Comm comm, Vars& var, Par& par)
-      : var(var), par_(par)
-  {
-    // Create kernel factory
-    KF kf(par_);
-
-    var.Double.Set("t", 0.);
-
-    // Initialize blocks
-    Distr* b; // base
-    if (var.Int["loc"]) {
-      b = CreateLocal(comm, kf, var);
-    } else {
-      //b = CreateCubism(comm, kf, var);
-    }
-
-    d_.reset(dynamic_cast<D*>(b));
-    if (!d_) {
-      throw std::runtime_error("DistrSolver: Can't cast to D");
-    }
-  }
-  void MakeStep() {
-    d_->Run();
-  }
-  GBlock<IdxCell, dim> GetBlock() const {
-    return d_->GetGlobalBlock();
-  }
-  FieldCell<Scal> GetField() const {
-    return d_->GetGlobalField(0);
-  }
-  double GetTime() const { 
-    return var.Double["t"]; 
-  }
-  double GetTimeStep() const { 
-    return var.Double["dt"]; 
-  }
-
- private:
-  Vars& var;
-  std::unique_ptr<D> d_;
-  Par& par_;
-};
 
 void Main(MPI_Comm comm, Vars& var) {
   using M = MeshStructured<double, 3>;
   using Scal = typename M::Scal;
   using Vect = typename M::Vect;
 
-  typename Advection<M>::Par par;
+  using K = Advection<M>;
+  using Par = typename K::Par;
+  Par par;
   par.fu0 = CreateInitU<Vect>(var);
   par.fvel = CreateInitVel<Vect>(var);
 
-  DistrSolver<M> ds(comm, var, par);
+  DistrSolver<M, K> ds(comm, var, par);
 
   size_t k = 0;
 
