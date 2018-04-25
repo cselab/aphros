f978deeeb917dfe47ac9de77c0490868b68df166
Petr Karnakov <pkarnakov@gmail.com>
2018-04-26 01:36:51 +0200

    test/reconst: add VolStr test

--- test/reconst/main.cpp
+++ test/reconst/main.cpp
@@ -12,35 +12,34 @@
 #include "geom/vect.h"
 #include "solver/advection_vof.h"
 
+using Scal = double;
+using Vect = GVect<Scal, 3>;
 
-void TestLine() {
-  using Scal = double;
-  using Vect = GVect<Scal, 3>;
+void TestRandom() {
+  std::cerr << "check u=u(a(u)) on random input" << std::endl;
+  std::default_random_engine g(0);
+  std::uniform_real_distribution<double> f(0.,1.);
+  size_t ni = 10000;
+  Scal e = 0.; // error
+  for (size_t i = 0; i < ni; ++i) {
+    while (true) {
+      Vect n(f(g), f(g), 0.);
+      n /= n.norm();
+      Scal u = f(g);
+      e += std::abs(solver::GetLineU1(n, solver::GetLineA1(n, u)) - u);
 
-  {
-    std::cerr << "check u=u(a(u)) on random input" << std::endl;
-    std::default_random_engine g(0);
-    std::uniform_real_distribution<double> f(0.,1.);
-    size_t ni = 10000;
-    Scal e = 0.; // error
-    for (size_t i = 0; i < ni; ++i) {
-      while (true) {
-        Vect n(f(g), f(g), 0.);
-        n /= n.norm();
-        Scal u = f(g);
-        e += std::abs(solver::GetLineU(n, solver::GetLineA(n, u)) - u);
-
-        Vect h(0.1, 0.2, 0.3);
-        e += std::abs(solver::GetLineU(n, solver::GetLineA(n, u, h), h) - u);
-        break;
-      }
+      Vect h(0.1, 0.2, 0.3);
+      e += std::abs(solver::GetLineU(n, solver::GetLineA(n, u, h), h) - u);
+      break;
     }
-    e /= ni;
-    std::cerr << "TestLine(): error=" << e << " samples=" << ni << std::endl;
-    assert(e < 1e-16);
   }
+  e /= ni;
+  std::cerr << "TestLine(): error=" << e << " samples=" << ni << std::endl;
+  assert(e < 1e-16);
+}
 
-  // rectangular cell test
+// rectangular cell test
+void TestRect() {
   auto p = [](Scal nx, Scal ny, Scal u, Scal hx, Scal hy) {
     Vect n(nx, ny, 0.);
     Vect h(hx, hy, 1.);
@@ -66,8 +65,10 @@ void TestLine() {
   p(1., 1., 0.25 , 1., 1.);
   p(1., 1., 0.25 , 0.1, 1.);
   p(0., 1., 0.5 , 1., 1.);
+}
 
-  // flux test
+// volume surplus test
+void TestVol() {
   // nx, ny: normal
   // u: volume fraction
   // dx: displacement
@@ -179,6 +180,76 @@ void TestLine() {
   hy = hy0;
 }
 
-int main () {
-  TestLine(); 
+// stretching volume surplus test 
+void TestVolStr() {
+  // nx, ny: normal
+  // u: volume fraction
+  // dx: displacement
+  // dxu: displacement upwind
+  // dve: volume surplus (exact)
+  auto f = [](Scal nx, Scal ny, Scal u, Scal hx, Scal hy, 
+              Scal dx, Scal dxu, Scal dve, bool xx=true) {
+    Vect n(nx, ny, 0.);
+    Vect h(hx, hy, 1.);
+    n /= n.norm();
+    Scal a = solver::GetLineA(n, u, h);
+    Scal dv;
+    if (xx) {
+      dv = solver::GetLineVolStrX(n, a, h, dx, dxu);
+    } else {
+      dv = solver::GetLineVolStrY(n, a, h, dx, dxu);
+    }
+    std::cerr 
+        << "n=" << n
+        << " u=" << u
+        << " h=" << h
+        << " dx=" << dx
+        << " dxu=" << dxu
+        << " a=" << a
+        << " dv=" << dv
+        << " dve=" << dve
+        << std::endl;
+    assert(std::abs(dv - dve) < 1e-12);
+  };
+
+  Scal nx, ny, u, hx, hy, dx, dxu;
+  Scal nx0, ny0, u0, hx0, hy0, dx0, dxu0;
+
+  std::cerr << "check volume surplus" << std::endl;
+  nx0 = 1.; ny0 = 0.; u0 = 0.5; hx0 = 1.; hy0 = 1.; dx0 = 0.; dxu0 = 0.;
+  nx = nx0; ny = ny0; u = u0; hx = hx0; hy = hy0; dx = dx0; dxu = dxu0;
+
+  std::cerr << "base" << std::endl;
+  f(nx, ny, u, hx, hy, dx=1., dxu=1., 0.5);
+  f(nx, ny, u, hx, hy, dx=-1., dxu=-1., 0.5);
+  f(nx, ny, u, hx, hy, dx=1., dxu=0., 0.);
+  f(nx, ny, u, hx, hy, dx=-1., dxu=0., 1.);
+  f(nx, ny, u, hx, hy, dx=0.5, dxu=0., 0.);
+  f(nx, ny, u, hx, hy, dx=-1.5, dxu=-1., 0.75);
+  f(nx, ny, u, hx, hy, dx=-0.5, dxu=0., 0.5);
+
+  std::cerr << "set n=(0,1,0)" << std::endl;
+  nx = 0.; ny = 1.;
+  f(nx, ny, u, hx, hy, dx=1., dxu=1., 0.5);
+  f(nx, ny, u, hx, hy, dx=-1., dxu=-1., 0.5);
+  f(nx, ny, u, hx, hy, dx=1., dxu=0., 0.5);
+  f(nx, ny, u, hx, hy, dx=-1., dxu=0., 0.5);
+  f(nx, ny, u, hx, hy, dx=0.5, dxu=0.5, 0.25);
+  f(nx, ny, u, hx, hy, dx=-0.5, dxu=-0.5, 0.25);
+  nx = nx0; ny = ny0;
+
+  std::cerr << "set n=(1,1,0)" << std::endl;
+  nx = 1.; ny = 1.;
+  f(nx, ny, u, hx, hy, dx=1., dxu=1., 0.5);
+  f(nx, ny, u, hx, hy, dx=-1., dxu=-1., 0.5);
+  f(nx, ny, u, hx, hy, dx=1., dxu=0., 0.25);
+  f(nx, ny, u, hx, hy, dx=-1., dxu=0., 0.75);
+  nx = nx0; ny = ny0;
+}
+
+int main() {
+  //TestRandom();
+  //TestRect();
+  //TestVol();
+  TestVolStr();
 }
