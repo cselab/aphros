ee3e21648f66cd7c60babc2084a4e5acbd280878
Petr Karnakov <pkarnakov@gmail.com>
2018-04-29 13:51:20 +0200

    convdiff: rename vars (wip)

--- src/solver/convdiff.h
+++ src/solver/convdiff.h
@@ -4,37 +4,39 @@
 
 namespace solver {
 
-template <class Mesh>
+// Solver for convection-diffusion equation
+// du/dt + div(v * u) = 1/r(d * grad^2 u + s)
+// where:
+// u: scalar field solved for
+// v: velocity field
+// d: diffusion rate
+template <class M_>
 class ConvectionDiffusionScalar : public UnsteadyIterativeSolver {
-  using Scal = typename Mesh::Scal;
-  static constexpr size_t dim = Mesh::dim;
+  using M = M_;
+  using Scal = typename M::Scal;
+  static constexpr size_t dim = M::dim;
   using Expr = Expression<Scal, IdxCell, 1 + dim * 2>;
 
  protected:
-  const FieldCell<Scal>* p_fc_scaling_;  // density
-  const FieldFace<Scal>* p_ff_diffusion_rate_;  // dynamic viscosity
-  const FieldCell<Scal>* p_fc_source_;
-  const FieldFace<Scal>* p_ff_vol_flux_;
+  const FieldCell<Scal>* fcr_;  // density
+  const FieldFace<Scal>* fcd_;  // diffusion 
+  const FieldCell<Scal>* fcs_;  // source
+  const FieldFace<Scal>* ffv_;  // volume flux
 
  public:
   ConvectionDiffusionScalar(
       double t, double dt,
-      const FieldCell<Scal>* p_fc_scaling,
-      const FieldFace<Scal>* p_ff_diffusion_rate,
-      const FieldCell<Scal>* p_fc_source,
-      const FieldFace<Scal>* p_ff_vol_flux)
+      const FieldCell<Scal>* fcr,
+      const FieldFace<Scal>* ffd,
+      const FieldCell<Scal>* fcs,
+      const FieldFace<Scal>* ffv)
       : UnsteadyIterativeSolver(t, dt)
-      , p_fc_scaling_(p_fc_scaling)
-      , p_ff_diffusion_rate_(p_ff_diffusion_rate)
-      , p_fc_source_(p_fc_source)
-      , p_ff_vol_flux_(p_ff_vol_flux)
-  {
-
-  }
+      , fcr_(fcr) , fcd_(fcd) , fcs_(fcs) , fcv_(fcv)
+  {}
   virtual const FieldCell<Scal>& GetField() = 0;
   virtual const FieldCell<Scal>& GetField(Layers layer) = 0;
   virtual void CorrectField(Layers layer,
-                            const FieldCell<Scal>& fc_corr) = 0;
+                            const FieldCell<Scal>& fcc /*correction*/) = 0;
   virtual const FieldCell<Expr>& GetEquations() = 0;
 };
 
