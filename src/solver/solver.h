92d998fb15b1e6d332c4e645be654271480ae52c
Petr Karnakov <kpetr@ethz.ch>
2018-05-15 19:38:34 +0200

    solver: move Condition to cond.h

--- src/solver/solver.h
+++ src/solver/solver.h
@@ -5,6 +5,7 @@
 
 #include "geom/mesh.h"
 #include "linear/linear.h"
+#include "cond.h"
 
 template <class Scal>
 bool IsNan(Scal a) {
@@ -22,102 +23,6 @@ bool IsNan(const GField<T, Idx>& field) {
 }
 
 namespace solver {
-
-class ConditionFace {
- public:
-  ConditionFace(size_t nci) : nci_(nci) {}
-  virtual ~ConditionFace() {}
-  // neighbour cell id
-  virtual size_t GetNci() const {
-    return nci_;
-  }
- private:
-  size_t nci_;
-};
-
-class ConditionFaceExtrapolation : public ConditionFace {
- public:
-  ConditionFaceExtrapolation(size_t nci) : ConditionFace(nci) {}
-};
-
-template <class Value>
-class ConditionFaceValue : public ConditionFace {
- public:
-  ConditionFaceValue(size_t nci) : ConditionFace(nci) {}
-  virtual Value GetValue() const = 0;
-};
-
-template <class Vect>
-class ConditionFaceValueExtractComponent :
-    public ConditionFaceValue<typename Vect::value_type> {
- public:
-  using Scal = typename Vect::value_type;
-  using P = ConditionFaceValue<Scal>; // parent
-  ConditionFaceValueExtractComponent(ConditionFaceValue<Vect>* o, size_t d)
-      : P(o->GetNci()), o_(o), d_(d) {}
-  Scal GetValue() const override { return o_->GetValue()[d_]; }
-
- private:
-  ConditionFaceValue<Vect>* o_;
-  size_t d_;
-};
-
-template <class V>
-class ConditionFaceValueFixed : public ConditionFaceValue<V> {
- public:
-  ConditionFaceValueFixed(const V& v, size_t nci)
-      : ConditionFaceValue<V>(nci), v_(v) {}
-  V GetValue() const override { return v_; }
-  void Set(const V& v) { v_ = v; }
-
- private:
-  V v_;
-};
-
-template <class Value>
-class ConditionFaceDerivative : public ConditionFace {
- public:
-  ConditionFaceDerivative(size_t nci) : ConditionFace(nci) {}
-  virtual Value GetDerivative() const = 0;
-};
-
-template <class Value>
-class ConditionFaceDerivativeFixed : public ConditionFaceDerivative<Value> {
-  Value derivative_;
- public:
-  explicit ConditionFaceDerivativeFixed(const Value& derivative, size_t nci)
-      : ConditionFaceDerivative<Value>(nci)
-      , derivative_(derivative)
-  {}
-  virtual Value GetDerivative() const override {
-    return derivative_;
-  }
-  void Set(const Value& v) {
-    derivative_ = v;
-  }
-};
-
-class ConditionCell {
- public:
-  virtual ~ConditionCell() {}
-};
-
-template <class Value>
-class ConditionCellValue : public ConditionCell {
- public:
-  virtual Value GetValue() const = 0;
-};
-
-template <class V>
-class ConditionCellValueFixed : public ConditionCellValue<V> {
- public:
-  explicit ConditionCellValueFixed(const V& v) : v_(v) {}
-  V GetValue() const override { return v_; }
-  void Set(const V& v) { v_ = v; }
-
- private:
-  V v_;
-};
 
 template <class Mesh, class Idx, class Expr>
 class Approximation {
