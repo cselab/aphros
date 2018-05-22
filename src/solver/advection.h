78a9d67d31f5b60a396ad1165152a5f10698e049
Petr Karnakov <kpetr@ethz.ch>
2018-05-22 16:12:15 +0200

    advection: add GetCurv(), revise tvd

--- src/solver/advection.h
+++ src/solver/advection.h
@@ -19,13 +19,12 @@ class AdvectionSolver : public UnsteadyIterativeSolver {
                   const FieldFace<Scal>* ffv /*volume flux*/,
                   const FieldCell<Scal>* fcs /*source*/)
       : UnsteadyIterativeSolver(t, dt)
-      , m(m)
-      , ffv_(ffv)
-      , fcs_(fcs) {}
+      , m(m), ffv_(ffv), fcs_(fcs) {}
   virtual const FieldCell<Scal>& GetField(Layers) = 0;
   virtual const FieldCell<Scal>& GetField() {
     return GetField(Layers::time_curr);
   }
+  virtual const FieldCell<Scal>& GetCurv() = 0;
 };
 
 
