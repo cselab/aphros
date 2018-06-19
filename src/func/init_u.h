0a555cf02f886bca92096f824d21bcbc6dab42ae
Petr Karnakov <kpetr@ethz.ch>
2018-06-19 12:09:27 +0200

    hydro: reimplement with init_u, init_u: add list

--- src/func/init_u.h
+++ src/func/init_u.h
@@ -3,6 +3,7 @@
 #include <stdexcept>
 #include <functional>
 #include <cmath>
+#include <limits>
 
 #include "parse/vars.h"
 #include "geom/field.h"
@@ -83,18 +84,85 @@ CreateInitU(Vars& par) {
     Scal r = par.Double["circle_r"];
     size_t dim = par.Int["dim"];
     g = [xc,r,dim](FieldCell<Scal>& fc, const M& m) { 
-      auto f = [xc,r,dim](const Vect& x) -> Scal {
-        Vect dx = x - xc;
+      // level-set for particle of radius 1 centered at zero,
+      // positive inside,
+      // cylinder along z if dim=2
+      auto f = [dim](const Vect& x) -> Scal {
+        Vect xd = x;
         if (dim == 2) {
-          dx[2] = 0.;
+          xd[2] = 0.;
         }
-        return sqr(r) - (x - xc).sqrnorm();
+        return 1. - xd.sqrnorm();
       };
       for (auto c : m.Cells()) {
         auto x = m.GetCenter(c);
         Vect h = m.GetNode(m.GetNeighbourNode(c, 7)) - 
             m.GetNode(m.GetNeighbourNode(c, 0));
-        fc[c] = GetLevelSetVolume<Scal>(f, x, h);
+        fc[c] = GetLevelSetVolume<Scal>(f, (x - xc) / r, h / r);
+      }
+    };
+  } else if (v == "list") {
+    std::string fn = par.String["list_path"];
+    bool ls = par.Int["list_ls"]; // level-set if 1, else stepwise
+    size_t dim = par.Int["dim"];
+
+    struct P { Vect c; Scal r; };
+    std::vector<P> pp;
+
+    std::ifstream f(fn);
+    if (!f.good()) {
+      throw std::runtime_error("Can't open particle list '" + fn + "'");
+    }
+
+    // Read until eof
+    while (true) {
+      P p;
+      // Read single particle: x y z r
+      f >> p.c[0] >> p.c[1] >> p.c[2] >> p.r;
+      if (f.good()) {
+        pp.push_back(p);
+      } else {
+        break;
+      }
+    }
+
+    std::cout << "Read " << pp.size() << " particles from " 
+        << "'" << fn << "'" << std::endl;
+
+    g = [dim,ls,pp](FieldCell<Scal>& fc, const M& m) { 
+      // level-set for particle of radius 1 centered at zero,
+      // positive inside,
+      // cylinder along z if dim=2
+      auto f = [dim](const Vect& x) -> Scal {
+        Vect xd = x;
+        if (dim == 2) {
+          xd[2] = 0.;
+        }
+        return 1. - xd.sqrnorm();
+      };
+      for (auto c : m.Cells()) {
+        auto x = m.GetCenter(c);
+        // find particle with largest value of level-set
+        Scal fm = -std::numeric_limits<Scal>::max(); 
+        size_t im; // index of particle
+        for (size_t i = 0; i < pp.size(); ++i) {
+          auto& p = pp[i];
+          Scal fi = f((x - p.c) / p.r);
+          if (fi > fm) {
+            fm = fi;
+            im = i;
+          }
+        }
+        // cell size
+        Vect h = m.GetNode(m.GetNeighbourNode(c, 7)) - 
+            m.GetNode(m.GetNeighbourNode(c, 0));
+        // volume fraction
+        auto& p = pp[im];
+        if (ls) {
+          fc[c] = GetLevelSetVolume<Scal>(f, (x - p.c) / p.r, h / p.r);
+        } else {
+          fc[c] = (fm >= 0. ? 1. : 0.);
+        }
       }
     };
   } else if (v == "box") {
@@ -108,11 +176,25 @@ CreateInitU(Vars& par) {
   } else if (v == "line") {
     Vect xc(par.Vect["line_c"]); // center
     Vect n(par.Vect["line_n"]);  // normal
-    Scal h(par.Double["line_h"]);  // thickness (sigma)
+    Scal h(par.Double["line_h"]);  // thickness 
     g = [xc,n,h](FieldCell<Scal>& fc, const M& m) { 
       for (auto c : m.Cells()) {
         Scal d = (m.GetCenter(c) - xc).dot(n);
         fc[c] = 1. / (1. + std::exp(-d / h));
+      }
+    };
+  } else if (v == "sin") {
+    Vect k;
+    if (auto p = par.Vect("sin_k")) {
+      k = Vect(*p);
+    } else {
+      k = Vect(2. * M_PI);
+    }
+
+    g = [k](FieldCell<Scal>& fc, const M& m) { 
+      for (auto c : m.Cells()) {
+        Vect z = m.GetCenter(c) * k;
+        fc[c] = std::sin(z[0]) * std::sin(z[1]) * std::sin(z[2]);
       }
     };
   } else if (v == "sinc") {
@@ -128,6 +210,12 @@ CreateInitU(Vars& par) {
         Scal u = std::sin(r) / r;
         u = (u - u0) / (u1 - u0);
         fc[c] = std::max(0., std::min(1., u));
+      }
+    };
+  } else if (v == "zero") {
+    g = [](FieldCell<Scal>& fc, const M& m) { 
+      for (auto c : m.Cells()) {
+        fc[c] = 0.;
       }
     };
   } else {
