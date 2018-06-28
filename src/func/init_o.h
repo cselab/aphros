ad96cb6715c9f132815041d7ff6c6b7d162d84f1
Petr Karnakov <kpetr@ethz.ch>
2018-06-28 20:59:51 +0200

    init_o: wip

--- src/func/init_o.h
+++ src/func/init_o.h
@@ -10,46 +10,7 @@
 #include "solver/vof.h"
 #include "geom/block.h"
 #include "geom/vect.h"
-
-// Volume fraction cut by interface defined by level-set function
-// f: level-set function, interface f=0, f>0 for volume fraction 1
-// xc: cell center
-// h: cell size
-template <class Scal, size_t dim=3>
-Scal GetLevelSetVolume(std::function<Scal(const GVect<Scal, 3>&)> f,
-                       const GVect<Scal, 3>& xc, const GVect<Scal, 3>& h) {
-  using Vect = GVect<Scal, dim>;
-  using MIdx = GVect<IntIdx, dim>;
-
-  const Scal dx = 1e-3; // step to compute gradient relative to h
-
-  GBlockCells<dim> b(MIdx(2));
-
-  Scal fc = f(xc);
-  for (auto w : b) {
-    Vect x = xc + (Vect(w) - Vect(0.5)) * h;
-    if ((fc > 0.) != (f(x) > 0.)) {
-      Vect dh = h * 1e-3;
-
-      // linear approximation
-      // f(x) = f(xc) + (x - xc).dot(grad(f, xc))
-      Vect n; // normal
-      for (size_t i = 0; i < dim; ++i) {
-        Vect xp(xc), xm(xc);
-        Scal dxh = dx * h[i];
-        xp[i] += dxh * 0.5;
-        xm[i] -= dxh * 0.5;
-        n[i] = (f(xp) - f(xm)) / dxh;
-      }
-      Scal a; // line constant
-      a = f(xc);
-
-      return solver::GetLineU(n, a, h);
-    }
-  }
-
-  return fc > 0. ? 1. : 0.;
-}
+#include "solver/tracker.h"
 
 // Volume fraction field.
 // par: parameters
@@ -96,7 +57,7 @@ CreateInitO(Vars& par, bool verb=true) {
           << "'" << fn << "'" << std::endl;
     }
 
-    g = [dim,ls,pp](FieldCell<Scal>& fc, const M& m) { 
+    g = [dim,pp](FieldCell<Scal>& fc, const M& m) { 
       if (pp.empty()) {
         fc.Reinit(m, 0.);
       } else {
@@ -104,22 +65,14 @@ CreateInitO(Vars& par, bool verb=true) {
           auto x = m.GetCenter(c);
           for (size_t i = 0; i < pp.size(); ++i) {
             auto& p = pp[i];
-            Scal fi = f((x - p.c) / p.r);
-            if (fi > fm) {
-              fm = fi;
-              im = i;
-            }
+            fc[c] = (fm >= 0. ? 1. : 0.);
           }
           // cell size
           Vect h = m.GetNode(m.GetNeighbourNode(c, 7)) - 
               m.GetNode(m.GetNeighbourNode(c, 0));
           // volume fraction
           auto& p = pp[im];
-          if (ls) {
-            fc[c] = GetLevelSetVolume<Scal>(f, (x - p.c) / p.r, h / p.r);
-          } else {
-            fc[c] = (fm >= 0. ? 1. : 0.);
-          }
+          fc[c] = (fm >= 0. ? 1. : 0.);
         }
       }
     };
@@ -131,45 +84,6 @@ CreateInitO(Vars& par, bool verb=true) {
         fc[c] = (xc - m.GetCenter(c)).norminf() < s * 0.5 ? 1. : 0.; 
       }
     };
-  } else if (v == "line") {
-    Vect xc(par.Vect["line_c"]); // center
-    Vect n(par.Vect["line_n"]);  // normal
-    Scal h(par.Double["line_h"]);  // thickness 
-    g = [xc,n,h](FieldCell<Scal>& fc, const M& m) { 
-      for (auto c : m.Cells()) {
-        Scal d = (m.GetCenter(c) - xc).dot(n);
-        fc[c] = 1. / (1. + std::exp(-d / h));
-      }
-    };
-  } else if (v == "sin") {
-    Vect k;
-    if (auto p = par.Vect("sin_k")) {
-      k = Vect(*p);
-    } else {
-      k = Vect(2. * M_PI);
-    }
-
-    g = [k](FieldCell<Scal>& fc, const M& m) { 
-      for (auto c : m.Cells()) {
-        Vect z = m.GetCenter(c) * k;
-        fc[c] = std::sin(z[0]) * std::sin(z[1]) * std::sin(z[2]);
-      }
-    };
-  } else if (v == "sinc") {
-    Vect k(par.Vect["sinc_k"]);
-    g = [k](FieldCell<Scal>& fc, const M& m) { 
-      for (auto c : m.Cells()) {
-        Vect x = m.GetCenter(c);
-        x -= Vect(0.5);
-        x *= k;
-        Scal r = x.norm();
-        Scal u0 = -0.2;
-        Scal u1 = 1.;
-        Scal u = std::sin(r) / r;
-        u = (u - u0) / (u1 - u0);
-        fc[c] = std::max(0., std::min(1., u));
-      }
-    };
   } else if (v == "zero") {
     g = [](FieldCell<Scal>& fc, const M& m) { 
       for (auto c : m.Cells()) {
@@ -177,7 +91,9 @@ CreateInitO(Vars& par, bool verb=true) {
       }
     };
   } else {
-    throw std::runtime_error("Unknown init_vf=" + v);
+    if (verb) {
+      std::cout << "Notify: no color function for init_vf=" << v << std::endl;
+    }
   }
   return g;
 }
