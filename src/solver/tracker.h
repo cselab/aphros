2b3543bbdd65b1a3cb9c728debe613077e4aba2a
Petr Karnakov <pkarnakov@gmail.com>
2018-06-28 08:31:07 +0200

    func: add init_o.h

--- src/solver/tracker.h
+++ src/solver/tracker.h
@@ -13,20 +13,20 @@ class Tracker {
   using Scal = typename M::Scal;
 
  public:
-  // fcr: initial color, [g]roup [a]
+  // fco: initial color, [g]roup [a]
   // th: threshold for detection u > 0
   // dim: space dimension, 2 or 3
-  Tracker(M& m, const FieldCell<Scal>& fcr, Scal th, size_t dim)
-      : m(m), fcr_(fcr), th_(th), dim_(dim) {}
+  Tracker(M& m, const FieldCell<Scal>& fco, Scal th, size_t dim)
+      : m(m), fco_(fco), th_(th), dim_(dim) {}
   // Propagates color.
   // fcu: volume fraction [a]
   void Update(const FieldCell<Scal>& fcu);
   // Returns color [a]
-  const FieldCell<Scal>& GetColor() const { return fcr_; }
+  const FieldCell<Scal>& GetColor() const { return fco_; }
 
  private:
   M& m;
-  FieldCell<Scal> fcr_; // colo[r] [a]
+  FieldCell<Scal> fco_; // colo[r] [a]
   size_t dim_;
   const Scal kNone = std::numeric_limits<Scal>::max(); // no color
 };
@@ -43,18 +43,18 @@ void Tracker<M_>::Update(const FieldCell<Scal>& fcu) {
                           MIdx(sn, sn, dim_ == 2 ? 1 : sn)); 
 
   for (auto c : m.Cells()) {
-    Scal& r = fcr_[c];
+    Scal& o = fco_[c];
     MIdx w = bc.GetMIdx(c);
     if (fcu[c] > th_) { // cell contains liquid
       // traverse neighbours, pick smallest color
       for (MIdx wo : bo) {
         IdxCell cn = bc.GetIdx(w + wo); // neighbour cell
         if (fcu[cn] > th) {
-          r = std::min(r, fcr_[cn]);
+          o = std::min(o, fco_[cn]);
         }
       }
     } else {  // no liquid in cell
-      r = kNone;
+      o = kNone;
     }
   }
 }
