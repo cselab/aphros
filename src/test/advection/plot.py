61a8b0379b624c26a465bce85bdfc44b4b914df0
Petr Karnakov <kpetr@ethz.ch>
2018-06-29 21:14:11 +0200

    test/advection: add curv_ge

--- src/test/advection/plot.py
+++ src/test/advection/plot.py
@@ -68,6 +68,12 @@ def PlotField(ax, u, vmin=None, vmax=None):
     ax.imshow(np.flipud(u), extent=(0, 1, 0, 1), interpolation='nearest',
               vmin=vmin, vmax=vmax)
 
+# u: 2d numpy array
+# po: output path
+def PlotFieldBwr(ax, u, vmin=None, vmax=None):
+    ax.imshow(np.flipud(u), extent=(0, 1, 0, 1), interpolation='nearest',
+              vmin=vmin, vmax=vmax, cmap=plt.get_cmap("bwr"))
+
 # sx, sy: number of cells
 def PlotGrid(ax, x1, y1):
     ax.set_xticks(x1)
@@ -145,7 +151,11 @@ def GetLines(xc, yc, a, nx, ny, hx, hy, u):
 
     return xa, ya, xb, yb
 
-def CmpCurv(x, y, u, k, kp, po):
+# kk: list of fields
+# ll: list of labels
+def CmpCurv(x, y, u, kk, ll, po):
+    global reff, hx
+
     cx = (x * u).sum() / u.sum()
     cy = (y * u).sum() / u.sum()
     th = 1e-3
@@ -154,26 +164,29 @@ def CmpCurv(x, y, u, k, kp, po):
     dx = x - cx; dy = y - cy
     an = np.arctan2(dy, dx)
     deg = np.degrees(an)
-    k = k[ii]
-    kp = kp[ii]
 
     s = np.argsort(an)
 
-    [sx, sy] = u.shape
-    hx = 1. / sx
-    hy = 1. / sy
-    r = ((u.sum() * hx * hy) / np.pi) ** 0.5
-    ke = 1. / r
+    ke = 1. / reff  # exact
+
 
     fig, ax = plt.subplots()
     ax.axhline(y=1., c='k')
-    ax.plot(deg[s], k[s] / ke, label="height")
-    ax.plot(deg[s], kp[s] / ke, label="particles")
+
+    for k,l in zip(kk,ll):
+        if k is not None:
+            print("plot l={:}, median={:}".format(l, np.max(k)))
+            ax.plot(deg[s], k[ii][s] / ke, label=l)
+
     ax.set_xlabel(r"angle [deg]")
     ax.set_ylabel(r"normalized curvature")
+    q=180.
+    ax.set_xticks(np.arange(-q, q*1.1, 90.))
+    ax.set_xlim(-q, q)
     ax.legend()
     ax.set_ylim(0., 2.)
     ax.grid()
+    plt.title("R/h={:.3f}".format(reff / hx))
     fig.tight_layout()
     fig.savefig(po, dpi=300)
     plt.close()
@@ -223,12 +236,27 @@ pre = 'u'
 pp = sorted(glob.glob(pre + "*.dat"))
 #pp = pp[1:2]
 for p in pp:
-    print(p)
     suf = re.findall(pre + "(.*)", p)[0]
+    print("p={:}, suf={:}".format(p, suf))
+
     u = Get2d(Read(p))
-    [sx, sy] = u.shape
+
+    [sx, sy] = u.shape # cells
+
+    # check if gerris (odd sx)
+    ge = (sx % 2 == 1)
+
+    if ge:
+        sx -= 1
+        sy -= 1
+
     hx = 1. / sx
     hy = 1. / sy
+
+    # equivalent radius
+    reff = ((u.sum() * hx * hy) / np.pi) ** 0.5
+    print("reff={:}".format(reff))
+
     # nodes, 1 means 1d
     xn1 = np.arange(sx + 1) * hx
     yn1 = np.arange(sy + 1) * hy
@@ -236,10 +264,16 @@ for p in pp:
     x1 = (0.5 + np.arange(sx)) * hx
     y1 = (0.5 + np.arange(sy)) * hy
     x, y = np.meshgrid(x1, y1)
+
     a = Get2d(Read('a' + suf)) # alpha
     nx = Get2d(Read('nx' + suf)) # normal
     ny = Get2d(Read('ny' + suf))
-    k = Get2d(Read('kh' + suf))
+    k = Get2d(Read('k' + suf))
+
+
+    # invert curvature if gerris
+    if ge:
+        k = -k
 
     # volume fraction u
     po = os.path.splitext(p)[0] + ".pdf"
@@ -253,21 +287,29 @@ for p in pp:
         l = GetLines(x, y, a, nx, ny, hx, hy, u)
         PlotLines(ax, *l)
     # plot partilces
-    n = re.findall("_(\d*)\.", suf)[0]
-    pa = "partit.{:}.csv".format(n)
-    if os.path.isfile(pa):
-        print(pa)
-        PlotPart(ax, pa, sk=5)
+    n = re.findall("_(\d*)\.", suf)
+    if n:
+        pa = "partit.{:}.csv".format(n[0])
+        if os.path.isfile(pa):
+            print(pa)
+            PlotPart(ax, pa, sk=5)
+    # save
     PlotSave(fig, ax, po)
 
     # curvature k
     if k is not None:
-        po = os.path.splitext('kh' + suf)[0] + ".pdf"
+        po = os.path.splitext('k' + suf)[0] + ".pdf"
         print(po)
         fig, ax = PlotInit()
         PlotGrid(ax, xn1, yn1)
-        vmax = 1. / 0.2 * 2.
-        PlotField(ax, k, vmin=-vmax, vmax=vmax)
+
+        # average curvature
+        ke = 1. / reff  # exact
+
+        # exclude points with zero curvature (for ch)
+        k[np.where(k == 0.)] = np.nan
+
+        PlotFieldBwr(ax, k / ke, vmin=0.8, vmax=1.2)
         # plot lines
         if all([e is not None for e in [a, nx, ny]]):
             l = GetLines(x, y, a, nx, ny, hx, hy, u)
@@ -276,8 +318,14 @@ for p in pp:
         PlotSave(fig, ax, po)
 
     # plot curvature comparison
+    kh = Get2d(Read('kh' + suf))
     kp = Get2d(Read('kp' + suf))
-    if k is not None and kp is not None:
+    kg = Get2d(Read('gerris_k/k0011.dat'))
+    if kg is not None:
+        kg = -kg
+    if kh is not None and kp is not None:
         po = os.path.splitext('kcmp' + suf)[0] + ".pdf"
-        CmpCurv(x, y, u, k, kp, po)
+        kk = [kh, kp, kg]
+        ll = ["height", "particles", "gerris"]
+        CmpCurv(x, y, u, kk, ll, po)
 
