042b2b5b9ee070d3478605a72866f13e2feb663a
Petr Karnakov <kpetr@ethz.ch>
2018-07-19 03:22:22 +0300

    sim/partstr: add paraview to curv3d

--- sim/partstr/curv3d/ch/s.py
+++ sim/partstr/curv3d/ch/s.py
@@ -7,6 +7,7 @@
 # trace generated using paraview version 5.5.1
 
 #### import the simple module from the paraview
+import sys
 from paraview.simple import *
 #### disable automatic camera reset on 'Show'
 paraview.simple._DisableFirstRenderCameraReset()
@@ -16,18 +17,23 @@ materialLibrary1 = GetMaterialLibrary()
 
 # Create a new 'Render View'
 renderView1 = CreateView('RenderView')
-renderView1.ViewSize = [1769, 1205]
+renderView1.ViewSize = [1998, 923]
 renderView1.AnnotationColor = [0.0, 0.0, 0.0]
 renderView1.AxesGrid = 'GridAxes3DActor'
+renderView1.OrientationAxesVisibility = 0
 renderView1.OrientationAxesLabelColor = [0.0, 0.0, 0.0]
 renderView1.OrientationAxesOutlineColor = [0.0, 0.0, 0.0]
-renderView1.CenterOfRotation = [0.5100770108401775, 0.47003214433789253, 0.559935986995697]
+renderView1.CenterOfRotation = [0.5099512603467459, 0.49048581092909566, 0.5599339630758458]
 renderView1.StereoType = 0
-renderView1.CameraPosition = [2.6175173876539772, 1.9956633671894988, 1.2075107981789321]
-renderView1.CameraFocalPoint = [0.510077010840177, 0.47003214433789225, 0.5599359869956971]
-renderView1.CameraViewUp = [0.1944234025884112, -0.5984618922823668, 0.7772019711836515]
-renderView1.CameraParallelScale = 0.6939154699620841
+renderView1.CameraPosition = [2.813868569960714, -0.5074590100179297, 1.4878129079851992]
+renderView1.CameraFocalPoint = [0.5099512603467459, 0.49048581092909566, 0.5599339630758458]
+renderView1.CameraViewUp = [-0.31739082620003495, 0.13938956480260697, 0.9379944630264078]
+renderView1.CameraParallelScale = 0.6927889287942449
 renderView1.Background = [1.0, 1.0, 1.0]
+renderView1.EnableOSPRay = 1
+renderView1.Shadows = 1
+renderView1.AmbientSamples = 5
+renderView1.SamplesPerPixel = 5
 renderView1.OSPRayMaterialLibrary = materialLibrary1
 
 # init the 'GridAxes3DActor' selected for 'AxesGrid'
@@ -55,82 +61,94 @@ SetActiveView(renderView1)
 # ----------------------------------------------------------------
 
 # create a new 'CSV Reader'
-partit0csv = CSVReader(FileName=['/home/kpetr/s/ch/sim/partstr/curv3d/ch/partit.0.csv'])
+partitfcsv = CSVReader(FileName=[sys.argv[2]])
 
 # create a new 'Table To Points'
-tableToPoints1 = TableToPoints(Input=partit0csv)
+tableToPoints1 = TableToPoints(Input=partitfcsv)
 tableToPoints1.XColumn = 'x'
 tableToPoints1.YColumn = 'y'
 tableToPoints1.ZColumn = 'z'
 
+# create a new 'Legacy VTK Reader'
+s_vtk = LegacyVTKReader(FileNames=[sys.argv[1]])
+
 # create a new 'Calculator'
 calculator1 = Calculator(Input=tableToPoints1)
 calculator1.ResultArrayName = 'cc'
-calculator1.Function = 'sin(1237*c)'
-
-# create a new 'Legacy VTK Reader'
-s0vtk = LegacyVTKReader(FileNames=['/home/kpetr/s/ch/sim/partstr/curv3d/ch/s.0.vtk'])
+calculator1.Function = 'sin(1234567*c)'
 
 # ----------------------------------------------------------------
 # setup the visualization in view 'renderView1'
 # ----------------------------------------------------------------
 
-# show data from s0vtk
-s0vtkDisplay = Show(s0vtk, renderView1)
+# show data from s_vtk
+s_vtkDisplay = Show(s_vtk, renderView1)
 
 # trace defaults for the display properties.
-s0vtkDisplay.Representation = 'Surface'
-s0vtkDisplay.AmbientColor = [0.0, 0.0, 0.0]
-s0vtkDisplay.ColorArrayName = [None, '']
-s0vtkDisplay.OSPRayScaleFunction = 'PiecewiseFunction'
-s0vtkDisplay.SelectOrientationVectors = 'None'
-s0vtkDisplay.ScaleFactor = 0.08013630285859108
-s0vtkDisplay.SelectScaleArray = 'None'
-s0vtkDisplay.GlyphType = 'Arrow'
-s0vtkDisplay.GlyphTableIndexArray = 'None'
-s0vtkDisplay.GaussianRadius = 0.004006815142929554
-s0vtkDisplay.SetScaleArray = [None, '']
-s0vtkDisplay.ScaleTransferFunction = 'PiecewiseFunction'
-s0vtkDisplay.OpacityArray = [None, '']
-s0vtkDisplay.OpacityTransferFunction = 'PiecewiseFunction'
-s0vtkDisplay.DataAxesGrid = 'GridAxesRepresentation'
-s0vtkDisplay.SelectionCellLabelFontFile = ''
-s0vtkDisplay.SelectionPointLabelFontFile = ''
-s0vtkDisplay.PolarAxes = 'PolarAxesRepresentation'
+s_vtkDisplay.Representation = 'Surface'
+s_vtkDisplay.AmbientColor = [0., 0., 0.]
+s_vtkDisplay.ColorArrayName = [None, '']
+s_vtkDisplay.OSPRayScaleFunction = 'PiecewiseFunction'
+s_vtkDisplay.SelectOrientationVectors = 'None'
+s_vtkDisplay.ScaleFactor = 0.0801363028585911
+s_vtkDisplay.SelectScaleArray = 'None'
+s_vtkDisplay.GlyphType = 'Arrow'
+s_vtkDisplay.GlyphTableIndexArray = 'None'
+s_vtkDisplay.GaussianRadius = 0.00400681514292955
+s_vtkDisplay.SetScaleArray = [None, '']
+s_vtkDisplay.ScaleTransferFunction = 'PiecewiseFunction'
+s_vtkDisplay.OpacityArray = [None, '']
+s_vtkDisplay.OpacityTransferFunction = 'PiecewiseFunction'
+s_vtkDisplay.DataAxesGrid = 'GridAxesRepresentation'
+s_vtkDisplay.SelectionCellLabelFontFile = ''
+s_vtkDisplay.SelectionPointLabelFontFile = ''
+s_vtkDisplay.PolarAxes = 'PolarAxesRepresentation'
+# change solid color
+s_vtkDisplay.DiffuseColor = [0.6, 0.6, 0.6]
+
+# change representation type
+s_vtkDisplay.SetRepresentationType('Surface With Edges')
+
+# Properties modified on s_0000vtkDisplay
+s_vtkDisplay.RenderLinesAsTubes = 1
+
+# Properties modified on s_0000vtkDisplay
+s_vtkDisplay.LineWidth = 2.0
+
+# Properties modified on s_0000vtkDisplay
+s_vtkDisplay.EdgeColor = [0.3, 0.3, 0.3]
 
 # init the 'GridAxesRepresentation' selected for 'DataAxesGrid'
-s0vtkDisplay.DataAxesGrid.XTitleColor = [0.0, 0.0, 0.0]
-s0vtkDisplay.DataAxesGrid.XTitleFontFile = ''
-s0vtkDisplay.DataAxesGrid.YTitleColor = [0.0, 0.0, 0.0]
-s0vtkDisplay.DataAxesGrid.YTitleFontFile = ''
-s0vtkDisplay.DataAxesGrid.ZTitleColor = [0.0, 0.0, 0.0]
-s0vtkDisplay.DataAxesGrid.ZTitleFontFile = ''
-s0vtkDisplay.DataAxesGrid.GridColor = [0.0, 0.0, 0.0]
-s0vtkDisplay.DataAxesGrid.XLabelColor = [0.0, 0.0, 0.0]
-s0vtkDisplay.DataAxesGrid.XLabelFontFile = ''
-s0vtkDisplay.DataAxesGrid.YLabelColor = [0.0, 0.0, 0.0]
-s0vtkDisplay.DataAxesGrid.YLabelFontFile = ''
-s0vtkDisplay.DataAxesGrid.ZLabelColor = [0.0, 0.0, 0.0]
-s0vtkDisplay.DataAxesGrid.ZLabelFontFile = ''
+s_vtkDisplay.DataAxesGrid.XTitleColor = [0.0, 0.0, 0.0]
+s_vtkDisplay.DataAxesGrid.XTitleFontFile = ''
+s_vtkDisplay.DataAxesGrid.YTitleColor = [0.0, 0.0, 0.0]
+s_vtkDisplay.DataAxesGrid.YTitleFontFile = ''
+s_vtkDisplay.DataAxesGrid.ZTitleColor = [0.0, 0.0, 0.0]
+s_vtkDisplay.DataAxesGrid.ZTitleFontFile = ''
+s_vtkDisplay.DataAxesGrid.GridColor = [0.0, 0.0, 0.0]
+s_vtkDisplay.DataAxesGrid.XLabelColor = [0.0, 0.0, 0.0]
+s_vtkDisplay.DataAxesGrid.XLabelFontFile = ''
+s_vtkDisplay.DataAxesGrid.YLabelColor = [0.0, 0.0, 0.0]
+s_vtkDisplay.DataAxesGrid.YLabelFontFile = ''
+s_vtkDisplay.DataAxesGrid.ZLabelColor = [0.0, 0.0, 0.0]
+s_vtkDisplay.DataAxesGrid.ZLabelFontFile = ''
 
 # init the 'PolarAxesRepresentation' selected for 'PolarAxes'
-s0vtkDisplay.PolarAxes.PolarAxisTitleColor = [0.0, 0.0, 0.0]
-s0vtkDisplay.PolarAxes.PolarAxisTitleFontFile = ''
-s0vtkDisplay.PolarAxes.PolarAxisLabelColor = [0.0, 0.0, 0.0]
-s0vtkDisplay.PolarAxes.PolarAxisLabelFontFile = ''
-s0vtkDisplay.PolarAxes.LastRadialAxisTextColor = [0.0, 0.0, 0.0]
-s0vtkDisplay.PolarAxes.LastRadialAxisTextFontFile = ''
-s0vtkDisplay.PolarAxes.SecondaryRadialAxesTextColor = [0.0, 0.0, 0.0]
-s0vtkDisplay.PolarAxes.SecondaryRadialAxesTextFontFile = ''
+s_vtkDisplay.PolarAxes.PolarAxisTitleColor = [0.0, 0.0, 0.0]
+s_vtkDisplay.PolarAxes.PolarAxisTitleFontFile = ''
+s_vtkDisplay.PolarAxes.PolarAxisLabelColor = [0.0, 0.0, 0.0]
+s_vtkDisplay.PolarAxes.PolarAxisLabelFontFile = ''
+s_vtkDisplay.PolarAxes.LastRadialAxisTextColor = [0.0, 0.0, 0.0]
+s_vtkDisplay.PolarAxes.LastRadialAxisTextFontFile = ''
+s_vtkDisplay.PolarAxes.SecondaryRadialAxesTextColor = [0.0, 0.0, 0.0]
+s_vtkDisplay.PolarAxes.SecondaryRadialAxesTextFontFile = ''
 
 # show data from calculator1
 calculator1Display = Show(calculator1, renderView1)
 
 # get color transfer function/color map for 'cc'
 ccLUT = GetColorTransferFunction('cc')
-ccLUT.AutomaticRescaleRangeMode = 'Never'
-ccLUT.EnableOpacityMapping = 1
-ccLUT.RGBPoints = [0.0, 0.0, 0.0, 1.0, 0.0166, 0.0, 0.0, 1.0, 0.016700000000000003, 1.0, 0.0, 1.0, 0.0332, 1.0, 0.0, 1.0, 0.0333, 0.0, 1.0, 1.0, 0.05, 0.0, 1.0, 1.0, 0.050100000000000006, 0.0, 1.0, 0.0, 0.0666, 0.0, 1.0, 0.0, 0.06670000000000001, 1.0, 1.0, 0.0, 0.0832, 1.0, 1.0, 0.0, 0.0833, 1.0, 0.0, 0.0, 0.1, 1.0, 0.0, 0.0]
+ccLUT.RGBPoints = [-0.9999950949114859, 0.0, 0.0, 1.0, -0.6679959104213643, 0.0, 0.0, 1.0, -0.6659959153340744, 1.0, 0.0, 1.0, -0.33599672593124275, 1.0, 0.0, 1.0, -0.33399673084395265, 0.0, 1.0, 1.0, 2.4487334587819376e-06, 0.0, 1.0, 1.0, 0.002002443820748767, 0.0, 1.0, 0.0, 0.33200163322358056, 0.0, 1.0, 0.0, 0.3340016283108701, 1.0, 1.0, 0.0, 0.6640008177137019, 1.0, 1.0, 0.0, 0.6660008128009919, 1.0, 0.0, 0.0, 0.9999999923784034, 1.0, 0.0, 0.0]
 ccLUT.ColorSpace = 'HSV'
 ccLUT.ScalarRangeInitialized = 1.0
 
@@ -139,16 +157,16 @@ calculator1Display.Representation = 'Surface'
 calculator1Display.AmbientColor = [0.0, 0.0, 0.0]
 calculator1Display.ColorArrayName = ['POINTS', 'cc']
 calculator1Display.LookupTable = ccLUT
-calculator1Display.PointSize = 8.0
+calculator1Display.PointSize = 5.0
 calculator1Display.RenderPointsAsSpheres = 1
 calculator1Display.OSPRayScaleArray = 'c'
 calculator1Display.OSPRayScaleFunction = 'PiecewiseFunction'
 calculator1Display.SelectOrientationVectors = 'None'
-calculator1Display.ScaleFactor = 0.08047477005217046
+calculator1Display.ScaleFactor = 0.0804747700521705
 calculator1Display.SelectScaleArray = 'None'
 calculator1Display.GlyphType = 'Arrow'
 calculator1Display.GlyphTableIndexArray = 'None'
-calculator1Display.GaussianRadius = 0.004023738502608522
+calculator1Display.GaussianRadius = 0.00402373850260852
 calculator1Display.SetScaleArray = ['POINTS', 'c']
 calculator1Display.ScaleTransferFunction = 'PiecewiseFunction'
 calculator1Display.OpacityArray = ['POINTS', 'c']
@@ -159,10 +177,10 @@ calculator1Display.SelectionPointLabelFontFile = ''
 calculator1Display.PolarAxes = 'PolarAxesRepresentation'
 
 # init the 'PiecewiseFunction' selected for 'ScaleTransferFunction'
-calculator1Display.ScaleTransferFunction.Points = [5010013.0, 0.0, 0.5, 0.0, 30019017.0, 1.0, 0.5, 0.0]
+calculator1Display.ScaleTransferFunction.Points = [10027032.0, 0.0, 0.5, 0.0, 61035034.0, 1.0, 0.5, 0.0]
 
 # init the 'PiecewiseFunction' selected for 'OpacityTransferFunction'
-calculator1Display.OpacityTransferFunction.Points = [5010013.0, 0.0, 0.5, 0.0, 30019017.0, 1.0, 0.5, 0.0]
+calculator1Display.OpacityTransferFunction.Points = [10027032.0, 0.0, 0.5, 0.0, 61035034.0, 1.0, 0.5, 0.0]
 
 # init the 'GridAxesRepresentation' selected for 'DataAxesGrid'
 calculator1Display.DataAxesGrid.XTitleColor = [0.0, 0.0, 0.0]
@@ -189,23 +207,6 @@ calculator1Display.PolarAxes.LastRadialAxisTextFontFile = ''
 calculator1Display.PolarAxes.SecondaryRadialAxesTextColor = [0.0, 0.0, 0.0]
 calculator1Display.PolarAxes.SecondaryRadialAxesTextFontFile = ''
 
-# setup the color legend parameters for each legend in this view
-
-# get color legend/bar for ccLUT in view renderView1
-ccLUTColorBar = GetScalarBar(ccLUT, renderView1)
-ccLUTColorBar.Title = 'cc'
-ccLUTColorBar.ComponentTitle = ''
-ccLUTColorBar.TitleColor = [0.0, 0.0, 0.0]
-ccLUTColorBar.TitleFontFile = ''
-ccLUTColorBar.LabelColor = [0.0, 0.0, 0.0]
-ccLUTColorBar.LabelFontFile = ''
-
-# set color bar visibility
-ccLUTColorBar.Visibility = 1
-
-# show color legend
-calculator1Display.SetScalarBarVisibility(renderView1, True)
-
 # ----------------------------------------------------------------
 # setup color maps and opacity mapes used in the visualization
 # note: the Get..() functions create a new object, if needed
@@ -213,10 +214,18 @@ calculator1Display.SetScalarBarVisibility(renderView1, True)
 
 # get opacity transfer function/opacity map for 'cc'
 ccPWF = GetOpacityTransferFunction('cc')
-ccPWF.Points = [0.0, 0.0, 0.5, 0.0, 0.012195121496915817, 0.0, 0.5, 0.0, 0.012195121496915817, 1.0, 0.5, 0.0, 0.08249641209840775, 1.0, 0.5, 0.0, 0.08249641209840775, 0.0, 0.5, 0.0, 0.1, 0.0, 0.5, 0.0]
+ccPWF.Points = [-0.9999950949114859, 0.0, 0.5, 0.0, -0.7560932640841367, 0.0, 0.5, 0.0, -0.7560932640841367, 1.0, 0.5, 0.0, 0.64992909424709, 1.0, 0.5, 0.0, 0.64992909424709, 0.0, 0.5, 0.0, 0.9999999923784034, 0.0, 0.5, 0.0]
 ccPWF.ScalarRangeInitialized = 1
 
 # ----------------------------------------------------------------
 # finally, restore active source
-SetActiveSource(partit0csv)
+#SetActiveSource(calculator1)
 # ----------------------------------------------------------------
+
+# reset view to fit data
+renderView1.ResetCamera()
+
+
+# save screenshot
+SaveScreenshot('s.png', renderView1, ImageResolution=[1000, 1000])
+
