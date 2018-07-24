e3f53d614447d1be6cf56c07a7c6af535c9bba0a
Petr Karnakov <kpetr@ethz.ch>
2018-07-24 09:05:19 +0300

    block: add BlockPad

--- src/geom/blockface.h
+++ src/geom/blockface.h
@@ -171,6 +171,94 @@ class GBlock<IdxFace, dim_> {
   }
 };
 
+// Specialization for IdxFace
+// Enumeration order (to more frequent): dir, z, y, x
+template <size_t dim_>
+class GBlockPad<IdxFace, dim_> {
+ public:
+  static constexpr size_t dim = dim_;
+  using Idx = IdxFace;
+  using MIdx = GMIdx<dim>;
+  using Dir = GDir<dim>;
+
+  // b: begin, lower corner
+  // rs: raw size (underlying block that fits all faces_)
+  GBlockPad(MIdx b, MIdx rs) 
+      : b_(b), rs_(rs), br_(b_, rs_)
+  {
+    // number of faces in each direction
+    nfa_ = 0;
+    for (size_t d = 0; d < dim; ++d) {
+      nf_[d] = CalcNumFaces(d);
+      nfa_ += nf_[d];
+    }
+    // cumulative number of faces
+    nfc_[0] = 0;
+    for (size_t d = 1; d < dim; ++d) {
+      nfc_[d] = nfc_[d - 1] + nf_[d - 1];
+    }
+  }
+  GBlockFacesPad(MIdx rs) : GBlockFacesPad(MIdx(0), rs) {}
+  GBlockFacesPad() : GBlockFacesPad(MIdx(0), MIdx(0)) {}
+  size_t size() const {
+    return nfa_;
+  }
+  operator GRange<Idx>() const {
+    return GRange<Idx>(0, size());
+  }
+  Idx GetIdx(MIdx w, Dir d) const {
+    size_t r = GetBase(size_t(d)) + br_.GetIdx(w).GetRaw();
+    return Idx(r);
+  }
+  Idx GetIdx(std::pair<MIdx, Dir> p) const {
+    return GetIdx(p.first, p.second);
+  }
+  Dir GetDir(Idx f) const {
+    size_t r = f.GetRaw();
+    size_t d = 0;
+    while (r >= GetNumFaces(d) && d < dim) {
+      r -= GetNumFaces(d);
+      ++d;
+    }
+    return Dir(d);
+  }
+  std::pair<MIdx, Dir> GetMIdxDir(Idx f) const {
+    size_t r = f.GetRaw();
+    size_t d = 0;
+    while (r >= GetNumFaces(d) && d < dim) {
+      r -= GetNumFaces(d);
+      ++d;
+    }
+    return {br_.GetMIdx(r), Dir(d)};
+  }
+  MIdx GetMIdx(Idx f) const {
+    size_t r = f.GetRaw();
+    size_t d = 0;
+    while (r >= GetNumFaces(d) && d < dim) {
+      r -= GetNumFaces(d);
+      ++d;
+    }
+    return br_.GetMIdx(r);
+  }
+
+ private:
+  const MIdx b_;
+  const MIdx rs_; // size of cells with padding
+  const GBlock<IdxCell, dim> br_; // block of cells with padding
+  MIdx nf_; // number of faces in each direction with padding
+  size_t nfa_; // total number of indices with padding
+  MIdx nfc_; // cumulative number of faces up to direction: sum(nf_[0:d])
+             // (base index for faces of direction d)
+  size_t GetNumFaces(size_t d) const {
+    return nf_[d];
+  }
+  size_t GetBase(size_t d) const {
+    return nfc_[d];
+  }
+  size_t CalcNumFaces(size_t d) const {
+    return bs_.size();
+  }
+};
+
 template <size_t dim>
 using GBlockFaces = GBlock<IdxFace, dim>;
-
