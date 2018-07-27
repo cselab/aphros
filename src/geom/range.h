d1d94467d8dfc7fd4f5b2e3c07388e7375970057
Petr Karnakov <kpetr@ethz.ch>
2018-07-27 11:40:17 +0300

    range: rename vars

--- src/geom/range.h
+++ src/geom/range.h
@@ -1,56 +1,49 @@
 #pragma once
 
-template <class _Idx>
+template <class Idx_>
 class GRange {
-  size_t pos_begin_, pos_end_;
-
  public:
-  using Idx = _Idx;
+  using Idx = Idx_;
+
   class iterator {
-    size_t pos_;
    public:
-    explicit iterator(size_t pos)
-      : pos_(pos)
-    {}
+    explicit iterator(size_t i) : i_(i) {}
     iterator() = default;
     iterator(const iterator&) = default;
     iterator& operator=(const iterator&) = default;
     iterator& operator++() {
-      ++pos_;
+      ++i_;
       return *this;
     }
-    iterator& operator--() {
-      --pos_;
-      return *this;
+    bool operator==(const iterator& o) const {
+      return i_ == o.i_;
     }
-    bool operator==(const iterator& other) const {
-      return pos_ == other.pos_;
-    }
-    bool operator!=(const iterator& other) const {
-      return !(*this == other);
+    bool operator!=(const iterator& o) const {
+      return !(*this == o);
     }
     Idx operator*() const {
-      return Idx(pos_);
+      return Idx(i_);
     }
+
+   private:
+    size_t i_;
   };
 
-  GRange()
-    : pos_begin_(0)
-    , pos_end_(0)
-  {}
-  GRange(size_t pos_begin, size_t pos_end)
-    : pos_begin_(pos_begin)
-    , pos_end_(pos_end)
-  {}
+  GRange() : b_(0) , e_(0) {}
+  GRange(size_t b, size_t e) : b_(b) , e_(e) {}
+
   iterator begin() const {
-    return iterator(pos_begin_);
+    return iterator(b_);
   }
   iterator end() const {
-    return iterator(pos_end_);
+    return iterator(e_);
   }
   size_t size() const {
-    return pos_end_ - pos_begin_;
+    return e_ - b_;
   }
+
+ private:
+  size_t b_, e_;
 };
 
 
