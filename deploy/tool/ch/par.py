4d7df809357c0e4577c98ff847caf36488f64abf
Petr Karnakov <pkarnakov@gmail.com>
2018-09-15 01:06:21 +0200

    sim04: add poiseuille velocity, revise with ch.par

--- deploy/tool/ch/par.py
+++ deploy/tool/ch/par.py
@@ -9,30 +9,44 @@ import os
 # v: default value or type [int, float, list] (then default is None)
 # h: description for help
 def StdParAll():
-    Re = (float, "Reynolds number")
-    Ca = (float, "capillary number")
-    We = (float, "Weber number")
-    La = (float, "Laplace number")
-    Ga = (float, "Galilei number")
-    np = (1, "number of processors")
-    nx = (64, "mesh size")
-    cpr = (4., "cells per bubble radius")
-    tmax = (1., "total time")
-    nfr = (100, "number of frames")
-    mu0 = (1., "dynamic viscosity of carrier fluid")
-    mu = (1., "dynamic viscosity of bubble relative to carrier")
-    rho0 = (1., "density of carrier fluid")
-    rho = (1., "density of bubble relative to carrier")
-    dim = (3, "dimension, 2 or 3")
-    chsm = (1, "ch smooth steps (rhor>10: 1, rhor>100: 2)")
-    gesm = (1, "ge smooth steps")
-    wallx = (0, "wall in x direction, else periodic")
-    wally = (0, "wall in y direction, else periodic")
-    wallz = (0, "wall in z direction, else periodic")
-    vel0 = ([4., 3., 2.], "direction of velocity")
-    bcoh = ([0.2, 0.4, 0.6], "offset of bubble center relative to h")
-    bcor = ([0., 0., 0.], "offset of bubble center relative to br")
-    bryk = (1. , "stretching factor for bubble size in y")
+    Re = [float, "Reynolds number"]
+    Red = [float, "Reynolds number by bubble diameter"]
+    Ca = [float, "capillary number"]
+    We = [float, "Weber number"]
+    La = [float, "Laplace number"]
+    Ga = [float, "Galilei number"]
+    np = [1, "number of processors"]
+    nx = [64, "mesh size"]
+    cpr = [4., "cells per bubble radius"]
+    br = [0.125, "bubble radius relative to dom"]
+    tmax = [1., "total time"]
+    nfr = [100, "number of frames"]
+    mu0 = [1., "dynamic viscosity of carrier fluid"]
+    mu = [1., "dynamic viscosity of bubble relative to carrier"]
+    rho0 = [1., "density of carrier fluid"]
+    rho = [1., "density of bubble relative to carrier"]
+    dim = [3, "dimension, 2 or 3"]
+    chsm = [1, "ch smooth steps [rhor>10: 1, rhor>100: 2]"]
+    gesm = [1, "ge smooth steps"]
+    wallx = [0, "wall in x direction, else periodic"]
+    wally = [0, "wall in y direction, else periodic"]
+    wallz = [0, "wall in z direction, else periodic"]
+    vel0 = [[4., 3., 2.], "direction of velocity"]
+    bcoh = [[0.2, 0.4, 0.6], "offset of bubble center relative to h"]
+    bcod = [[0., 0., 0.], "offset of bubble center relative to dom"]
+    bcor = [[0., 0., 0.], "offset of bubble center relative to br"]
+    bryk = [1. , "stretching factor for bubble size in y"]
+    dom = [1.    , "domain size [ignored]"]
+    ch = [1, "run ch", [0, 1]]
+    ge = [1, "run ge", [0, 1]]
+    pos = ["corner", "initial position of bubbles", ["center", "corner"]]
+    mode = ["vel", '''timescale; vel: velocity magnitude D;
+osc: oscillation frequency sqrt[7] / pi;
+mu: viscosity mu = rho * D ^ 2, pois: velocity magnitude dom''',
+           ['vel', 'osc', 'mu', 'pois']]
+    pois = [0, "Poiseuille profile by force along x", [0, 1]]
+    b2rr = [0. , "second bubble radius relative to br"]
+    b2xr = [[2., 0., 0.] , "second bubble displacement realative to br"]
     return locals().copy()
 
 # ordering:
@@ -72,14 +86,17 @@ def IsClose(a, b):
     return abs(a - b) < 1e-10
 
 # Returns Namespace with parameters from c overriden by args
-# c: dict with parameters as in StdParAll()
+# cc: dict with parameters as in StdParAll()
 # desc: description for command line help
-def GetArgs(c, desc=None):
+def GetArgs(cc, desc=None):
     p = argparse.ArgumentParser(description=desc,
             formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    for k in c:
-        v,h = c[k]
+    for k in cc:
+        c = cc[k]
+        v = c[0]  # default value
+        h = c[1]  # help message
+        o = c[2] if len(c) > 2 else None  # choices
         if v in [int, float, list]:
             t = v
             v = None
@@ -93,9 +110,10 @@ def GetArgs(c, desc=None):
             m = ('X','Y','Z')
             assert v is None or len(v) == len(m)
             t = type(v[0]) if v is not None else float
-            p.add_argument(n, nargs=len(m), metavar=m, default=v, help=h, type=t)
+            p.add_argument(n, nargs=len(m), choices=o, metavar=m,
+                    default=v, help=h, type=t)
         else:
-            p.add_argument(n, nargs='?', default=v, help=h, type=t)
+            p.add_argument(n, nargs='?', choices=o, default=v, help=h, type=t)
 
     a = p.parse_args()
     return a
