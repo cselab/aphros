585e1582603a5054da41ec022938973eee0e827e
Petr Karnakov <pkarnakov@gmail.com>
2018-10-23 22:58:23 +0200

    sim04: add case/jump/vis

---
+++ sim/sim04_univel/case/jump/vis/cont.py
@@ -0,0 +1,233 @@
+#!/usr/bin/env pvbatch
+
+from paraview.simple import *
+
+from glob import glob
+import sys
+import re
+import math
+import os
+import numpy as np
+
+def Log(s):
+    s += "\n"
+    o = sys.stderr
+    o.write(s)
+    o.flush()
+
+# natural sort
+def natkey(s, _nsre=re.compile('([0-9]+)')):
+    return [int(text) if text.isdigit() else text.lower()
+            for text in re.split(_nsre, s)]
+
+def natsorted(v):
+    return sorted(v, key=natkey)
+# Returns sorted list of files in base by pattern pre_*.xmf
+
+# Sets time of datasets to step i
+def SetTime(i):
+    global vft, vt
+    for j in range(len(vft)):
+        s = vft[j]
+        s.ForcedTime = vt[j][i]
+        s.UpdatePipeline()
+
+# Returns bounding box of object o
+def GetBox(o):
+    o.UpdatePipeline()
+    di = o.GetDataInformation()
+    lim = di.DataInformation.GetBounds()
+    lim0 = np.array(lim[::2])
+    lim1 = np.array(lim[1::2])
+    return lim0, lim1
+
+
+ospray = 1
+vort = 1
+
+av = sys.argv
+if len(av) < 2 or av[1] == '-h':
+    sys.stderr.write('''usage: {:} [s_*.vtk]
+Plots bubbles.
+# Output:
+# a_*.png in current folder
+'''.format(av[0]))
+    exit(1)
+
+# vf input
+ff = natsorted(av[1:])
+# vf basename
+ffb = list(map(os.path.basename, ff))
+# vf dirname
+ffd = list(map(os.path.dirname, ff))
+# steps
+ss = [int(re.findall("_([0-9]*)", fb)[0]) for fb in ffb]
+
+# output pattern (:0 substituted by frame number)
+bo = "a_{:}.png"
+
+#####################################################
+### BEGIN OF STATE FILE
+#####################################################
+
+#### disable automatic camera reset on 'Show'
+paraview.simple._DisableFirstRenderCameraReset()
+
+# ----------------------------------------------------------------
+# RENDER
+# ----------------------------------------------------------------
+
+# get the material library
+materialLibrary1 = GetMaterialLibrary()
+
+# Create a new 'Render View'
+renderView1 = CreateView('RenderView')
+renderView1.ViewSize = [500, 500]
+renderView1.AnnotationColor = [0.0, 0.0, 0.0]
+renderView1.AxesGrid = 'GridAxes3DActor'
+renderView1.OrientationAxesVisibility = 0
+renderView1.OrientationAxesLabelColor = [0.0, 0.0, 0.0]
+renderView1.OrientationAxesOutlineColor = [0.0, 0.0, 0.0]
+renderView1.CenterOfRotation = [0.5, 0.5, 0.5]
+renderView1.StereoType = 0
+renderView1.CameraPosition = [0.5, 0.5, 2.0]
+renderView1.CameraFocalPoint = [0.5, 0.5, 0.5]
+renderView1.CameraViewUp = [-4.440892098500626e-16, -1.0, 0.0]
+renderView1.CameraParallelScale = 0.49938997351313513
+renderView1.CameraParallelProjection = 1
+renderView1.Background = [1.0, 1.0, 1.0]
+renderView1.OSPRayMaterialLibrary = materialLibrary1
+
+# init the 'GridAxes3DActor' selected for 'AxesGrid'
+renderView1.AxesGrid.XTitleFontFile = ''
+renderView1.AxesGrid.YTitleFontFile = ''
+renderView1.AxesGrid.ZTitleFontFile = ''
+renderView1.AxesGrid.XLabelFontFile = ''
+renderView1.AxesGrid.YLabelFontFile = ''
+renderView1.AxesGrid.ZLabelFontFile = ''
+
+# ----------------------------------------------------------------
+# restore active view
+SetActiveView(renderView1)
+# ----------------------------------------------------------------
+
+
+# ----------------------------------------------------------------
+# END RENDER
+# ----------------------------------------------------------------
+
+# ----------------------------------------------------------------
+# BEGIN READERS
+# ----------------------------------------------------------------
+
+# create a new 'XDMF Reader'
+svtk = LegacyVTKReader(FileNames=ff)
+
+
+# list of all sources
+vs = [svtk]
+
+# time steps
+vt = [np.array(s.TimestepValues) for s in vs]
+
+# replace with ForceTime
+svtk = ForceTime(svtk)
+
+# all ForceTime
+vft = [svtk]
+
+# ----------------------------------------------------------------
+# END READERS
+# ----------------------------------------------------------------
+
+# create a new 'Clip'
+clip1 = Clip(Input=svtk)
+clip1.ClipType = 'Box'
+clip1.Scalars = ['CELLS', 'c']
+clip1.Value = 32009550.0
+
+# init the 'Box' selected for 'ClipType'
+clip1.ClipType.Bounds = [0.10114700347185135, 0.8403609991073608, 0.0, 0.5078120231628418, 0.0, 0.5078120231628418]
+clip1.ClipType.Scale = [1.0, 0.9, 0.9]
+
+# create a new 'Box'
+box1 = Box()
+box1.Center = [0.5, 0.5, 0.5]
+
+# ----------------------------------------------------------------
+# setup the visualization in view 'renderView1'
+# ----------------------------------------------------------------
+
+# show data from clip1
+clip1Display = Show(clip1, renderView1)
+
+# trace defaults for the display properties.
+clip1Display.Representation = 'Surface'
+clip1Display.AmbientColor = [0.0, 0.0, 0.0]
+clip1Display.ColorArrayName = ['POINTS', '']
+clip1Display.DiffuseColor = [1.0, 0.0, 0.0]
+clip1Display.Opacity = 0.25
+clip1Display.OSPRayScaleFunction = 'PiecewiseFunction'
+clip1Display.SelectOrientationVectors = 'None'
+clip1Display.ScaleFactor = 0.034518309682607655
+clip1Display.SelectScaleArray = 'c'
+clip1Display.GlyphType = 'Arrow'
+clip1Display.GlyphTableIndexArray = 'c'
+clip1Display.GaussianRadius = 0.0017259154841303827
+clip1Display.SetScaleArray = [None, '']
+clip1Display.ScaleTransferFunction = 'PiecewiseFunction'
+clip1Display.OpacityArray = [None, '']
+clip1Display.OpacityTransferFunction = 'PiecewiseFunction'
+clip1Display.DataAxesGrid = 'GridAxesRepresentation'
+clip1Display.SelectionCellLabelFontFile = ''
+clip1Display.SelectionPointLabelFontFile = ''
+clip1Display.PolarAxes = 'PolarAxesRepresentation'
+clip1Display.ScalarOpacityUnitDistance = 0.02122408020961942
+
+# init the 'GridAxesRepresentation' selected for 'DataAxesGrid'
+clip1Display.DataAxesGrid.XTitleColor = [0.0, 0.0, 0.0]
+clip1Display.DataAxesGrid.XTitleFontFile = ''
+clip1Display.DataAxesGrid.YTitleColor = [0.0, 0.0, 0.0]
+clip1Display.DataAxesGrid.YTitleFontFile = ''
+clip1Display.DataAxesGrid.ZTitleColor = [0.0, 0.0, 0.0]
+clip1Display.DataAxesGrid.ZTitleFontFile = ''
+clip1Display.DataAxesGrid.GridColor = [0.0, 0.0, 0.0]
+clip1Display.DataAxesGrid.XLabelColor = [0.0, 0.0, 0.0]
+clip1Display.DataAxesGrid.XLabelFontFile = ''
+clip1Display.DataAxesGrid.YLabelColor = [0.0, 0.0, 0.0]
+clip1Display.DataAxesGrid.YLabelFontFile = ''
+clip1Display.DataAxesGrid.ZLabelColor = [0.0, 0.0, 0.0]
+clip1Display.DataAxesGrid.ZLabelFontFile = ''
+
+# init the 'PolarAxesRepresentation' selected for 'PolarAxes'
+clip1Display.PolarAxes.PolarAxisTitleColor = [0.0, 0.0, 0.0]
+clip1Display.PolarAxes.PolarAxisTitleFontFile = ''
+clip1Display.PolarAxes.PolarAxisLabelColor = [0.0, 0.0, 0.0]
+clip1Display.PolarAxes.PolarAxisLabelFontFile = ''
+clip1Display.PolarAxes.LastRadialAxisTextColor = [0.0, 0.0, 0.0]
+clip1Display.PolarAxes.LastRadialAxisTextFontFile = ''
+clip1Display.PolarAxes.SecondaryRadialAxesTextColor = [0.0, 0.0, 0.0]
+clip1Display.PolarAxes.SecondaryRadialAxesTextFontFile = ''
+
+# ----------------------------------------------------------------
+# finally, restore active source
+SetActiveSource(svtk)
+# ----------------------------------------------------------------
+
+#####################################################
+### END OF STATE FILE
+#####################################################
+
+for i in list(range(len(ss))):
+    fn = bo.format("{:04d}".format(ss[i]))
+    if os.path.isfile(fn):
+        Log("skip existing {:}".format(fn))
+        continue
+
+    SetTime(i)
+
+    Log("{:}/{:}: {:}".format(i + 1, len(ss), fn))
+    SaveScreenshot(fn, renderView1, TransparentBackground=1,
+            ImageResolution=[1000,1000])
+
+exit(0)
