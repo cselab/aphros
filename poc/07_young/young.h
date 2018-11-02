0f1912b00194f4e577f16a3bf34d49a4321a333b
Sergey Litvinov <slitvinov@gmail.com>
2018-11-02 22:38:43 +0100

    initial import of young

---
+++ poc/07_young/young.h
@@ -0,0 +1,9 @@
+typedef struct YoungParam YoungParam;
+struct YoungParam {
+    double rhov, rhou, muv, muu, hv, hu, gamma1, gamma0, T1, T0;
+    double R;
+};
+
+int young_set(YoungParam*);
+int young_ini(YoungParam);
+int young_fields(double, double, /**/ double *vy, double *vz, double *p, double *T);
