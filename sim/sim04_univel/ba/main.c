f8c049bd1db927068d04f3d73b12ec2a58aa0630
Petr Karnakov <kpetr@ethz.ch>
2018-11-20 16:49:07 +0100

    sim04: add vary_cpr_coal, add b2 to ba

--- sim/sim04_univel/ba/main.c
+++ sim/sim04_univel/ba/main.c
@@ -11,6 +11,19 @@ double sqr(double a) {
   return a * a;
 }
 
+double ifr2(double x, double y) {
+  double r = sq(x - BCX) + sq(y - BCY);
+  double r2 = sq(x - BC2X) + sq(y - BC2Y);
+  return fmax(sq(BR) - r, sq(BR2) - r2);
+}
+
+double ifr3(double x, double y, double z) {
+  double r = sq(x - BCX) + sq(y - BCY) + sq(z - BCZ);
+  double r2 = sq(x - BC2X) + sq(y - BC2Y) + sq(z - BC2Z);
+  return fmax(sq(BR) - r, sq(BR2) - r2);
+}
+
+
 int main() {
   init_grid(1 << REFINE);
 
@@ -36,9 +49,9 @@ event init (i = 0) {
   }
 
 #if dimension == 2
-  fraction (f, sq(BR) - (sq(x - BCX) + sq(y - BCY)));
+  fraction(f, ifr2(x, y));
 #elif dimension == 3
-  fraction (f, sq(BR) - (sq(x - BCX) + sq(y - BCY) + sq(z - BCZ)));
+  fraction(f, ifr3(x, y, z));
 #endif
 }
 
@@ -50,6 +63,10 @@ event out (t += DUMPDT ; t < TMAX) {
   FILE * fp = fopen(name, "w");
   scalar * a = {u, p, f};
   io(a, fp);
+}
+
+event statout (i += 10) {
+  printf("step i=%d\n", i);
 }
 
 event logfile (i += 1) {
@@ -69,8 +86,8 @@ event logfile (i += 1) {
     vby += u.y[]*dv;
     vbz += u.z[]*dv;
     sb += dv;
-    p0 = min(p0, p[]);
-    p1 = max(p1, p[]);
+    p0 = fmin(p0, p[]);
+    p1 = fmax(p1, p[]);
   }
 
   double vlmx = 0., vlmy = 0., vlmz = 0.;
@@ -78,9 +95,9 @@ event logfile (i += 1) {
   foreach(reduction(+:vl2x) reduction(+:vl2y) reduction(+:vl2z)
    reduction(max:vlmx) reduction(max:vlmy) reduction(max:vlmz)
    ) {
-    vlmx = max(vlmx, fabs(u.x[] - VELX) * f[]);
-    vlmy = max(vlmy, fabs(u.y[] - VELY) * f[]);
-    vlmz = max(vlmz, fabs(u.z[] - VELZ) * f[]);
+    vlmx = fmax(vlmx, fabs(u.x[] - VELX) * f[]);
+    vlmy = fmax(vlmy, fabs(u.y[] - VELY) * f[]);
+    vlmz = fmax(vlmz, fabs(u.z[] - VELZ) * f[]);
 
     double dv = f[]*dv();
     vl2x += sqr(u.x[] - VELX) * dv;
@@ -89,10 +106,14 @@ event logfile (i += 1) {
   }
   static char* fn = "o/sc";
   static FILE* f = NULL;
+
   if (!f) {
     f = fopen(fn, "w");
     fprintf(f, "t m2 c2x c2y c2z v2x v2y v2z p0 p1 pd vlmx vlmy vlmz vl2x vl2y vl2z\n");
+  } else {
+    f = fopen(fn, "a");
   }
+
   fprintf(f,
     "%.20f %.20f %.20f %.20f %.20f %.20f %.20f %.20f %.20f %.20f %.20f %.20f %.20f %.20f %.20f %.20f %.20f\n",
     t, sb,
@@ -101,5 +122,6 @@ event logfile (i += 1) {
     vlmx, vlmy, vlmz, 
     sqrt(vl2x/sb), sqrt(vl2y/sb), sqrt(vl2z/sb)
     );
-  fflush(f);
+
+  fclose(f);
 }
