19e739a574d55d9c43b0c5ebb833dc3a2402538d
Petr Karnakov <pkarnakov@gmail.com>
2018-11-29 07:55:21 +0100

    sim06: add par.h

---
+++ sim/sim06_taylor-green/ba/par.h
@@ -0,0 +1,14 @@
+#define TMAX (20.0)
+#define DUMPDT (0.1)
+#define SIGMA (0.1)
+#define RHO1 (1.0)
+#define RHO2 (0.01)
+#define MU1 (0.00125)
+#define MU2 (0.0000125)
+#define NX (256)
+#define EXTENT (6.283185307179586)
+
+#define BCX 3.429127926091155132e+00 
+#define BCY 4.409162652076484612e+00 
+#define BCZ 3.746918904823720808e+00 
+#define BR 1.963495408493620697e-01
