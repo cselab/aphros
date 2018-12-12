f9be3b1b8c79062a2fc27e342f4568fd4858083b
Petr Karnakov <kpetr@ethz.ch>
2018-12-12 11:20:35 +0100

    partstr/randc: add levelset init to ba'

--- sim/partstr/randc/ba/main.c
+++ sim/partstr/randc/ba/main.c
@@ -9,39 +9,88 @@ double sqr(double a) {
   return a * a;
 }
 
-double ifr2(double x, double y, 
-    double bx, double by, double br) {
-  double r = sq(x - bx) + sq(y - by);
-  return sq(br) - r;
+struct Bub {
+  double x;
+  double y;
+  double z;
+  double r;
+} b;
+
+double ifr2(double x, double y) {
+  double r2 = sq(x - b.x) + sq(y - b.y);
+  return sq(b.r) - r2;
 }
 
-double ifr3(double x, double y, double z, 
-    double bx, double by, double bz, double br) {
-  double r = sq(x - bx) + sq(y - by) + sq(z - bz);
-  return sq(br) - r;
+double ifr3(double x, double y, double z) {
+  double r2 = sq(x - b.x) + sq(y - b.y) + sq(z - b.z);
+  return sq(b.r) - r2;
+}
+
+double ls(double x, double y, double z) {
+  return ifr3(x, y, z);
+}
+
+void fraction2(scalar c) {
+  foreach_cell() {
+    double h = Delta;
+    double hh = h * 0.5;
+    if (
+        ls(x, y, z) * ls(x + h, y + h, z + h) > 0. && 
+        ls(x, y, z) * ls(x + h, y + h, z - h) > 0. && 
+        ls(x, y, z) * ls(x + h, y - h, z + h) > 0. && 
+        ls(x, y, z) * ls(x + h, y - h, z - h) > 0. && 
+        ls(x, y, z) * ls(x - h, y + h, z + h) > 0. && 
+        ls(x, y, z) * ls(x - h, y + h, z - h) > 0. && 
+        ls(x, y, z) * ls(x - h, y - h, z + h) > 0. && 
+        ls(x, y, z) * ls(x - h, y - h, z - h) > 0.
+        ) {
+      c[] = (ls(x, y, z) > 0. ? 1. : 0.);
+    } else {
+      double dx = 1e-3 * h;
+      double q = dx * 0.5;
+      double nx = (ls(x + q, y, z) - ls(x - q, y, z)) / dx;
+      double ny = (ls(x, y + q, z) - ls(x, y - q, z)) / dx;
+      double nz = (ls(x, y, z + q) - ls(x, y, z - q)) / dx;
+      double a = ls(x, y, z);
+      c[] = rectangle_fraction(
+        (coord){nx, ny, nz}, a, 
+        (coord){-hh, -hh, -hh}, 
+        (coord){hh, hh, hh}); 
+    }
+  }
+
+  double a = 0.;
+  foreach() {
+    double h = Delta;
+    a += c[] * h * h * h;
+  }
+  printf("%.16g", a);
 }
 
 
 int main() {
   init_grid(1 << REFINE);
 
-  double bx, by, bz, br;
   FILE* fb = fopen("../b.dat", "r");
-  fscanf(fb, "%lf %lf %lf %lf", &bx, &by, &bz, &br);
+  fscanf(fb, "%lf %lf %lf %lf", &b.x, &b.y, &b.z, &b.r);
   fclose(fb);
 
   origin (0.,0.,0.);
 
   scalar vf[]; // volume fraction
 
+  /*
 #if dimension == 2
-  fraction(vf, ifr2(x, y, bx, by, br));
+  fraction(vf, ifr2(x, y));
 #elif dimension == 3
-  fraction(vf, ifr3(x, y, z, bx, by, bz ,br));
+  fraction(vf, ifr3(x, y, z));
 #endif
+*/
+
+  fraction2(vf);
 
   scalar k[]; // curvature
-  curvature(vf, k);
+  //curvature(vf, k);
   
   {
     FILE* q = fopen("t", "w");
