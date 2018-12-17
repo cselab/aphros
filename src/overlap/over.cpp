8f0b4ee2d0e75ba39129958b5ed5d5c0e48256cc
Petr Karnakov <pkarnakov@gmail.com>
2018-12-17 08:34:28 +0100

    overlap: check inner/outer cells

--- src/overlap/over.cpp
+++ src/overlap/over.cpp
@@ -15,12 +15,24 @@ double GetSphereOverlap(const GVect<double, 3>& x, const GVect<double, 3>& h,
     vector_t{-1,  1,  1}
   };
 
+  bool ai = true; // all inside sphere
+  bool ao = true; // all outside sphere
   auto hh = h * 0.5;
   for (size_t i = 0; i < vv.size(); ++i) {
     auto& v = vv[i];
     v[0] = x[0] + v[0] * hh[0];
     v[1] = x[1] + v[1] * hh[1];
     v[2] = x[2] + v[2] * hh[2];
+    bool in = GVect<double, 3>(v[0], v[1], v[2]).sqrdist(c) < sqr(r);
+    ai = ai && in;
+    ao = ao && !in;
+  }
+
+  if (ai) {
+    return 1.;
+  }
+  if (ao) {
+    return 0.;
   }
 
   Hexahedron hex{vv};
