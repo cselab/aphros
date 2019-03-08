e4269f8937d6422e8b65b6b771a39f554299970d
Sergey Litvinov <slitvinov@gmail.com>
2019-03-08 14:26:23 +0100

    wip: segment/example

--- poc/09_javascript/segment/lib/segment.h
+++ poc/09_javascript/segment/lib/segment.h
@@ -1,2 +1,8 @@
+#ifdef __cplusplus
+#    define EXTERNC extern "C"
+#else
+#    define EXTERNC
+#endif
+
 typedef double Scal;
-int segment_get(const Scal*, /**/ Scal **normal, Scal **a);
+EXTERNC int segment_get(const Scal*, /**/ Scal **normal, Scal **a);
