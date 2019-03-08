81040fef263207e56bf892eacf35f3a1762b9648
Sergey Litvinov <slitvinov@gmail.com>
2019-03-08 11:17:09 +0100

    wip: mesh.c, clean up

--- poc/09_javascript/mesh/main.cpp
+++ poc/09_javascript/mesh/main.cpp
@@ -1,32 +1,18 @@
 #undef NDEBUG
 #include "geom/mesh.h"
 
-// Returns true if a < b (lex starting from end)
-template <class T, size_t d>
-bool Cmp(const GVect<T, d>& a, const GVect<T, d>& b) {
-  int i = a.size();
-  while (i--) {
-    if (a[i] != b[i]) {
-      return a[i] < b[i];
-    }
-  }
-  return false;
-}
-
 const int dim = 3;
 using MIdx = GMIdx<dim>;
 using Dir = GDir<dim>;
 using Scal = double;
 using Vect = GVect<Scal, dim>;
 
-void TestMesh() {
+int main() {
   Rect<Vect> dom(Vect(0), Vect(1));
   using M = MeshStructured<Scal, dim>;
   MIdx b(0);
   MIdx s(5, 5, 1);
   int hl = 2;
-  Vect doms = dom.GetDimensions();
-  Vect h = dom.GetDimensions() / Vect(s);
   M m = InitUniformMesh<M>(dom, b, s, hl, true, s);
 
   // Total volume
@@ -36,8 +22,4 @@ void TestMesh() {
   }
   // Comm
   FieldCell<Scal> fc;
-}
-
-int main() {
-  TestMesh();
 }
