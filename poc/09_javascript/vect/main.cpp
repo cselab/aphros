3e7a67ae13ab9ce26ac42578b97714affda27f78
Sergey Litvinov <slitvinov@gmail.com>
2019-03-08 11:02:14 +0100

    clean up Makefiles

--- poc/09_javascript/vect/main.cpp
+++ poc/09_javascript/vect/main.cpp
@@ -2,7 +2,7 @@
 
 int main() {
     using std::cout;
-    GVect<double ,3> a(1), b(2), c;
+    GVect<double, 3> a(1), b(2), c;
 
     c = a + b;
 
@@ -12,6 +12,6 @@ int main() {
 
     cout << a.dot(b) << '\n';
     cout << a.cross(b) << '\n';
-    
+
     return 0;
 }
