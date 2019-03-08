f81aa558224dc19c2764aa2c5738433d06c35ee4
Sergey Litvinov <slitvinov@gmail.com>
2019-03-08 15:05:24 +0100

    expose CalcNormalYoung

--- src/solver/normal.h
+++ src/solver/normal.h
@@ -13,17 +13,27 @@ class UNormal {
   using Scal = typename M::Scal;
   using Vect = typename M::Vect;
 
-  // Computes normal by combined Young's scheme and height-functions
+  // Computes normal by combined Youngs scheme and height-functions
   // m: mesh
   // fcu: volume fraction
   // fci: interface mask (1: contains interface)
   // edim: effective dimension
   // Output: set to NaN if fci=0
-  // fcn: normal with norm1()=1, antigradient of fcu [s] 
+  // fcn: normal with norm1()=1, antigradient of fcu [s]
   // fck: curvature
   static void CalcNormal(
       M& m, const FieldCell<Scal>& fcu, const FieldCell<bool>& fci,
       size_t edim, FieldCell<Vect>& fcn, FieldCell<Scal>& fck);
+
+  // Computes normal by Youngs scheme
+  // m: mesh
+  // fcu: volume fraction
+  // fci: interface mask (1: contains interface)
+  // Output: set to NaN if fci=0
+  // fcn: normal with norm1()=1, antigradient of fcu [s]
+  static void CalcNormalYoung(
+      M& m, const FieldCell<Scal>& fcu, const FieldCell<bool>& fci,
+      FieldCell<Vect>& fcn);
 
  private:
   struct Imp; // implementation
