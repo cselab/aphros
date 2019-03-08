68e1973712a47a56e22924c87d80ef5d29d9a50b
Sergey Litvinov <slitvinov@gmail.com>
2019-03-08 10:54:23 +0100

    play with vect.h

---
+++ poc/09_javascript/vect/main.cpp
@@ -0,0 +1,17 @@
+#include "geom/mesh.h"
+
+int main() {
+    using std::cout;
+    GVect<double ,3> a(1), b(2), c;
+
+    c = a + b;
+
+    cout << a << '\n';
+    cout << b << '\n';
+    cout << c << '\n';
+
+    cout << a.dot(b) << '\n';
+    cout << a.cross(b) << '\n';
+    
+    return 0;
+}
