f8b6e588d97fd73924426669cdcff933dac7ffd4
Sergey Litvinov <slitvinov@gmail.com>
2019-03-11 15:50:27 +0100

    use @ in js.mk

--- make/js.mk
+++ make/js.mk
@@ -1,7 +1,8 @@
 M4 = m4
 M4FLAGS =
 .in.js.js:
-	trap 'rm -f $$t; exit 2' 1 2 3 15 && \
+	@echo creating $@
+	@trap 'rm -f $$t; exit 2' 1 2 3 15 && \
 	t=/tmp/t.$$$$ && \
 	$(M4) $(M4FLAGS) $< > $$t && \
 	mv $$t $@
