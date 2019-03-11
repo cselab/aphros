e665ce443f74178f312d35c0356db19660159607
Sergey Litvinov <slitvinov@gmail.com>
2019-03-11 18:40:39 +0100

    take argumetns in javascript/ends

--- poc/09_javascript/ends/main.in.js
+++ poc/09_javascript/ends/main.in.js
@@ -1,9 +1,17 @@
 undivert(partstr.js)dnl
 changequote()
 
+var stdout = process.stdout
+var stderr = process.stdout
+
 const AX = 0, AY = 1, BX = 2, BY = 3
 var nx, ny, a
 var seg = Array(4)
 
+i = 2
+nx = parseFloat(process.argv[i++])
+ny = parseFloat(process.argv[i++])
+a  = parseFloat(process.argv[i++])
+
 partstr_ends(nx, ny, a, seg)
-process.stdout.write(`${seg[AX]} ${seg[AY]} ${seg[BX]} ${seg[BY]}\n`)
+stdout.write(`${seg[AX]} ${seg[AY]} ${seg[BX]} ${seg[BY]}\n`)
