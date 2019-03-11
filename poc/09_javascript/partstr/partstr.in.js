aa13dce9cfe99fdc52e88666e8ae60bc58abe1c8
Sergey Litvinov <slitvinov@gmail.com>
2019-03-11 19:23:21 +0100

    wip: parser_ends

--- poc/09_javascript/partstr/partstr.in.js
+++ poc/09_javascript/partstr/partstr.in.js
@@ -46,6 +46,58 @@ function partstr_vof_line(M, N, u, a) {
         }
 }
 
-function partstr_ends(nx, ny, a, /**/ s) {
-    s[0] = s[1] = s[2] = s[3] = 0
+function partstr_ends(nx, ny, a, /**/ e) {
+    const X = 0, Y = 1
+    const AX = 0, AY = 1, BX = 2, BY = 3
+    var xl = Array(2)
+    var xr = Array(2)
+    var  n = Array(2)
+    var hh = Array(2)
+
+    n[X] = nx
+    n[Y] = ny
+
+    xl[X] = (a + 0.5 * n[1]) / n[0]
+    xl[Y] = (a + 0.5 * n[0]) / n[1]
+
+    xr[X] = (a - 0.5 * n[1]) / n[0]
+    xr[Y] = (a - 0.5 * n[0]) / n[1]
+
+    e[AX] = e[AY] = e[BX] = e[BY] = 0
+
+    hh[X] = hh[Y] = 0.5
+
+    j = 0
+    if (-hh[0] <= xl[0] && xl[0] <= hh[0]) {
+	e[2*j]   = xl[0]
+	e[2*j+1] = -hh[1]
+	j++
+    }
+    
+    if (-hh[0] <= xr[0] && xr[0] <= hh[0]) {
+	e[2*j] = xr[0]
+	e[2*j + 1] = hh[1]
+	j++
+    }
+    
+    if (i < 2 && -hh[1] <= xl[1] && xl[1] <= hh[1]) {
+	e[2*j] = -hh[0]
+	e[2*j + 1] = xl[1]
+	j++
+    }
+    
+    if (i < 2 && -hh[1] <= xr[1] && xr[1] <= hh[1]) {
+	e[2*j] = hh[0]
+	e[2*j + 1] = xr[1]
+	j++
+    }
+    
+    if (j == 1) {
+	e[2*j] = e[0]
+	e[2*j + 1] = e[1]
+    }
+    return e;
+
+    process.stderr.write(`xl: ${xl[X]} ${xl[Y]}\n`)
+    process.stderr.write(`xr: ${xr[X]} ${xr[Y]}\n`)
 }
