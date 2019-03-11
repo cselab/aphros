417db01edcff60f72a33d8fa18efded1d601639a
Sergey Litvinov <slitvinov@gmail.com>
2019-03-11 18:29:27 +0100

    initial import of /ends

---
+++ poc/09_javascript/ends/main.in.js
@@ -0,0 +1,14 @@
+undivert(matrix.js)dnl
+undivert(partstr.js)dnl
+changequote()
+
+var stdout = process.stdout
+var n = 5
+var h = 2
+var u = matrix_new(n, n)
+var a = matrix_new(n, n)
+
+InitGrid(n, n, u)
+matrix_halo_zero(n, n, h, u)
+partstr_vof_line(n, n, u, a)
+matrix_write(stdout, n, n, a)
