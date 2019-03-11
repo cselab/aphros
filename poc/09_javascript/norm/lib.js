abc2253911b4f2d895389da11a8581545e8a4985
Sergey Litvinov <slitvinov@gmail.com>
2019-03-11 11:12:59 +0100

    wip: js/norm

--- poc/09_javascript/norm/lib.js
+++ poc/09_javascript/norm/lib.js
@@ -1,12 +1,9 @@
 function InitGrid(nx, ny, u) {
-  for (var j = 0; j < ny; j++) {
+  var b = 2
+  for (var j = -b; j < ny + b; j++) {
       u[j] = [];
-      for (var i = 0; i < nx; i++) {
-	  u[j][i] = 1. / (sqr(i - (nx - 1) * 0.5) +
-		    sqr(j - (ny - 1) * 0.5));
-	  if (u[j][i] < 0.3) {
-	      u[j][i] = 0.;
-	  }
+      for (var i = -b; i < nx + b; i++) {
+	  u[j][i] = (i * j) / (nx * ny)
 	  u[j][i] = Clip(u[j][i], 0., 1.)
       }
   }
@@ -21,3 +18,14 @@ function sqr(a) {
     return a * a;
 }
 
+function Norm(i, j, u, /**/ p) {
+    var nx, ny, n
+    var X = 0, Y = 1
+    nx = (u[i+1][j+1]+2*u[i+1][j]+u[i+1][j-1]-u[i-1][j+1]-2*u[i-1][j]-u[i-1][j-1])/8
+    ny = (u[i+1][j+1]-u[i+1][j-1]+2*u[i][j+1]-2*u[i][j-1]+u[i-1][j+1]-u[i-1][j-1])/8
+    n =  -(Math.abs(nx) + Math.abs(ny))
+    nx /= n
+    ny /= n
+    p[X] = nx
+    p[Y] = ny
+}
--- poc/09_javascript/norm/main.in.js
+++ poc/09_javascript/norm/main.in.js
@@ -5,15 +5,25 @@ var nx = 5, ny = 5, u = []
 InitGrid(5, 5, u)
 
 function expo(x) {
-  return Number.parseFloat(x).toExponential(16);
+  return Number.parseFloat(x).toExponential(1);
 }
 
+
 function print(x) { process.stdout.write(x) }
-for (j = 0; j < ny; j++) {
-    for (i = 0; i < nx; i++) {
-	if (i > 0) print(" ")
+
+var px = [], py = [], n = [];
+Norm(2, 2, u, /**/ n)
+
+for (j = -1; j < ny; j++) {
+    for (i = -1; i < nx; i++) {
 	print(expo(u[i][j]))
+	print(" ")
     }
     print("\n")
 }
 
+print("========\n")
+print(expo(n[0]))
+print(" ")
+print(expo(n[1]))
+print("\n")
