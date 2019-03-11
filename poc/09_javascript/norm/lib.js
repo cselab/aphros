2963f0fed1475d58c6edf09b9e5991cfb853802b
Sergey Litvinov <slitvinov@gmail.com>
2019-03-11 10:49:13 +0100

    wip: js/norm

--- poc/09_javascript/norm/lib.js
+++ poc/09_javascript/norm/lib.js
@@ -1,3 +1,23 @@
-function f() {
-    console.log("f")
+function InitGrid(nx, ny, u) {
+  for (var j = 0; j < ny; j++) {
+      u[j] = [];
+      for (var i = 0; i < nx; i++) {
+	  u[j][i] = 1. / (sqr(i - (nx - 1) * 0.5) +
+		    sqr(j - (ny - 1) * 0.5));
+	  if (u[j][i] < 0.3) {
+	      u[j][i] = 0.;
+	  }
+	  u[j][i] = Clip(u[j][i], 0., 1.)
+      }
+  }
+  return u;
 }
+
+function Clip(a,l,h) {
+    return Math.min(Math.max(a, l), h);
+}
+
+function sqr(a) {
+    return a * a;
+}
+
--- poc/09_javascript/norm/main.in.js
+++ poc/09_javascript/norm/main.in.js
@@ -1,3 +1,19 @@
-include(lib.js)dnl
-f()
-f()
+include(lib.js)
+changequote()
+
+var nx = 5, ny = 5, u = []
+InitGrid(5, 5, u)
+
+function expo(x) {
+  return Number.parseFloat(x).toExponential(16);
+}
+
+function print(x) { process.stdout.write(x) }
+for (j = 0; j < ny; j++) {
+    for (i = 0; i < nx; i++) {
+	if (i > 0) print(" ")
+	print(expo(u[i][j]))
+    }
+    print("\n")
+}
+
