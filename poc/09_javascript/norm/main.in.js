d213e0efa650e013ffc88fb756aec66a11a9e15e
Sergey Litvinov <slitvinov@gmail.com>
2019-03-11 17:03:34 +0100

    use partstr_vof_line

--- poc/09_javascript/norm/main.in.js
+++ poc/09_javascript/norm/main.in.js
@@ -7,13 +7,9 @@ var stdout = process.stdout
 var n = 5
 var h = 2
 var u = matrix_new(n, n)
+var a = matrix_new(n, n)
+
 InitGrid(n, n, u)
 matrix_halo_zero(n, n, h, u)
-matrix_lh_write(stdout, -h, -h, n + h, n + h, u)
-
-var p = partstr_norm(2, 0, u)
-stdout.write(`norm: ${p}\n`)
-
-const X = 0, Y = 1
-a = partstr_line(p[X], p[Y], u[2][0])
-stdout.write(`a: ${a}\n`)
+partstr_vof_line(n, n, u, a)
+matrix_write(stdout, n, n, a)
