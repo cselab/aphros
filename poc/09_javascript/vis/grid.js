1d6d80832f327fff35b3c2b643c19303f7f763e7
Petr Karnakov <kpetr@ethz.ch>
2019-03-11 19:56:55 +0100

    poc09/grid: add bar

--- poc/09_javascript/vis/grid.js
+++ poc/09_javascript/vis/grid.js
@@ -12,8 +12,8 @@ var base = {x: 100, y: 100}
 mouse.x = base.x
 mouse.y = base.y
 canvas.addEventListener('mousemove', function(e) {
-  mouse.x = e.pageX - this.offsetLeft;
-  mouse.y = e.pageY - this.offsetTop;
+    mouse.x = e.pageX - this.offsetLeft;
+    mouse.y = e.pageY - this.offsetTop;
 }, false);
 
 function Clip(a,l,h) {
@@ -25,34 +25,34 @@ function sqr(a) {
 }
 
 function InitGrid(nx, ny) {
-  var u = [];
+    var u = [];
 
-  for (var j = 0; j < ny; j++) {
-      u[j] = [];
-      for (var i = 0; i < nx; i++) {
-      u[j][i] = 1. / (sqr(i - (nx - 1) * 0.5) +
-                      sqr(j - (ny - 1) * 0.5));
-      if (u[j][i] < 0.3) {
-          u[j][i] = 0.;
-      }
-          u[j][i] = Clip(u[j][i], 0., 1.)
-      }
-  }
+    for (var j = 0; j < ny; j++) {
+        u[j] = [];
+        for (var i = 0; i < nx; i++) {
+            u[j][i] = 1. / (sqr(i - (nx - 1) * 0.5) +
+                sqr(j - (ny - 1) * 0.5));
+            if (u[j][i] < 0.3) {
+                u[j][i] = 0.;
+            }
+            u[j][i] = Clip(u[j][i], 0., 1.)
+        }
+    }
 
-  return u;
+    return u;
 }
 
 function CopyGrid(nx, ny, us) {
-  var u = [];
+    var u = [];
 
-  for (var j = 0; j < ny; j++) {
-      u[j] = [];
-      for (var i = 0; i < nx; i++) {
-          u[j][i] = us[j][i];
-      }
-  }
+    for (var j = 0; j < ny; j++) {
+        u[j] = [];
+        for (var i = 0; i < nx; i++) {
+            u[j][i] = us[j][i];
+        }
+    }
 
-  return u;
+    return u;
 }
 
 canvas.addEventListener('mousedown', function(e) {
@@ -65,6 +65,10 @@ canvas.addEventListener('mousedown', function(e) {
 }, false);
 
 canvas.addEventListener('mouseup', function() {
+    canvas.removeEventListener('mousemove', onPaint, false);
+}, false);
+
+canvas.addEventListener('mouseout', function() {
     canvas.removeEventListener('mousemove', onPaint, false);
 }, false);
 
@@ -88,42 +92,74 @@ function RGBToHex(r,g,b) {
 }
 
 function F(a) {
-    return Math.pow(a, 2);
+    //return a * a;
+    return a;
 }
+
+function Finv(a) {
+    //return Math.sqrt(a);
+    return a;
+}
+
 
 
 var onPaint = function() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     var w = 60;
+    var k = 2.;
+    var wk = w * k;
 
     var i = Clip(Math.floor((start.x - base.x) / w), 0, nx - 1);
     var j = Clip(Math.floor((start.y - base.y) / w), 0, ny - 1);
 
-    dy = start.y - mouse.y;
-    dy = dy / w;
-    dy = F(Math.abs(dy)) * Math.sign(dy) / F(2.);
-    u[j][i] = Clip(ustart[j][i] + dy, 0., 1.);
+    // displacement [px]
+    dy = mouse.y - start.y;
+    // range of u correction: [-dum,dup]
+    dup = 1. - ustart[j][i]
+    dum = ustart[j][i]
+    // normalize
+    dyn = dy / wk
+
+    dyp = Finv(dup) * wk
+    dym = Finv(dum) * wk
+
+    dy = Clip(dy, -dyp, dym)
+
+    // correction
+    du = -F(Math.abs(dyn)) * Math.sign(dyn);
+
+    u[j][i] = Clip(ustart[j][i] + du, 0., 1.);
 
     for (var j = 0 ; j < ny; j++) {
-    for (var i = 0 ; i < nx; i++) {
-        x = base.x + i * w;
-        y = base.y + j * w;
-        ctx.strokeRect(x, y, w, w);
-        q = u[j][i];
-        q = Math.floor((1-q) * 255 + q * 127);
-        ctx.fillStyle = RGBToHex(q, q, q);
-        ctx.fillRect(x, y, w, w);
+        for (var i = 0 ; i < nx; i++) {
+            x = base.x + i * w;
+            y = base.y + j * w;
+            ctx.strokeRect(x, y, w, w);
+            q = u[j][i];
+            q = Math.floor((1-q) * 255 + q * 127);
+            ctx.fillStyle = RGBToHex(q, q, q);
+            ctx.fillRect(x, y, w, w);
 
-        ctx.font = "20px Arial";
-        ctx.fillStyle = "#000000";
-        ctx.textAlign = "center";
-        ctx.textBaseline = "middle";
-        ctx.fillText((u[j][i]).toFixed(2), x + w * 0.5, y + w * 0.5);
+            ctx.font = "20px Arial";
+            ctx.fillStyle = "#000000";
+            ctx.textAlign = "center";
+            ctx.textBaseline = "middle";
+            ctx.fillText((u[j][i]).toFixed(2), x + w * 0.5, y + w * 0.5);
+        }
     }
-    }
-    ctx.fillStyle = 'red';
-    ctx.fillRect(mouse.x, mouse.y, 10, 10);
+    q = 8
+    qh = q / 2
+    qq = q / 2
+    qqh = qq / 2
+    ctx.fillStyle = '#a0a0a0';
+    ctx.fillRect(start.x-qh, start.y +dym, q, -dyp-dym-qq);
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(start.x-qqh, start.y-qqh, qq, dy);
+    ctx.fillStyle = 'green';
+    ctx.fillRect(start.x-qh, start.y-qh, q, q);
+    //ctx.fillStyle = 'red';
+    //ctx.fillRect(start.x-qh, start.y + dy-qh, q, q);
 };
 
 ctx.lineWidth = 3;
