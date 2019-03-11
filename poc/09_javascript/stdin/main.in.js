2b01d08f86b8379a45882dc7b93933248f5e2f01
Sergey Litvinov <slitvinov@gmail.com>
2019-03-11 15:27:27 +0100

    add matrix_read and matrix_write

--- poc/09_javascript/stdin/main.in.js
+++ poc/09_javascript/stdin/main.in.js
@@ -1,13 +1,60 @@
 changequote()
 
-const fs = require('fs')
-const readline = require('readline')
+function matrix_read(file) {
+    var a, nr, NR, l, line, i, j, x, m
+    var lines = require('fs').readFileSync(file, 'ascii')
+        .split('\n')
+        .filter(Boolean);
 
-const rl = readline.createInterface({
-    input: process.stdin
-})
+    NR = lines.length
+    if (NR < 0)
+        throw new Error(`no lines in file ${file}`)
 
-rl.on('line', (line) => {
-    process.stdout.write(line)
-    process.stdout.write("\n")
-})
+    nr = 0
+    line = lines[nr++]
+    l = line.split(" ")
+    if (l.length != 2)
+        throw new Error(`expecting 'M N' got '${line}'`)
+    M = parseInt(l[0])
+    if (Number.isNaN(M))
+        throw new Error(`expecting 'M', got '${l[0]}'`)
+    N = parseInt(l[1])
+    if (Number.isNaN(N))
+        throw new Error(`expecting 'N', got '${l[1]}'`)
+    a = new Array(M)
+    for (i = 0; i < M; i++)
+        a[i] = new Array(N)
+
+    for (i = 0; i < M; i++) {
+        if (nr >= NR)
+            throw new Error(`too few lines in ${file}, expecting ${NR}, got ${nr}`)
+        line = lines[nr++]
+        l = line.split(" ")
+        if (l.length != N)
+            throw new Error(`expecting ${N} numbers, got '${line}'`)
+        for (j = 0; j < N; j++) {
+            x = parseFloat(l[j])
+            if (Number.isNaN(x))
+                throw new Error(`expecting float, got '${l[i]}'`)
+            a[i][j] = x
+        }
+    }
+    return a
+}
+
+function matrix_write(stream, M, N, a) {
+    var i, j
+    for (i = 0; i < M; i++) {
+        for (j = 0; j < N; j++) {
+            if (j > 0)
+                stream.write(" ")
+            stream.write(`${a[i][j]}`)
+        }
+        stream.write("\n")
+    }
+}
+
+a = matrix_read("/dev/stdin")
+M = a.length
+N = a[0].length
+matrix_write(process.stdout, M, N, a)
