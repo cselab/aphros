076e12092fc9628d61db78bbfb32d36de4b8670d
Sergey Litvinov <slitvinov@gmail.com>
2019-03-13 19:59:39 +0100

    poc09: initial import of example/force

---
+++ poc/09_javascript/example/force/main.in.js
@@ -0,0 +1,24 @@
+undivert(matrix.js)dnl
+undivert(partstr.js)dnl
+undefine(shift)dnl
+changequote()
+
+const X = 0, Y = 1
+const stdout = process.stdout
+const stderr = process.stderr
+const argv = process.argv
+var ax, ay, bx, by, xx, xy, y
+var k, ends, n, file
+
+argv.shift(); argv.shift()
+file = argv.shift()
+k = parseFloat(argv.shift())
+xx = parseFloat(argv.shift())
+xy = parseFloat(argv.shift())
+
+ends = partstr_ends_read(file)
+n = ends.length
+y = partstr_nearest_ends(n, ends, [xx, xy], k)
+
+stderr.write(`${y[X]} ${y[Y]}\n`)
+partstr_ends_gnuplot_write(stdout, ends)
