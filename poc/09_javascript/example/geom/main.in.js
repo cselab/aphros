e2c944cc70f778d1b3d5fe0e4201118e3de89a35
Sergey Litvinov <slitvinov@gmail.com>
2019-03-13 13:56:03 +0100

    poc09: initial import of geom; add a test for part

---
+++ poc/09_javascript/example/geom/main.in.js
@@ -0,0 +1,18 @@
+undivert(matrix.js)dnl
+undivert(partstr.js)dnl
+changequote()
+
+const stdout = process.stdout
+const stderr = process.stdout
+
+var k = 2, i, j, e
+var nh = parseInt(process.argv[k++])
+var hp = parseFloat(process.argv[k++])
+var a = parseFloat(process.argv[k++])
+var t = parseFloat(process.argv[k++])
+
+var p = [0, 0]
+
+var xx = partstr_part(nh, hp, p, a, t)
+var M = xx.length
+matrix_write(stdout, M, 2, xx)
