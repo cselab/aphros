c76eab41de24cdf88f846b51367c4293baa085f8
Sergey Litvinov <slitvinov@gmail.com>
2019-03-13 11:48:58 +0100

    poc09: initial import of partstr_cell_ends

--- poc/09_javascript/lib0/partstr/partstr.in.js
+++ poc/09_javascript/lib0/partstr/partstr.in.js
@@ -27,12 +27,12 @@ function partstr_line(nx, ny, u) {
     nx = Math.abs(nx)
     ny = Math.abs(ny)
     if (ny < nx) {
-        t = nx; nx = ny; ny = t
+	t = nx; nx = ny; ny = t
     }
     if (u < 0.5)
-        return _line(nx, ny, u)
+	return _line(nx, ny, u)
     else
-        return -_line(nx, ny, 1 - u)
+	return -_line(nx, ny, 1 - u)
 }
 
 function partstr_vof_line(M, N, u, /**/ a) {
@@ -40,10 +40,10 @@ function partstr_vof_line(M, N, u, /**/ a) {
     var n, u0, a0
     for (i = 0; i < M; i++)
     for (j = 0; j < N; j++) {
-        n = partstr_norm(i, j, u)
-        u0 = u[i][j]
-        a0 = partstr_line(n[X], n[Y], u0)
-        a[i][j] = a0
+	n = partstr_norm(i, j, u)
+	u0 = u[i][j]
+	a0 = partstr_line(n[X], n[Y], u0)
+	a[i][j] = a0
     }
 }
 
@@ -55,12 +55,12 @@ function partstr_vof_ends(M, N, u, /**/ ends) {
     var e = Array(4)
     for (i = 0; i < M; i++)
     for (j = 0; j < N; j++) {
-        n = partstr_norm(i, j, u)
-        a = partstr_line(n[X], n[Y], u[i][j])
-        partstr_ends(n[X], n[Y], a, /**/ e)
-        e[AX] += i*h + h/2; e[AY] += j*h + h/2
-        e[BX] += i*h + h/2; e[BY] += j*h + h/2
-        ends[i][j] = e.slice()
+	n = partstr_norm(i, j, u)
+	a = partstr_line(n[X], n[Y], u[i][j])
+	partstr_ends(n[X], n[Y], a, /**/ e)
+	e[AX] += i*h + h/2; e[AY] += j*h + h/2
+	e[BX] += i*h + h/2; e[BY] += j*h + h/2
+	ends[i][j] = e.slice()
     }
 }
 
@@ -78,28 +78,48 @@ function partstr_ends(M, N, a, /**/ e) {
     e[0] = e[1] = e[2] = e[3] = 0
     j = 0
     if (-h <= x && x <= h) {
-        e[2*j] = x; e[2*j+1] = -h; j++
+	e[2*j] = x; e[2*j+1] = -h; j++
     }
 
     if (-h <= u && u <= h) {
-        e[2*j] = u; e[2*j + 1] = h; j++
+	e[2*j] = u; e[2*j + 1] = h; j++
     }
 
     if (j < 2 && -h <= y && y <= h) {
-        e[2*j] = -h; e[2*j + 1] = y; j++
+	e[2*j] = -h; e[2*j + 1] = y; j++
     }
 
     if (j < 2 && -h <= v && v <= h) {
-        e[2*j] = h; e[2*j + 1] = v; j++
+	e[2*j] = h; e[2*j + 1] = v; j++
     }
 
     if (j == 1) {
-        e[2*j] = e[0]; e[2*j + 1] = e[1]
+	e[2*j] = e[0]; e[2*j + 1] = e[1]
     }
 
     cross = (e[BY]-e[AY])*M-(e[BX]-e[AX])*N
     if (cross < 0) {
-        t = e[AX]; e[AX] = e[BX]; e[BX] = t
-        t = e[AY]; e[AY] = e[BY]; e[BY] = t
+	t = e[AX]; e[AX] = e[BX]; e[BX] = t
+	t = e[AY]; e[AY] = e[BY]; e[BY] = t
     }
+}
+
+function partstr_cell_ends(M, N, m, n, /**/ ends) {
+    const s = 2
+    var e, ans, k
+    ans = Array(); k = 1
+    for (i = m - s; i < m + s; i++) {
+	if (i <  0) continue
+	if (i >= M) continue
+	for (j = n - s; j < n + s; j++) {
+	    if (j <  0) continue
+	    if (j >= N) continue
+	    e = ends[i][j]
+	    if (i == n && j == m)
+		ans[0] = e
+	    else
+		ans[k++] = e
+	}
+    }
+    return ans
 }
