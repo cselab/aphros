6ac62f8a38ba47f762f4d6ec54738b8917d89ce2
Sergey Litvinov <slitvinov@gmail.com>
2019-03-13 14:55:05 +0100

    poc09: tab/spaces

--- poc/09_javascript/lib0/matrix/matrix.in.js
+++ poc/09_javascript/lib0/matrix/matrix.in.js
@@ -2,37 +2,37 @@ changequote()dnl
 function matrix_read(file) {
     var a, nr, NR, l, line, i, j, x, m
     var lines = require('fs').readFileSync(file, 'ascii')
-	.split('\n')
-	.filter(Boolean)
+        .split('\n')
+        .filter(Boolean)
     NR = lines.length
     if (NR < 0)
-	throw new Error(`no lines in file ${file}`)
+        throw new Error(`no lines in file ${file}`)
     nr = 0
     line = lines[nr++]
     l = line.trim().split(" ")
     if (l.length != 2)
-	throw new Error(`expecting 'M N' got '${line}'`)
+        throw new Error(`expecting 'M N' got '${line}'`)
     M = parseInt(l[0])
     if (Number.isNaN(M))
-	throw new Error(`expecting 'M', got '${l[0]}'`)
+        throw new Error(`expecting 'M', got '${l[0]}'`)
     N = parseInt(l[1])
     if (Number.isNaN(N))
-	throw new Error(`expecting 'N', got '${l[1]}'`)
+        throw new Error(`expecting 'N', got '${l[1]}'`)
     a = matrix_new(M, N)
 
     for (i = 0; i < M; i++) {
-	if (nr >= NR)
-	    throw new Error(`too few lines in ${file}, expecting ${NR}, got ${nr}`)
-	line = lines[nr++]
-	l = line.trim().split(" ")
-	if (l.length != N)
-	    throw new Error(`expecting ${N} numbers, got '${line}'`)
-	for (j = 0; j < N; j++) {
-	    x = parseFloat(l[j])
-	    if (Number.isNaN(x))
-		throw new Error(`expecting float, got '${l[i]}'`)
-	    a[i][j] = x
-	}
+        if (nr >= NR)
+            throw new Error(`too few lines in ${file}, expecting ${NR}, got ${nr}`)
+        line = lines[nr++]
+        l = line.trim().split(" ")
+        if (l.length != N)
+            throw new Error(`expecting ${N} numbers, got '${line}'`)
+        for (j = 0; j < N; j++) {
+            x = parseFloat(l[j])
+            if (Number.isNaN(x))
+                throw new Error(`expecting float, got '${l[i]}'`)
+            a[i][j] = x
+        }
     }
     return a
 }
@@ -41,27 +41,27 @@ function matrix_write(stream, M, N, a) {
     var i, j
     stream.write(`${M} ${N}\n`)
     for (i = 0; i < M; i++) {
-	if (a[i] === void 0)
-	    throw new Error(`a[${i}] is undefined, M = ${M}, N = ${N}`)
-	for (j = 0; j < N; j++) {
-	    if (j > 0)
-		stream.write(" ")
-	    stream.write(`${a[i][j]}`)
-	}
-	stream.write("\n")
+        if (a[i] === void 0)
+            throw new Error(`a[${i}] is undefined, M = ${M}, N = ${N}`)
+        for (j = 0; j < N; j++) {
+            if (j > 0)
+                stream.write(" ")
+            stream.write(`${a[i][j]}`)
+        }
+        stream.write("\n")
     }
 }
 
 function matrix_lh_write(stream, m, n, M, N, a) {
     var i, j
     for (i = m; i < M; i++) {
-	if (a[i] == void 0)
-	    throw new Error(`a[${i}] is undefined, M = ${M}, N = ${N}`)
-	for (j = n; j < N; j++) {
-	    if (j > n) stream.write(" ")
-	    stream.write(`${a[i][j]}`)
-	}
-	stream.write("\n")
+        if (a[i] == void 0)
+            throw new Error(`a[${i}] is undefined, M = ${M}, N = ${N}`)
+        for (j = n; j < N; j++) {
+            if (j > n) stream.write(" ")
+            stream.write(`${a[i][j]}`)
+        }
+        stream.write("\n")
     }
 }
 
@@ -69,7 +69,7 @@ function matrix_new(M, N) {
     var i
     a = new Array(M)
     for (i = 0; i < M; i++)
-	a[i] = new Array(N)
+        a[i] = new Array(N)
     return a
 }
 
@@ -77,8 +77,8 @@ function matrix_zero(M, N) {
     var i, j
     a = matrix_new(M, N)
     for (i = 0; i < M; i++)
-	for (j = 0; j < M; j++)
-	    a[i][j] = 0
+        for (j = 0; j < M; j++)
+            a[i][j] = 0
     return a
 }
 
@@ -89,13 +89,13 @@ function matrix_halo_zero(M, N, h, a) {
     for (i =  N; i < N + h; i++) a[i] = []
 
     for (i = -h; i < 0; i++)
-	for (j = -h; j < N + h; j++) a[i][j] = 0;
+        for (j = -h; j < N + h; j++) a[i][j] = 0;
 
     for (i = 0; i < M; i++) {
-	for (j = -h; j < 0;      j++) a[i][j] = 0;
-	for (j =   N; j < N + h; j++) a[i][j] = 0;
+        for (j = -h; j < 0;      j++) a[i][j] = 0;
+        for (j =   N; j < N + h; j++) a[i][j] = 0;
     }
 
     for (i = M; i < M + h; i++)
-	for (j = -h; j < N + h; j++) a[i][j] = 0;
+        for (j = -h; j < N + h; j++) a[i][j] = 0;
 }
--- poc/09_javascript/lib0/partstr/partstr.in.js
+++ poc/09_javascript/lib0/partstr/partstr.in.js
@@ -17,9 +17,9 @@ function _line(nx, ny, u) {
     var u1
     u1 = 0.5 * nx / ny
     if (u <= u1)
-      return -0.5 * (nx + ny) + Math.sqrt(2*nx*ny*u)
+        return -0.5 * (nx + ny) + Math.sqrt(2*nx*ny*u)
     else
-      return ny * (u - 0.5)
+        return ny * (u - 0.5)
 }
 
 function partstr_line(nx, ny, u) {
@@ -27,12 +27,12 @@ function partstr_line(nx, ny, u) {
     nx = Math.abs(nx)
     ny = Math.abs(ny)
     if (ny < nx) {
-	t = nx; nx = ny; ny = t
+        t = nx; nx = ny; ny = t
     }
     if (u < 0.5)
-	return _line(nx, ny, u)
+        return _line(nx, ny, u)
     else
-	return -_line(nx, ny, 1 - u)
+        return -_line(nx, ny, 1 - u)
 }
 
 function partstr_vof_line(M, N, u, /**/ a) {
@@ -40,12 +40,12 @@ function partstr_vof_line(M, N, u, /**/ a) {
     var i, j
     var n, u0, a0
     for (i = 0; i < M; i++)
-    for (j = 0; j < N; j++) {
-	n = partstr_norm(i, j, u)
-	u0 = u[i][j]
-	a0 = partstr_line(n[X], n[Y], u0)
-	a[i][j] = a0
-    }
+        for (j = 0; j < N; j++) {
+            n = partstr_norm(i, j, u)
+            u0 = u[i][j]
+            a0 = partstr_line(n[X], n[Y], u0)
+            a[i][j] = a0
+        }
 }
 
 function partstr_vof_ends(M, N, u, /**/ ends) {
@@ -55,14 +55,14 @@ function partstr_vof_ends(M, N, u, /**/ ends) {
     var n, a, i, j;
     var e = Array(4)
     for (i = 0; i < M; i++)
-    for (j = 0; j < N; j++) {
-	n = partstr_norm(i, j, u)
-	a = partstr_line(n[X], n[Y], u[i][j])
-	partstr_ends(n[X], n[Y], a, /**/ e)
-	e[AX] += i*h + h/2; e[AY] += j*h + h/2
-	e[BX] += i*h + h/2; e[BY] += j*h + h/2
-	ends[i][j] = e.slice()
-    }
+        for (j = 0; j < N; j++) {
+            n = partstr_norm(i, j, u)
+            a = partstr_line(n[X], n[Y], u[i][j])
+            partstr_ends(n[X], n[Y], a, /**/ e)
+            e[AX] += i*h + h/2; e[AY] += j*h + h/2
+            e[BX] += i*h + h/2; e[BY] += j*h + h/2
+            ends[i][j] = e.slice()
+        }
 }
 
 function partstr_ends(M, N, a, /**/ e) {
@@ -80,29 +80,29 @@ function partstr_ends(M, N, a, /**/ e) {
     e[0] = e[1] = e[2] = e[3] = 0
     j = 0
     if (-h <= x && x <= h) {
-	e[2*j] = x; e[2*j+1] = -h; j++
+        e[2*j] = x; e[2*j+1] = -h; j++
     }
 
     if (-h <= u && u <= h) {
-	e[2*j] = u; e[2*j + 1] = h; j++
+        e[2*j] = u; e[2*j + 1] = h; j++
     }
 
     if (j < 2 && -h <= y && y <= h) {
-	e[2*j] = -h; e[2*j + 1] = y; j++
+        e[2*j] = -h; e[2*j + 1] = y; j++
     }
 
     if (j < 2 && -h <= v && v <= h) {
-	e[2*j] = h; e[2*j + 1] = v; j++
+        e[2*j] = h; e[2*j + 1] = v; j++
     }
 
     if (j == 1) {
-	e[2*j] = e[0]; e[2*j + 1] = e[1]
+        e[2*j] = e[0]; e[2*j + 1] = e[1]
     }
 
     cross = (e[BY]-e[AY])*M-(e[BX]-e[AX])*N
     if (cross < 0) {
-	t = e[AX]; e[AX] = e[BX]; e[BX] = t
-	t = e[AY]; e[AY] = e[BY]; e[BY] = t
+        t = e[AX]; e[AX] = e[BX]; e[BX] = t
+        t = e[AY]; e[AY] = e[BY]; e[BY] = t
     }
 }
 
@@ -112,22 +112,22 @@ function partstr_cell_ends(M, N, m, n, /**/ ends) {
     Seen = false
     ans = Array(); k = 1
     for (i = m - s; i < m + s + 1; i++) {
-	if (i <  0) continue
-	if (i >= M) continue
-	for (j = n - s; j < n + s + 1; j++) {
-	    if (j <  0) continue
-	    if (j >= N) continue
-	    e = ends[i][j]
-	    if (i == m && j == n) {
-		Seen = true
-		ans[0] = e
-	    } else
-		ans[k++] = e
-	}
+        if (i <  0) continue
+        if (i >= M) continue
+        for (j = n - s; j < n + s + 1; j++) {
+            if (j <  0) continue
+            if (j >= N) continue
+            e = ends[i][j]
+            if (i == m && j == n) {
+                Seen = true
+                ans[0] = e
+            } else
+                ans[k++] = e
+        }
     }
     if (!Seen)
-	throw new Error(
-	    `have not seen ends[${m}][${n}], M = ${M}, N = ${N}\n`)
+        throw new Error(
+            `have not seen ends[${m}][${n}], M = ${M}, N = ${N}\n`)
     return ans
 }
 
@@ -153,9 +153,9 @@ function partstr_part(nh, hp, p, a, t) {
     xx = matrix_new(n, 2)
     xx[nh] = p.slice()
     for (j = 0; j < nh; j++) {
-	jp = j + 0.5
-	_axpy( hp, _E(a + t*jp), xx[nh + j], /**/ xx[nh + j + 1])
-	_axpy(-hp, _E(a - t*jp), xx[nh - j], /**/ xx[nh - j - 1])
+        jp = j + 0.5
+        _axpy( hp, _E(a + t*jp), xx[nh + j], /**/ xx[nh + j + 1])
+        _axpy(-hp, _E(a - t*jp), xx[nh - j], /**/ xx[nh - j - 1])
     }
     return xx
 }
@@ -165,13 +165,13 @@ function partstr_segcirc(k, l, d) {
     t1 = 1 - (k*l)**2
     t2 = 1 - (k*d)**2
     if (t1 < 0)
-	throw new Error(`t1=${t1} < 0, k=${k}, l=${l}, d=${d}\n`)
+        throw new Error(`t1=${t1} < 0, k=${k}, l=${l}, d=${d}\n`)
     if (t2 < 0)
-	throw new Error(`t2=${t2} < 0, k=${k}, l=${l}, d=${d}\n`)
+        throw new Error(`t2=${t2} < 0, k=${k}, l=${l}, d=${d}\n`)
     t1 = Math.sqrt(t1)
     t2 = Math.sqrt(t2)
     if (t1 + t2 === 0.0)
-	throw new Error(`t1 + t2 === 0, k=${k}, l=${l}, d=${d}\n`)
+        throw new Error(`t1 + t2 === 0, k=${k}, l=${l}, d=${d}\n`)
     return k*(l*l - d*d)/(t1 + t2)
 }
 
