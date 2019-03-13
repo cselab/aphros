8e160d2760f58f822d479ce739e320e6440abd6c
Sergey Litvinov <slitvinov@gmail.com>
2019-03-13 19:14:50 +0100

    poc09: minor

--- poc/09_javascript/example/nearest_end/main.in.js
+++ poc/09_javascript/example/nearest_end/main.in.js
@@ -8,13 +8,14 @@ const stderr = process.stdout
 const argv = process.argv
 
 argv.shift(); argv.shift()
-var ax = parseFloat(argv.shift())
-var ay = parseFloat(argv.shift())
+var ax, ay, bx, by, xx, xy
+ax = parseFloat(argv.shift())
+ay = parseFloat(argv.shift())
 
-var bx = parseFloat(argv.shift())
-var by = parseFloat(argv.shift())
+bx = parseFloat(argv.shift())
+by = parseFloat(argv.shift())
 
-var xx = parseFloat(argv.shift())
-var xy = parseFloat(argv.shift())
+xx = parseFloat(argv.shift())
+xy = parseFloat(argv.shift())
 
 stdout.write(`${ partstr_nearest([ax, ay], [bx, by], [xx, xy])}\n`)
