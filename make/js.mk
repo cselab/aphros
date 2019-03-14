94aefd0fd2e3505e7ee4ba6a133ac45309bdea20
Sergey Litvinov <slitvinov@gmail.com>
2019-03-14 09:18:49 +0100

    poc09: add a rule for js executable

--- make/js.mk
+++ make/js.mk
@@ -1,9 +1,15 @@
 M4 = m4
 M4FLAGS =
+
 .in.js.js:
 	@echo creating $@
 	@trap 'rm -f $$t; exit 2' 1 2 3 15 && \
 	t=/tmp/t.$$$$ && \
-	$(M4) $(M4FLAGS) $< > $$t && \
+	$(M4) $(M4FLAGS) $^ > $$t && \
 	mv $$t $@
+
+.js:
+	@echo creating executable $@
+	cat $< > $@ && chmod a+x $@
+
 .SUFFIXES: .js .in.js
