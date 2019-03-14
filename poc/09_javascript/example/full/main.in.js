8f909d2ccdad62a1f1b358e24df285277fe85f99
Sergey Litvinov <slitvinov@gmail.com>
2019-03-14 20:16:24 +0100

    poc09: initil import of example/full

---
+++ poc/09_javascript/example/full/main.in.js
@@ -0,0 +1,29 @@
+include(mh.m4)dnl
+#!/usr/bin/env node
+mh_include(matrix.js)dnl
+mh_include(partstr.js)dnl
+
+const AX = 0, AY = 1, BX = 2, BY = 3
+const stdout = process.stdout
+const stderr = process.stderr
+const hl = 2
+
+var i = 2
+var file = process.argv[i++]
+var u = matrix_read(file)
+var M = u.length
+var N = u[0].length
+
+matrix_halo_zero(M, N, hl, u)
+
+var ends = matrix_new(M, N)
+partstr_vof_ends(M, N, u, ends)
+
+var i, j, eb
+for (i = 0; i < M; i++)
+    for (j = 0; j < M; j++) {
+        e = ends[i][j]
+        stdout.write(`${e[AX]} ${e[AY]}\n`)
+        stdout.write(`${e[BX]} ${e[BY]}\n`)
+        stdout.write("\n")
+    }
