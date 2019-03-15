53ab1333c1bdbbe0991d819e5189e4ac42a11660
Sergey Litvinov <slitvinov@gmail.com>
2019-03-15 09:25:22 +0100

    mv Partstr to partstr.in.js

--- poc/09_javascript/example/full/main.in.js
+++ poc/09_javascript/example/full/main.in.js
@@ -4,49 +4,6 @@ include(mh.m4)dnl
 mh_include(matrix.js)dnl
 mh_include(partstr.js)dnl
 
-function Partstr(nh, hp, eta) {
-    const n = 2*nh + 1
-    this.nh = nh
-    this.hp = hp
-    this.eta = eta
-    this.ff = matrix_new(n, 2)
-    this.xx = matrix_new(n, 2)
-
-    this.start = function(ne, ends, a, t, p) {
-        if (!Array.isArray(ends))
-            throw new Error(`ends is not an array: ${ends}`)
-        if (!Array.isArray(p))
-            throw new Error(`p is not an array: ${p}`)
-        this.ne = ne
-        this.ends = ends
-        this.a = a
-        this.t = t
-        this.p = p.slice()
-    }
-
-    this.step = function() {
-        var nh, p, a, t, hp, eta, ends, ff, State, k, ne, xx
-        nh = this.nh
-        const n = 2*nh + 1
-        hp = this.hp
-        eta = this.eta
-        ends = this.ends
-        ne = this.ne
-        xx = this.xx
-        ff = this.ff
-        p = this.p; a = this.a; t = this.t
-        k = partstr_curv(hp, t)
-        partstr_part(nh, hp, p, a, t, /**/ xx)
-        partstr_force(ne, ends, n, xx, k, eta,     ff)
-        State = { p: p, a: a, t: t }
-        partstr_step(nh, ff, eta, hp, /*io*/ State)
-        this.a = State.a; this.t = State.t; this.p = State.p
-        this.k = k
-        this.xx = xx
-        this.ff = ff
-    }
-}
-
 var argv, nh, hl, hp, eta, a, t, file, u, M, N
 var ends, p, end, partstr, i, j, ne, n
 
