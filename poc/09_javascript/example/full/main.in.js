eb1bcd02e022981f9e7338a1f5094a303181440d
Sergey Litvinov <slitvinov@gmail.com>
2019-03-15 08:53:21 +0100

    simplify an interface

--- poc/09_javascript/example/full/main.in.js
+++ poc/09_javascript/example/full/main.in.js
@@ -4,23 +4,24 @@ include(mh.m4)dnl
 mh_include(matrix.js)dnl
 mh_include(partstr.js)dnl
 
-function Partstr(nh, hl, hp, eta, M, N) {
+function Partstr(nh, hp, eta) {
     const n = 2*nh + 1
     this.nh = nh
-    this.hl = hl
     this.hp = hp
     this.eta = eta
     this.ff = matrix_new(n, 2)
     this.xx = matrix_new(n, 2)
-    this.M = M
-    this.N = N
-    this.new = function(ends, i, j, a, t) {
+
+    this.start = function(ne, ends, a, t, p) {
         if (!Array.isArray(ends))
             throw new Error(`ends is not an array: ${ends}`)
-        this.ends = partstr_cell_ends(this.M, this.N, i, j, ends)
+        if (!Array.isArray(p))
+            throw new Error(`p is not an array: ${p}`)
+        this.ne = ne
+        this.ends = ends
         this.a = a
         this.t = t
-        this.p = [i + 0.5, j + 0.5]
+        this.p = p.slice()
     }
 
     this.step = function() {
@@ -30,11 +31,11 @@ function Partstr(nh, hl, hp, eta, M, N) {
         hp = this.hp
         eta = this.eta
         ends = this.ends
+        ne = this.ne
         xx = this.xx
         ff = this.ff
         p = this.p; a = this.a; t = this.t
         k = partstr_curv(hp, t)
-        ne = ends.length
         partstr_part(nh, hp, p, a, t, /**/ xx)
         partstr_force(ne, ends, n, xx, k, eta,     ff)
         State = { p: p, a: a, t: t }
@@ -46,7 +47,8 @@ function Partstr(nh, hl, hp, eta, M, N) {
     }
 }
 
-var argv, nh, hl, hp, eta, a, t, file, u, M, N, ends, partstr, i, j, ne, n
+var argv, nh, hl, hp, eta, a, t, file, u, M, N
+var ends, p, end, partstr, i, j, ne, n
 
 argv = process.argv
 
@@ -67,13 +69,16 @@ matrix_halo_zero(M, N, hl, u)
 ends = matrix_new(M, N)
 partstr_vof_ends(M, N, u, /**/ ends)
 
-partstr = new Partstr(nh, hl, hp, eta, M, N)
+partstr = new Partstr(nh, hl, hp, eta)
+
 i = 2; j = 4
-partstr.new(ends, i, j, a, t)
+end = partstr_cell_ends(M, N, i, j, ends)
+ne = end.length
+p = [i + 0.5, j + 0.5]
+partstr.start(ne, end, a, t, p)
 for (i = 0; i < 10000; i++)
     partstr.step()
 
-ne = partstr.ends.length
 partstr_cell_ends_gnuplot_write(process.stdout, ne, partstr.ends)
 process.stdout.write("\n\n")
 partstr_force_write(process.stdout, n, partstr.xx, partstr.ff)
