1de622cece40434803313271d7845dbb18af8e04
Sergey Litvinov <slitvinov@gmail.com>
2019-03-16 17:34:01 +0100

    poc07: wip vof

--- poc/09_javascript/example/vof/vof.in.js
+++ poc/09_javascript/example/vof/vof.in.js
@@ -1,12 +1,34 @@
 include(mh.m4)dnl
 #!/usr/bin/env node
 "use strict"
+mh_include(matrix.js)dnl
 mh_include(vof.js)dnl
 
-function f(x, y) {
-    return x > 0
+function circle(x, y, param) {
+    var x0, y0, r
+    x0 = param.x0
+    y0 = param.y0
+    r = param.r
+    x -= x0
+    y -= y0
+    return x*x + y*y < r*r
 }
 
-var vof = new Vof(0.1, 0.1, f)
+var u, lx, ly, dx, dy, M, N, i, j, x0, y0, r
+var vof, param
 
-console.log(vof.cell(1, 1))
+M = N = 5
+
+lx = ly = 1
+
+dx = lx/M
+dy = ly/N
+
+x0 = y0 = 0.5; r = 0.33
+param = {x0: x0, y0: y0, r: r}
+
+vof = new Vof(dx, dy, circle, param)
+u = matrix_new(M, N)
+vof.grid(M, N, u)
+
+matrix_write(process.stdout, M, N, u)
--- poc/09_javascript/lib0/vof/vof.js
+++ poc/09_javascript/lib0/vof/vof.js
@@ -1,4 +1,4 @@
-function vof_trapz(M, N, f, x, y, u, v) {
+function vof_trapz(M, N, f, param, x, y, u, v) {
     var i, j;
     var p, q, dx, dy;
     var s, a, b;
@@ -15,24 +15,34 @@ function vof_trapz(M, N, f, x, y, u, v) {
         for (j = 0; j < N + 1; j++) {
             b = (j == 0 || j == N) ? 1 : 2
             q = y + j*dy
-            s += a * b * f(p, q)
+            s += a * b * f(p, q, param)
         }
     }
     return s/(4*N*M)
 }
 
 
-function Vof(dx, dy, f) {
+function Vof(dx, dy, f, param) {
     if (typeof f !== "function")
         throw new Error("f should be a function: " + f)
     this.dx = dx
     this.dy = dy
     this.f  = f
+    this.param = param
     this.cell = function(i, j) {
-        var M = 10, N = 10
+        var M = 20, N = 20
         var x, y, u, v
         x = this.dx*i; y = this.dy*j
         u = this.dx*(i + 1); v = this.dy*(j + 1)
-        return vof_trapz(M, N, this.f, x, y, u, v)
+        return vof_trapz(M, N, this.f, this.param, x, y, u, v)
+    }
+
+    this.grid = function(M, N, /**/ a) {
+        var i, j
+        if (!Array.isArray(a))
+            throw new Error("a is not an array: " + a)
+        for (i = 0; i < M; i++)
+            for (j = 0; j < N; j++)
+                a[i][j] = this.cell(i, j)
     }
 }
