80c504f6cead4b0f7a1c4d602864c12980c6ded8
Petr Karnakov <pkarnakov@gmail.com>
2019-03-16 22:01:37 +0100

    overlap: fix GetSphereOverlap, condition for out

--- src/overlap/over.cpp
+++ src/overlap/over.cpp
@@ -24,8 +24,9 @@ double GetSphereOverlap(const GVect<double, 3>& x, const GVect<double, 3>& h,
     v[1] = x[1] + v[1] * hh[1];
     v[2] = x[2] + v[2] * hh[2];
     bool in = GVect<double, 3>(v[0], v[1], v[2]).sqrdist(c) < sqr(r);
+    bool out = GVect<double, 3>(v[0], v[1], v[2]).sqrdist(c) > sqr(r + h.max());
     ai = ai && in;
-    ao = ao && !in;
+    ao = ao && out;
   }
 
   if (ai) {
