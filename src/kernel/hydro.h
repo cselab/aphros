f7b2e86190205d181ee719307439e12efbcab2cd
Petr Karnakov <pkarnakov@gmail.com>
2019-04-09 01:28:37 +0200

    hydro: remove contangk from CalcMixture

--- src/kernel/hydro.h
+++ src/kernel/hydro.h
@@ -992,60 +992,10 @@ void Hydro<M>::CalcMixture(const FieldCell<Scal>& fc_vf0) {
           std::cout << s.str() << std::endl;
         }
         // zero on boundaries
+        // TODO: test with bubble jump
         for (auto it : mf_velcond_) {
           IdxFace f = it.GetIdx();
           ff_st[f] = 0.;
-        }
-        // contact angle on boundaries
-        // angle between normal to boundary and normal to interface
-        //Scal ang = var.Double["contang"] * pi / 180.;
-        // TODO: controller for contact angle
-        Scal k = var.Double["contangk"];
-        // apply correction to curvature on boundaries
-        for (auto it : mf_velcond_) {
-          IdxFace fb = it.GetIdx();
-          auto nci = it.GetValue()->GetNci();
-          IdxCell c = m.GetNeighbourCell(fb, nci);
-          const Scal th = 1e-2;
-          // TODO: move th to conf
-          if (ast[c] > th && ast[c] < 1. - th) { 
-            for (auto q : m.Nci(c)) {
-              auto f = m.GetNeighbourFace(c, q);
-              ff_st[f] *= k;
-            }
-            /*
-            // outer normal to interface (if vf=1 inside bubble)
-            Vect n = -gc[c]; 
-            n /= n.norm();
-            // outer normal to boundary
-            Vect nb = m.GetNormal(fb) * (nci == 1 ? 1. : -1.); 
-            // vector along boundary
-            Vect a = n - nb * nb.dot(n);
-            a /= a.norm();
-            // vector with angle ang to boundary normal
-            Vect ns = nb * std::cos(ang) + a * std::sin(ang);
-            // surface tension force
-            Vect s = ns * (-fck[c] * k * std::abs(gc[c].dot(ns)));
-            // set to all upwind neighbour faces
-            if(0)
-            std::cout 
-              << " nb=" << nb 
-              << " x=" << m.GetCenter(c)
-              << " s=" << s
-              << " gc=" << gc[c]
-              << std::endl;
-            //s = fck[c] * k * gc[c];
-            for (auto q : m.Nci(c)) {
-              auto f = m.GetNeighbourFace(c, q);
-              //ff_st[f] = ffk_[f] * k * gf[f].dot(m.GetNormal(f));
-              ff_st[f] *= k;
-              if ((m.GetCenter(f) - m.GetCenter(c)).dot(s) < 0. ||
-                  m.GetNormal(f) == m.GetNormal(fb)) {
-                ff_st[f] = s.dot(m.GetNormal(f));
-              }
-            }
-            */
-          }
         }
 
         // Surface tension decay between x0 and x1 
