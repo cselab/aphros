8dbb9659626217b3065e611bb0976a4d58684027
Petr Karnakov <kpetr@ethz.ch>
2019-04-30 11:56:15 +0200

    convdiffv: add GetDiag, GetConst

--- src/solver/convdiffv.h
+++ src/solver/convdiffv.h
@@ -42,9 +42,9 @@ class ConvDiffVect : public UnsteadyIterativeSolver {
   // u: velocity at previous iteration
   virtual const FieldCell<Expr>& GetVelocityEquations(size_t d) const = 0;
   // Returns the diagonal coefficient of the equation in direction d
-  //virtual const FieldCell<Scal> GetDiag(size_t d) const = 0;
+  virtual FieldCell<Scal> GetDiag(size_t d) const = 0;
   // Returns the constant term of the equation in direction d
-  //virtual const FieldCell<Scal> GetConst(size_t d) const = 0;
+  virtual FieldCell<Scal> GetConst(size_t d) const = 0;
   // Assembles linear system
   // fcw: current velocity
   // ffv: volume flux
