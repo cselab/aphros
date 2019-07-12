ca6acaf38a93fa45c2e5509b251fa8c1ad2aa7aa
Petr Karnakov <kpetr@ethz.ch>
2019-07-12 11:14:50 +0300

    test/util: add good2 for height

--- src/test/util/main.cpp
+++ src/test/util/main.cpp
@@ -11,6 +11,7 @@
 #include <random>
 
 #include "util/height.h"
+#include "debug/isnan.h"
 
 using Scal = double;
 using U = UHeight<Scal>;
@@ -25,13 +26,14 @@ std::ostream& operator<<(std::ostream& o, const std::vector<T>& v) {
   return o;
 }
 
-void TestHeight() {
+void TestGood() {
+  std::cout << "UHeight::Good(v,n)" << std::endl;
   auto t = [](const std::vector<Scal>& v, Scal n, bool ref) {
-    auto b = (U::Good(v, n) ? "true" : "false");
+    auto b = (!IsNan(U::Good(v, n)) ? "true" : "false");
     auto br = (ref ? "true" : "false");
     auto eq = (b == br ? "==" : "!=");
     std::cout << v << " n=" << n;
-    std::cout << " " << b << eq << br << std::endl;
+    std::cout << " , " << b << eq << br << std::endl;
     assert(b == br);
   };
   t({0, 0, 0, 0.5, 1, 1, 1}, -1, true);
@@ -49,10 +51,40 @@ void TestHeight() {
   t({0, 0, 0, 0.5, 1, 0.5, 0}, -1, true);
   t({1, 1, 1, 0.5, 0.8, 0, 0}, 1, true);
   t({1,1,0.42,0,0,0,0}, 1, true);
+}
+
+void TestGood2() {
+  std::cout << "UHeight::Good(v)" << std::endl;
+  auto t = [](const std::vector<Scal>& v, Scal ref) {
+    Scal r = U::Good(v);
+    bool cl = (std::abs(r - ref) < 1e-6);
+    if (IsNan(r)) {
+      cl = IsNan(ref);
+    }
+    auto eq = (cl ? "==" : "!=");
+    std::cout << v;
+    std::cout << " , " << r << eq << ref << std::endl;
+    assert(cl);
+  };
+  Scal nan = GetNan<Scal>();
+  t({0, 0, 0, 0.5, 1, 1, 1}, 0);
+  t({1, 1, 1, 0.5, 0, 0, 0}, 0);
+  t({1, 0.5, 0.5, 0.5, 0.5, 0.5, 0}, 0);
+  t({1, 0.7, 0.6, 0.5, 0.4, 0.3, 0}, 0);
+  t({0, 0.7, 0.6, 0.5, 0.4, 0.3, 1}, 0);
+  t({1, 1, 1, 0.1, 0, 0, 1}, -0.4);
+  t({0, 0, 0, 0.1, 1, 1, 1}, 0.4);
+  t({1, 0.5, 0.5, 1, 0.5, 0.5, 0}, nan);
+  t({0, 0.5, 0, 0, 1, 1, 1}, nan);
+  t({0, 0.5, 0, 0.1, 1, 1, 1}, 0.4);
+  t({0, 0, 0, 0.5, 1, 0.5, 0}, 0);
+  t({1, 1, 1, 0.5, 0.8, 0, 0}, 0.8);
+  t({1,1,0.42,0.1,0,0,0}, -0.98);
    
 }
 
 
 int main() {
-  TestHeight();
+  TestGood();
+  TestGood2();
 }
