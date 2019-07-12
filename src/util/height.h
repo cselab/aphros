7493d945df586201673f805d9a30bf39471df837
Petr Karnakov <kpetr@ethz.ch>
2019-07-12 16:13:12 +0300

    util/height: add Good(uu)

--- src/util/height.h
+++ src/util/height.h
@@ -45,4 +45,58 @@ struct UHeight {
     }
     return nan;
   }
+
+  // checks that the heiht in column is well-defined
+  // V: vector type with size() and operator[]
+  // uu: column of volume fractions
+  // Returns:
+  // offset from cell center to the interface
+  template <class V>
+  static Scal Good(const V& uu) {
+    auto I = [](Scal a) { return a > 0 && a < 1; }; // true if interface
+
+    const Scal nan = GetNan<Scal>();
+
+    const size_t si = uu.size();
+    const size_t sih = si / 2;
+
+    size_t i = sih; // center
+    if (!I(uu[i])) { // check center is interface
+      return nan;
+    }
+
+    size_t im = i; // closest pure cell below
+    while (im < si && I(uu[im])) { --im; }
+    if (im >= si) { return nan; }
+
+    size_t ip = i; // closest pure cell above
+    while (ip < si && I(uu[ip])) { ++ip; }
+    if (ip >= si) { return nan; }
+
+    if (uu[ip] != uu[im]) {
+      Scal u = 0.; // sum over column
+      for (size_t i = im; i <= ip; ++i) {
+        u += uu[i];
+      }
+      // add missing pure cells
+      u += im * uu[im] + (si - ip - 1) * uu[ip];
+
+      // assume cell center is x=0,
+      // if uu[im] == 1, u is distance from x=-si*0.5
+      // if uu[im] == 0, u is distance from x=+si*0.5
+
+      if (uu[im] == 0) {
+        u = si - u;
+      }
+
+      // u is distance from x=-(sih+0.5)
+
+      // subtract center
+      u -= si * 0.5;
+
+      // u is distance from x=0
+      return u;
+    }
+    return nan;
+  }
 };
