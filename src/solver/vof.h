f9e2aba18ab30204104905e194943fc8d0295c73
Petr Karnakov <kpetr@ethz.ch>
2019-07-13 11:44:51 +0300

    partstrmesh: set default maxr=0

--- src/solver/vof.h
+++ src/solver/vof.h
@@ -55,7 +55,7 @@ class Vof : public AdvectionSolver<M_> {
     Scal part_dtmax = 10.; 
     Scal part_anglim = 90.; 
     bool part_dn = false;
-    Scal part_maxr = 5;
+    Scal part_maxr = 0;
     using AF = typename solver::PartStrMesh<M>::AF;
     using AR = typename solver::PartStrMesh<M>::AR;
     AF part_attrforce = AF::line;
