7c0a9106f85e6f2b6550120cf76766330f3b8a38
Sergey Litvinov <slitvinov@gmail.com>
2019-08-12 15:49:42 +0200

    cmake: upd hydro.cmake for old cmake

--- src/hydro.cmake
+++ src/hydro.cmake
@@ -2,8 +2,7 @@ include("$ENV{CHPREFIX}/include/client.cmake")
 
 set(T "hydro")
 add_library(${T} INTERFACE)
-target_link_directories(${T} PUBLIC INTERFACE ${CHPREFIX}/lib)
-target_link_libraries(${T} INTERFACE
-distr local cubism hypre report suspender sysinfo utilfluid events timer git
+link_directories(${CHPREFIX}/lib)
+set_property(TARGET ${T} APPEND PROPERTY INTERFACE_LINK_LIBRARIES distr local cubism hypre report suspender sysinfo utilfluid events timer git
 gitgen simple vof normal partstrmesh tvd convdiffvi convdiffi convdiffe solver
 parser vars dumper overlap young hypreext hdf)
