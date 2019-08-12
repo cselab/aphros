fedcf2fb8a49080b2f277c46dee990540e3c55f8
Petr Karnakov <kpetr@ethz.ch>
2019-08-12 13:45:58 +0200

    cmake: split hydro.cmake

--- src/client.cmake
+++ src/client.cmake
@@ -30,12 +30,3 @@ set(HYPRE_DIR ${CHPREFIX})
 add_library(${T} INTERFACE IMPORTED)
 target_include_directories(${T} PUBLIC INTERFACE ${HYPRE_DIR}/include)
 target_link_libraries(${T} INTERFACE -L${HYPRE_DIR}/lib -lHYPRE -lm)
-
-# hydro
-set(T "hydro")
-add_library(${T} INTERFACE)
-target_link_directories(${T} PUBLIC INTERFACE ${CHPREFIX}/lib)
-target_link_libraries(${T} INTERFACE
-distr local cubism hypre report suspender sysinfo utilfluid events timer git
-gitgen simple vof normal partstrmesh tvd convdiffvi convdiffi convdiffe solver
-parser vars dumper overlap young hypreext hdf)
