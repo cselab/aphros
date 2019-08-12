b4a1bb5ffa73dd700c1ce1d2539e3b01c023e262
Petr Karnakov <kpetr@ethz.ch>
2019-08-12 17:10:37 +0200

    cmake: add include_directories to hydro.cmake

--- src/hydro.cmake
+++ src/hydro.cmake
@@ -3,6 +3,7 @@ include("$ENV{CHPREFIX}/include/client.cmake")
 set(T "hydro")
 add_library(${T} INTERFACE)
 link_directories(${CHPREFIX}/lib)
+include_directories(${CHPREFIX}/include/hydro)
 set_property(TARGET ${T} APPEND PROPERTY INTERFACE_LINK_LIBRARIES distr local cubism hypre report suspender sysinfo utilfluid events timer git
 gitgen simple vof normal partstrmesh tvd convdiffvi convdiffi convdiffe solver
 parser vars dumper overlap young hypreext hdf)
