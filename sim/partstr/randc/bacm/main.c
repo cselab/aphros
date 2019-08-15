0e7ae47885c4cfa04ba2d0f8d8d80b9f45bf93ff
Sergey Litvinov <slitvinov@gmail.com>
2019-08-15 17:27:34 +0200

    ba: wip shape

--- sim/partstr/randc/bacm/main.c
+++ sim/partstr/randc/bacm/main.c
@@ -10,6 +10,7 @@
 #include <mpi.h>
 
 #define ONROOT int rank; MPI_Comm_rank(MPI_COMM_WORLD, &rank); if (rank == 0)
+#define MAX_SIZE (1024)
 
 typedef struct Shape Shape;
 struct Shape {
@@ -18,29 +19,77 @@ struct Shape {
   double u, v, w;
   int type;
 };
-struct Shape b;
+enum {SHAPE_S, SHAPE_C};
 
-enum {SHAPE_S, SHAPE_C, SHAPE_E};
 int nxexp;
 int argnx;
-
-void CreateField(Shape b, scalar c) {
+void CreateFieldS(Shape b, scalar c) {
+  foreach() {
+    double h = Delta;
+    c[] = vof_3d((b.x - x)/h , (b.y - y)/h, (b.z - z)/h, b.rx/h);
+  }
+}
+int GoodS(Shape b, double x, double y, double z, double h) {
+  return 1;
+}
+void CreateFieldC(Shape b, scalar c) {
   foreach() {
     double h = Delta;
     c[] = vof_cylinder((b.x - x)/h , (b.y - y)/h, (b.z - z)/h,
                        b.rx/h, b.u, b.v, b.w);
   }
 }
-
-int Inside(Shape b, double x, double y, double z, double h) {
+int GoodC(Shape b, double x, double y, double z, double h) {
   return sq(x -  b.x) + sq(y -  b.y) + sq(z -  b.z) < sq(b.rx + h);
 }
+void (*CreateFieldArray[])(Shape, scalar) = {CreateFieldS, CreateFieldC};
+int (*GoodArray[])(Shape, double, double, double, double) = {GoodS, GoodC};
+void CreateField(Shape b, scalar c) {
+  return CreateFieldArray[b.type](b, c);
+}
+int Good(Shape b, double x, double y, double z, double h) {
+  return GoodArray[b.type](b, x, y, z, h);
+}
 
-int Read(Shape b, FILE *f) {
-  fscanf(f, "%lf %lf %lf %lf", &b.x, &b.y, &b.z, &b.rx);
+Shape Read(FILE *f) {
+  char t;
+  Shape b;
+  int n;
+  char ss[MAX_SIZE], *s = ss;
+  fgets(s, MAX_SIZE, f);
+  sscanf(s, "%c", &t);
+  if (('0' <= t && t <= '9') || t == '.' || t == '-' || t == '+')
+    t = 's';
+  else
+    s++;
+  switch (t) {
+  case 's':
+    b.type = SHAPE_S;
+    n = sscanf(s, "%lf %lf %lf %lf", &b.x, &b.y, &b.z, &b.rx);
+    if (n != 4) {
+      fprintf(stderr, "%s:%d: wrong line '%s'\n", __FILE__, __LINE__, ss);
+      exit(2);
+    }      
+    break;
+  case 'c':
+    b.type = SHAPE_C;
+    n = sscanf(s, "%lf %lf %lf %lf %lf %lf %lf",
+               &b.x, &b.y, &b.z, &b.rx, &b.u, &b.v, &b.w);
+    if (n != 7) {
+      fprintf(stderr, "%s:%d: wrong line '%s'\n", __FILE__, __LINE__, ss);
+      exit(2);
+    }
+    break;
+  default:
+    fprintf(stderr, "%s:%d: unknown type '%c' in line '%s'\n",
+            __FILE__, __LINE__, t, s);
+    exit(2);
+  }
+  return b;
 }
 
 int main() {
+  Shape b;
   {
     FILE* q = fopen("nxexp", "r");
     fscanf(q, "%d", &nxexp);
@@ -49,7 +98,7 @@ int main() {
   argnx = (1 << nxexp);
   init_grid(argnx);
   FILE* fb = fopen("b.dat", "r");
-  Read(b, fb);
+  b = Read(fb);
   fclose(fb);
   origin (0.,0.,0.);
   scalar vf[];
@@ -91,7 +140,7 @@ int main() {
     FILE* q = fopen("ok", "w");
     foreach() {
       if (vf[] > 0. && vf[] < 1.) {
-        if (Inside(b, x, y, z, Delta))
+        if (Good(b, x, y, z, Delta))
           fprintf(q, "%.16g\n", k[] * kc);
       }
     }
