9eca48e5ddba2197a321d1404b84264d14c5d442
Petr Karnakov <kpetr@ethz.ch>
2019-08-16 17:49:50 +0200

    ba: add bashape

---
+++ sim/ba/bashape/example/main.c
@@ -0,0 +1,85 @@
+#include <assert.h>
+#include "grid/multigrid3D.h"
+#include "fractions.h"
+#include ".u/bashape.h"
+#define myassert(EX) (void)((EX) || (__assert (#EX, __FILE__, __LINE__),0))
+
+#include <mpi.h>
+
+int nxexp;
+int argnx;
+int main() {
+  Shape b;
+  {
+    FILE* q = fopen("nxexp", "r");
+    fscanf(q, "%d", &nxexp);
+    fclose(q);
+  }
+  argnx = (1 << nxexp);
+  init_grid(argnx);
+  FILE* fb = fopen("b.dat", "r");
+  b = Read(fb);
+  fclose(fb);
+  origin (0.,0.,0.);
+  scalar vf[];
+  CreateField(b, vf);
+  scalar k[];
+#ifdef CURV_PARTSTR
+#ifdef PS_Np
+  kPartstr.Np = PS_Np;
+#endif
+#ifdef PS_Ns
+#if dimension == 3
+  kPartstr.Ns = PS_Ns;
+#endif
+#endif
+#ifdef PS_Hp
+  kPartstr.Hp = PS_Hp;
+#endif
+#ifdef PS_eps
+  kPartstr.eps = PS_eps;
+#endif
+#ifdef PS_itermax
+  kPartstr.itermax = PS_itermax;
+#endif
+#ifdef PS_eta
+  kPartstr.eta = PS_eta;
+#endif
+#ifdef PS_circ
+  kPartstr.circ = PS_circ;
+#endif
+#endif
+  boundary({vf});
+#if dimension == 2
+  double kc = 1.;
+#elif dimension == 3
+  double kc = 0.5;
+#endif
+
+  {
+    FILE* q = fopen("ok", "w");
+    foreach() {
+      if (vf[] > 0. && vf[] < 1.) {
+        if (Good(b, x, y, z, Delta))
+          fprintf(q, "%.16g\n", k[] * kc);
+      }
+    }
+    fclose(q);
+  }
+
+  {
+    FILE* q = fopen("ovf", "w");
+    double s = 0.;
+    foreach() {
+      double h = Delta;
+      s += vf[] * h * h * h;
+    }
+    fprintf(q, "%.16g\n", s);
+    fclose(q);
+  }
+
+  {
+    FILE* q = fopen("u.vtk", "w");
+    fclose(q);
+  }
+}
