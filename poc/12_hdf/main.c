6799358326e3ee36521560f82d0bfe03dfbfeeb5
Sergey Litvinov <slitvinov@gmail.com>
2019-08-18 23:41:38 +0200

    poc12: wip

--- poc/12_hdf/main.c
+++ poc/12_hdf/main.c
@@ -3,13 +3,51 @@
 #include <mpi.h>
 #include <hdf5.h>
 
+double buf[10*10*10];
+
+static hid_t
+h5_fcreate(MPI_Comm comm, const char *path)
+{
+    hid_t plist, file;
+    MPI_Info info;
+
+    plist = H5Pcreate(H5P_FILE_ACCESS);
+    MPI_Info_create(&info);
+    H5Pset_fapl_mpio(plist, comm, info);
+    file = H5Fcreate(path, H5F_ACC_TRUNC, H5P_DEFAULT, plist);
+    H5Pclose(plist);
+    return file;
+}
+
+static herr_t
+h5_dwrite(hid_t file, hid_t memspace, hid_t filespace, double *buf)
+{
+    hid_t plist, dset;
+    herr_t err;
+
+    dset = H5Dcreate(file, "data", H5T_NATIVE_FLOAT, filespace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+    plist = H5Pcreate(H5P_DATASET_XFER);
+    H5Pset_dxpl_mpio(plist, H5FD_MPIO_COLLECTIVE);
+    err = H5Dwrite(dset, H5T_NATIVE_DOUBLE, memspace, filespace, plist, buf);
+    H5Pclose(plist);
+    H5Dclose(dset);
+    return err;
+}
+
 int
 main(int argc, char **argv)
 {
     int status, rank;
-    hid_t fapl_id, file_id;
+    hid_t file, filespace, dset;
+    hid_t memspace;
     MPI_Comm comm;
     char path[] = "o.h5";
+    int xlo, ylo, zlo, xs, ys, zs;
+    xlo = ylo = zlo = 0;
+    xs = ys = zs = 10;
+    hsize_t start[4]  = { zlo, ylo, xlo,   0};
+    hsize_t extent[4] = { zs, ys, xs,      1};
+    hsize_t globalsize[] = {xs, ys, zs,    1};
 
     comm = MPI_COMM_WORLD;
     status = MPI_Init(&argc, &argv);
@@ -29,20 +67,24 @@ main(int argc, char **argv)
         fprintf(stderr, "MPI_Comm_rank failed\n");
         exit(2);
     }
-    if (rank == 0) {
-        fapl_id = H5Pcreate(H5P_FILE_ACCESS);
-        if (fapl_id < 0) {
-            fprintf(stderr, "H5Pcreate failed\n");
-            exit(2);
-        }
-        file_id = H5Fcreate(path, H5F_ACC_TRUNC, H5P_DEFAULT, fapl_id);
-        if (file_id < 0) {
-            fprintf(stderr, "H5Fcreate failed\n");
-            exit(2);
-        }
-        H5Pclose(fapl_id);
+
+    file = h5_fcreate(comm, path);
+    if (file < 0) {
+        fprintf(stderr, "%s : H5Fcreate failed\n", path);
+        exit(2);
     }
 
+    filespace = H5Screate_simple(4, globalsize, NULL);
+    memspace = H5Screate_simple(4, extent, NULL);
+    H5Sselect_hyperslab(filespace, H5S_SELECT_SET, start, NULL, extent, NULL);
+
+    h5_dwrite(file, memspace, filespace, buf);
+
+    H5Sclose(memspace);
+    H5Sclose(filespace);
+    H5Fclose(file);
+
+    MPI_Barrier(comm);
     fprintf(stderr, "rank: %d\n", rank);
     MPI_Finalize();
     return 0;
