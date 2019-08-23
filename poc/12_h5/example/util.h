77fdfdea7932eeaa8453b0ddb853a5e1ac42f936
Sergey Litvinov <slitvinov@gmail.com>
2019-08-23 03:34:08 +0200

    upd util

--- poc/12_h5/example/util.h
+++ poc/12_h5/example/util.h
@@ -6,7 +6,7 @@
 static int dprint(const char *fmt, ...) {
   int r, rank;
   va_list ap;
-  static FILE *f;
+  static FILE *f = NULL;
   char n[4092];
   if (f == NULL) {
       MPI_Comm_rank(MPI_COMM_WORLD, &rank);
