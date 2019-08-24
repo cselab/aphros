20c174f31867f4a4afd8724e6c1050600912adac
Sergey Litvinov <slitvinov@gmail.com>
2019-08-24 23:40:34 +0200

    h5_read_hdf

--- deploy/lib/h5/h5read.h
+++ deploy/lib/h5/h5read.h
@@ -1,1 +1,2 @@
-int h5_read_xmf(const char *path, char *name, double origin[3], double *spacing, int size[3]);
+int h5_read_xmf(const char *path, /**/ char *name, double origin[3], double *spacing, int size[3]);
+int h5_read_hdf(const char *path, /**/ int size[3], double**);
