91e2e87106f4981407dc1c671a84aa51d64e3f42
Sergey Litvinov <slitvinov@gmail.com>
2019-08-25 00:44:35 +0200

    h5: wip dump.c

--- deploy/lib/h5/example/dump.c
+++ deploy/lib/h5/example/dump.c
@@ -8,8 +8,13 @@
 int
 main(int argc, char **argv)
 {
+    enum {X, Y, Z};
+    enum {I = Z, J = Y, K = X};
+
     int status, size[3];
     double *buf;
+    int i, j, k, m;
+
     argv++;
     if (*argv == NULL) {
 	fprintf(stderr, "needs a path without suffix\n");
@@ -18,4 +23,11 @@ main(int argc, char **argv)
 
     h5_silence();
     status = h5_read_hdf(*argv, size, &buf);
+    fprintf(stderr, "size: %d %d %d\n", size[X], size[Y], size[Z]);
+
+    for (m = k = 0; k < size[K]; k++)
+	for (j = 0; j < size[J]; j++)
+	    for (i = 0; i < size[I]; i++)
+		printf("%d %d %d %g\n", i, j, k, buf[m++]);
+
 }
