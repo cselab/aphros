5d8093bf1c4784ffade04a659e578fb8c6e71e90
Sergey Litvinov <slitvinov@gmail.com>
2019-08-26 23:43:39 +0200

    h5: decl h5_serial_hdf

--- deploy/lib/h5/h5serial.h
+++ deploy/lib/h5/h5serial.h
@@ -1,2 +1,3 @@
 int h5_read_xmf(const char *path, /**/ char *name, double origin[3], double *spacing, int size[3]);
 int h5_read_hdf(const char *path, /**/ int size[3], double**);
+int h5_serial_hdf(const char *path, const int size[3], const double*);
