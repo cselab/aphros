d6546d98c7986c030346cda3ec84474fd0f3dfb9
Petr Karnakov <kpetr@ethz.ch>
2019-08-26 19:49:53 +0200

    poc: add 12_advection

---
+++ poc/12_advection/main.cpp
@@ -0,0 +1,311 @@
+#undef NDEBUG
+#include <iostream>
+#include <string>
+#include <mpi.h>
+#include <cassert>
+#include <iomanip>
+#include <fstream>
+#include <functional>
+#include <utility>
+#include <tuple>
+#include <stdexcept>
+
+#include "parse/vars.h"
+#include "kernel/kernelmeshpar.h"
+#include "distr/distrsolver.h"
+
+#include "util/suspender.h"
+#include "geom/vect.h"
+#include "geom/mesh.h"
+#include "solver/solver.h"
+#include "solver/advection.h"
+#include "solver/vof.h"
+#include "parse/vof.h"
+#include "solver/tvd.h"
+#include "parse/tvd.h"
+#include "func/init_vel.h"
+#include "func/init_u.h"
+#include "dump/dumper.h"
+#include "dump/output.h"
+
+#include "dump/dump.h"
+#include "dump/vtk.h"
+
+#include "cmp.h"
+
+template <class T>
+std::ostream& operator<<(std::ostream& o, const std::vector<T>& v) {
+  std::string p = "";
+  for (auto a : v) {
+    o << p << a;
+    p = " ";
+  }
+  return o;
+}
+
+template <class M_>
+class Advection;
+
+template <class M_>
+struct GPar {
+ public:
+  using M = M_;
+  using Scal = typename M::Scal;
+  using Vect = typename M::Vect;
+
+  std::function<void(FieldCell<Scal>&, const M&)> fu0; // init vf
+  std::function<Vect(Vect /*x*/,Scal /*t*/)> fv; // velocity
+
+  using K = Advection<M>;
+  DistrSolver<M, K>* ds;
+};
+
+template <class M_>
+class Advection : public KernelMeshPar<M_, GPar<M_>> {
+ public:
+  using P = KernelMeshPar<M_, GPar<M_>>; // parent
+  using M = M_;
+  using Mesh = M;
+  using Par = typename P::Par;
+  static constexpr size_t dim = M::dim;
+  using Scal = typename M::Scal;
+  using Vect = typename M::Vect;
+  using Sem = typename Mesh::Sem;
+  using AST = solver::Tvd<M>; // advection TVD
+  using ASV = solver::Vof<M>; // advection VOF
+
+  //using P::P; // inherit constructor
+  Advection(Vars& var, const MyBlockInfo& bi, Par& par);
+  void Run() override;
+  void Init(Sem& sem);
+  void Dump(Sem& sem);
+
+ protected:
+  using P::var;
+  using P::par_;
+  using P::m;
+  using P::IsRoot;
+  using P::IsLead;
+
+ private:
+  FieldFace<Scal> ff_flux_;
+  FieldCell<Scal> fc_src_;
+  FieldCell<Scal> fcu_; // volume fraction (used for initial)
+  std::unique_ptr<solver::AdvectionSolver<M>> as_; // advection solver
+  Scal maxvel_; // maximum velocity relative to cell length [1/time]
+                // cfl = dt * maxvel
+  Scal sumu_; // sum of fluid volume
+  FieldCell<Scal> fcnx_, fcny_, fcnz_; // normal to interface (tmp)
+                                             // used for Vof dump
+  Dumper dmf_; // fields
+  Dumper dms_; // statistics
+};
+
+template <class M>
+Advection<M>::Advection(Vars& v, const MyBlockInfo& b, Par& p)
+    : KernelMeshPar<M, Par>(v, b, p)
+    , dmf_(v, "dump_field_"), dms_(v, "dump_stat_") {}
+
+template <class M>
+void Advection<M>::Init(Sem& sem) {
+  if (sem("init-local")) {
+    // initial field for advection
+    fcu_.Reinit(m);
+    par_.fu0(fcu_, m);
+    m.Comm(&fcu_);
+  }
+
+  if (sem("init-create")) {
+    // flux
+    ff_flux_.Reinit(m, 0);
+    int vdim = var.Int["dim"];
+    for (auto f : m.AllFaces()) {
+      Vect x = m.GetCenter(f);
+      if (vdim == 2) {
+        x[2] = 0.;
+      }
+      ff_flux_[f] = par_.fv(x, 0.).dot(m.GetSurface(f));
+    }
+
+    // boundary conditions for advection (empty)
+    MapFace<std::shared_ptr<solver::CondFace>> bc;
+
+    // cell conditions for advection (empty)
+    MapCell<std::shared_ptr<solver::CondCell>> mc_cond;
+
+    // source
+    fc_src_.Reinit(m, 0.);
+
+    std::string as = var.String["advection_solver"];
+    if (as == "tvd") {
+      auto p = std::make_shared<typename AST::Par>();
+      Parse<M>(p.get(), var);
+      as_.reset(new AST(m, fcu_, bc, &ff_flux_, 
+                       &fc_src_, 0., var.Double["dt"], p));
+    } else if (as == "vof") {
+      auto p = std::make_shared<typename ASV::Par>();
+      Parse<M>(p.get(), var);
+      p->dmp = std::unique_ptr<Dumper>(new Dumper(var, "dump_part_"));
+      as_.reset(new ASV(m, fcu_, bc, &ff_flux_, 
+                       &fc_src_, 0., var.Double["dt"], p));
+    } else {
+      throw std::runtime_error("Unknown advection_solver=" + as);
+    }
+  }
+}
+
+
+template <class M, class Vect=typename M::Vect>
+Vect GetCellSize(const M& m) {
+  Vect h; // result
+  IdxCell c0(0);
+  h = m.GetNode(m.GetNeighbourNode(c0, 7)) - 
+      m.GetNode(m.GetNeighbourNode(c0, 0));
+  assert(std::abs(h.prod() - m.GetVolume(c0)) < 1e-10);
+  return h;
+}
+
+template <class M>
+void Advection<M>::Dump(Sem& sem) {
+  // TODO: Suspender: allow change stages between time steps
+  if (sem("dump")) {
+    if (dmf_.Try(var.Double["t"], var.Double["dt"])) {
+      auto& u = const_cast<FieldCell<Scal>&>(as_->GetField());
+      m.Dump(&u, "u");
+      auto& k = const_cast<FieldCell<Scal>&>(as_->GetCurv());
+      m.Dump(&k, "k");
+      if (auto as = dynamic_cast<solver::Vof<M>*>(as_.get())) {
+        auto& a = const_cast<FieldCell<Scal>&>(as->GetAlpha());
+        m.Dump(&a, "a");
+        auto &n = as->GetNormal();
+        m.Dump(&n, 0, "nx");
+        m.Dump(&n, 1, "ny");
+        m.Dump(&n, 2, "nz");
+
+        auto& kh = const_cast<FieldCell<Scal>&>(as->GetCurvH());
+        m.Dump(&kh, "kh");
+        auto& kp = const_cast<FieldCell<Scal>&>(as->GetCurvP());
+        m.Dump(&kp, "kp");
+      }
+
+      if (IsRoot()) {
+        dmf_.Report();
+      }
+    }
+  }
+  if (sem("dumpwrite")) {
+    // Empty stage for DumpWrite
+    // TODO: revise
+  }
+  // Dump reconstructed interface
+  if (auto as = dynamic_cast<solver::Vof<M>*>(as_.get())) {
+    if (sem("dumpsurf")) {
+      if (dmf_.Try(var.Double["t"], var.Double["dt"])) {
+        if (IsLead()) {
+          auto u = par_.ds->GetField(0);
+          auto k = par_.ds->GetField(1);
+          auto a = par_.ds->GetField(2);
+          auto nx = par_.ds->GetField(3);
+          auto ny = par_.ds->GetField(4);
+          auto nz = par_.ds->GetField(5);
+        }
+      }
+    }
+  }
+}
+
+template <class M>
+void Advection<M>::Run() {
+  auto sem = m.GetSem("advection");
+  Init(sem);
+  Dump(sem);
+  sem.LoopBegin();
+  if (sem("empty")) {
+    //nop // TODO: bugfix loop, empty stage needed
+  }
+  if (sem("checkloop")) {
+    if (as_->GetTime() >= var.Double["tmax"]) {
+      sem.LoopBreak();
+    }
+  }
+  if (sem("vel")) {
+    const Scal t = as_->GetTime();
+    for (auto f : m.AllFaces()) {
+      Vect x = m.GetCenter(f);
+      ff_flux_[f] = par_.fv(x, t).dot(m.GetSurface(f));
+    }
+
+    maxvel_ = 0.;
+    for (auto c : m.Cells()) {
+      for (auto q : m.Nci(c)) {
+        auto f = m.GetNeighbourFace(c, q);
+        maxvel_ = std::max(maxvel_, std::abs(ff_flux_[f]) / m.GetVolume(c));
+      }
+    }
+    m.Reduce(&maxvel_, "max");
+  }
+  if (sem("cfl")) {
+    Scal dt = var.Double["cfl"] / maxvel_;
+    dt = std::min(dt, var.Double["dtmax"]);
+    as_->SetTimeStep(dt);
+    var.Double.Set("dt", as_->GetTimeStep());
+  }
+  if (sem.Nested("start")) {
+    as_->StartStep();
+  }
+  if (sem.Nested("iter")) {
+    as_->MakeIteration();
+  }
+  if (sem.Nested("finish")) {
+    as_->FinishStep();
+  }
+  if (sem.Nested("post")) {
+    as_->PostStep();
+  }
+  if (sem("stat-loc")) {
+    sumu_ = 0.;
+    auto& u = as_->GetField();
+    for (auto c : m.Cells()) {
+      sumu_ += u[c] * m.GetVolume(c);
+    }
+    m.Reduce(&sumu_, "sum");
+  }
+  if (sem("stat")) {
+    if (IsLead()) {
+      ++var.Int["iter"];
+      var.Double["t"] = as_->GetTime();
+    }
+    if (IsRoot()) {
+      Scal t = var.Double["t"];
+      Scal dt = var.Double["dt"];
+      if (dms_.Try(t, dt)) {
+        std::cout 
+            << "t=" << t 
+            << " dt=" << dt 
+            << std::setprecision(16) << " sumu=" << sumu_
+            << std::endl;
+      }
+    }
+  }
+  Dump(sem);
+  sem.LoopEnd();
+}
+
+void Main(MPI_Comm comm, Vars& var) {
+  using M = MeshStructured<double, 3>;
+  using Vect = typename M::Vect;
+
+  using K = Advection<M>;
+  using Par = typename K::Par;
+  Par par;
+  par.fu0 = CreateInitU<M>(var);
+  par.fv = CreateInitVel<Vect>(var);
+
+  DistrSolver<M, K> ds(comm, var, par);
+  par.ds = &ds;
+  ds.Run();
+}
+
+int main(int argc, const char** argv) {
+  return RunMpi(argc, argv, Main);
+}
---
+++ poc/12_advection/plot.py
@@ -0,0 +1,300 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import glob
+import os
+import re
+
+# Read uniform grid data
+# p: path
+# Format:
+# <Nx> <Ny> <Nz>
+# <u[0,0,0]> <u[0,0,1]> ...
+# Return:
+# array of shape (Nz, Ny, Nx)
+# None if file not found
+def Read(p):
+    if not os.path.isfile(p):
+        return None
+    with open(p) as f:
+        ll = f.readlines()
+        # shape x,y,z
+        s = np.array(ll[0].split(), dtype=int)
+        # shape z,y,x
+        ss = tuple(reversed(s))
+        # data flat
+        u = np.array(ll[1].split(), dtype=float)
+        # data z,y,x
+        u = u.reshape(ss)
+        return u
+
+# u: numpy array (2d or 3d slice)
+def Get2d(u):
+    if u is None:
+        return None
+    s = u.shape
+    if len(s) == 2:
+        return u
+    else:
+        assert len(s) == 3
+        return u[0,:,:].reshape((s[1], s[2]))
+
+def PlotInit():
+    plt.close()
+    fig, ax = plt.subplots(figsize=(5,5))
+    return fig, ax
+
+def PlotSave(fig, ax, po):
+    ax.set_aspect('equal')
+    fig.tight_layout()
+    fig.savefig(po, dpi=300)
+    plt.close()
+
+from matplotlib.colors import LinearSegmentedColormap
+
+# u: 2d numpy array
+# po: output path
+def PlotFieldGray(ax, u, vmin=None, vmax=None):
+    cmap = LinearSegmentedColormap.from_list('mycmap', ['#c8c5bd', '#787672'])
+    ax.imshow(np.flipud(u), extent=(0, 1, 0, 1), interpolation='nearest',
+              vmin=vmin, vmax=vmax, cmap=cmap)
+
+# u: 2d numpy array
+# po: output path
+def PlotField(ax, u, vmin=None, vmax=None):
+    ax.imshow(np.flipud(u), extent=(0, 1, 0, 1), interpolation='nearest',
+              vmin=vmin, vmax=vmax)
+
+# u: 2d numpy array
+# po: output path
+def PlotFieldBwr(ax, u, vmin=None, vmax=None):
+    ax.imshow(np.flipud(u), extent=(0, 1, 0, 1), interpolation='nearest',
+              vmin=vmin, vmax=vmax, cmap=plt.get_cmap("bwr"))
+
+# sx, sy: number of cells
+def PlotGrid(ax, x1, y1):
+    ax.set_xticks(x1)
+    ax.set_yticks(y1)
+    plt.setp(ax.get_xticklabels(), visible=False)
+    plt.setp(ax.get_yticklabels(), visible=False)
+    ax.tick_params(axis='both', which='both', length=0)
+    ax.grid(True, lw=0.5, c='0.', alpha=0.08)
+
+def PlotLines(ax, xa, ya, xb, yb):
+    xa = xa.flatten()
+    ya = ya.flatten()
+    xb = xb.flatten()
+    yb = yb.flatten()
+    xy = np.vstack((xa, xb, ya, yb))
+    xy = xy.T
+    xy = xy.reshape((xa.size * 2, 2))
+    ax.plot(*xy, c='k', alpha=0.9, lw=1)
+
+# assume 0 < nx < ny, a < 0
+def GetLineEnds(nx, ny, a):
+    xl = [(a + 0.5 * ny) / nx, (a + 0.5 * nx) / ny]
+    xr = [(a - 0.5 * ny) / nx, (a - 0.5 * nx) / ny]
+
+    e = []
+    if -0.5 <= xl[0] and xl[0] <= 0.5:
+        e.append([xl[0], -0.5])
+    if -0.5 <= xr[0] and xr[0] <= 0.5:
+        e.append([xr[0], 0.5])
+    if len(e) < 2 and -0.5 <= xl[1] and xl[1] <= 0.5:
+        e.append([-0.5, xl[1]])
+    if len(e) < 2 and -0.5 <= xr[1] and xr[1] <= 0.5:
+        e.append([0.5, xr[1]])
+
+    if len(e) == 1: # if only one point found, set second to the same
+        e.append(e[0])
+
+    return e
+
+
+# xc,yc: cell centers
+# a: line constants
+# nx, ny: unit normals
+# hx, hy: cell size
+# Returns:
+# xa, ya, xb, yb: line end points (a,b) on cell edges
+# Equation of line:
+# (x-xc)/h dot n = a
+def GetLines(xc, yc, a, nx, ny, hx, hy, u):
+    xc = xc.flatten()
+    yc = yc.flatten()
+    nx = nx.flatten()
+    ny = ny.flatten()
+    a = a.flatten()
+    u = u.flatten()
+
+    xa = []
+    ya = []
+    xb = []
+    yb = []
+    for i in range(len(xc)):
+        th = 1e-5
+        if u[i] > th and u[i] < 1. - th:
+            e = GetLineEnds(nx[i] * hx, ny[i] * hy, a[i])
+            if len(e) == 2:
+                xa.append(xc[i] + e[0][0] * hx)
+                ya.append(yc[i] + e[0][1] * hy)
+                xb.append(xc[i] + e[1][0] * hx)
+                yb.append(yc[i] + e[1][1] * hy)
+
+    xa = np.array(xa)
+    ya = np.array(ya)
+    xb = np.array(xb)
+    yb = np.array(yb)
+
+    return xa, ya, xb, yb
+
+# kk: list of fields
+# ll: list of labels
+def CmpCurv(x, y, u, kk, ll, po):
+    global reff, hx
+
+    cx = (x * u).sum() / u.sum()
+    cy = (y * u).sum() / u.sum()
+    th = 1e-3
+    ii = np.where((u > th) & (u < 1. - th))
+    x = x[ii];   y = y[ii]
+    dx = x - cx; dy = y - cy
+    an = np.arctan2(dy, dx)
+    deg = np.degrees(an)
+
+    s = np.argsort(an)
+
+
+    # exact curvature
+    ane = np.linspace(-np.pi, np.pi, 200)
+    dege = np.degrees(ane)
+    cx,cy,cz,rx,ry = np.loadtxt('b.dat')
+    ke = rx*ry / ((ry * np.cos(ane)) ** 2 + (rx * np.sin(ane)) ** 2) ** (3. / 2.)
+
+    #kea = 1. / reff  # average curvature from area
+    kea = ke.mean()
+
+    fig, ax = plt.subplots()
+    #ax.axhline(y=1., c="0.5")
+    ax.plot(dege, ke / kea, label="exact", c="0.5")
+
+    for k,l in zip(kk,ll):
+        if k is not None:
+            print("plot l={:}, median={:}".format(l, np.max(k)))
+            ax.plot(deg[s], k[ii][s] / kea, label=l)
+
+    ax.set_xlabel(r"angle [deg]")
+    ax.set_ylabel(r"normalized curvature")
+    q=180.
+    ax.set_xticks(np.arange(-q, q*1.1, 90.))
+    ax.set_xlim(-q, q)
+    ax.legend()
+    ax.set_ylim(0., 2.)
+    ax.grid()
+    #plt.title("R/h={:.3f}".format(reff / hx))
+    plt.title("rx/h={:.3f}, ry/h={:.3f}".format(rx / hx, ry / hx))
+    fig.tight_layout()
+    fig.savefig(po, dpi=300)
+    plt.close()
+
+# Plot particle strings
+# ax: axes to plot on
+# p: path to csv with columns x,y,z,c (c: cell index)
+# sk: plot every sk string
+# Output:
+# plots particles on ax with color c
+def PlotPart(ax, p, sk=1):
+    d = np.loadtxt(p, skiprows=1, delimiter=',')
+    if d.size == 0:
+        return None
+    x,y,z,c = d.T
+    c = c.astype(int)
+
+
+    # separate particles strings by cell
+    tt = dict()  # lists of indices in x
+    for i in range(len(c)):
+        ci = c[i]
+        if not ci in tt:
+            tt[ci] = []
+        tt[ci].append(i)
+
+    # map cell index to color
+    nc = 16
+    c = (c * (2 ** 31 - 1) % nc).astype(float) / (nc - 1)
+
+    cmap = plt.get_cmap("Set1")
+
+    # plot strings
+    n = 0
+    for i in tt:
+        if i % sk == 0:
+            ti = np.array(tt[i])
+            cl = cmap(c[ti[0]])
+            # connecting lines
+            ax.plot(x[ti], y[ti], c=cl, zorder=10, lw=1, alpha=0.5)
+            # points
+            ax.scatter(x[ti], y[ti], c=cl, s=2, lw=0.3, zorder=11, edgecolor='black')
+        n += 1
+
+
+pre = 'u'
+pp = sorted(glob.glob(pre + "*.dat"))
+#pp = pp[1:2]
+for p in pp:
+    suf = re.findall(pre + "(.*)", p)[0]
+    print("p={:}, suf={:}".format(p, suf))
+
+    u = Get2d(Read(p))
+
+    [sx, sy] = u.shape # cells
+
+    # check if gerris (odd sx)
+    ge = (sx % 2 == 1)
+
+    if ge:
+        sx -= 1
+        sy -= 1
+
+    hx = 1. / sx
+    hy = 1. / sy
+
+    # equivalent radius
+    reff = ((u.sum() * hx * hy) / np.pi) ** 0.5
+    print("reff={:}".format(reff))
+
+    # nodes, 1 means 1d
+    xn1 = np.arange(sx + 1) * hx
+    yn1 = np.arange(sy + 1) * hy
+    # cell centers
+    x1 = (0.5 + np.arange(sx)) * hx
+    y1 = (0.5 + np.arange(sy)) * hy
+    x, y = np.meshgrid(x1, y1)
+
+    a = Get2d(Read('a' + suf)) # alpha
+    nx = Get2d(Read('nx' + suf)) # normal
+    ny = Get2d(Read('ny' + suf))
+
+    # volume fraction u
+    po = os.path.splitext(p)[0] + ".pdf"
+    print(po)
+    fig, ax = PlotInit()
+    PlotGrid(ax, xn1, yn1)
+    vmax = abs(u).max()
+    PlotFieldGray(ax, u, vmin=0., vmax=1.)
+    #PlotField(ax, np.clip(u, 0., 1.))
+    if all([e is not None for e in [a, nx, ny]]):
+        l = GetLines(x, y, a, nx, ny, hx, hy, u)
+        PlotLines(ax, *l)
+    # plot partilces
+    n = re.findall("_(\d*)\.", suf)
+    if n:
+        pa = "partit.{:}.csv".format(n[0])
+        if os.path.isfile(pa):
+            print(pa)
+            PlotPart(ax, pa, sk=5)
+    # save
+    PlotSave(fig, ax, po)
