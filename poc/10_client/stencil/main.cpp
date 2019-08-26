c35246a983d5ede38b0d39eabe87c6f73d57870f
Petr Karnakov <kpetr@ethz.ch>
2019-08-26 13:09:23 +0200

    poc10: add stencil

---
+++ poc/10_client/stencil/main.cpp
@@ -0,0 +1,47 @@
+#undef NDEBUG
+#include "geom/mesh.h"
+
+const int dim = 3, edim = 2;
+using MIdx = GMIdx<dim>;
+using Dir = GDir<dim>;
+using Scal = double;
+using Vect = GVect<Scal, dim>;
+using M = MeshStructured<Scal, dim>;
+using std::cout;
+
+int main() {
+  enum {X, Y, Z};
+  Rect<Vect> dom(Vect(0), Vect(1));
+
+  MIdx b(0); // mesh origin
+  MIdx s(5); // mesh size
+  int hl = 2;
+  M m = InitUniformMesh<M>(dom, b, s, hl, true, true, s, 0);
+  FieldCell<Scal> fcu(m, 0);
+  Vect x;
+  MIdx w;
+
+  for (auto c : m.Cells()) {
+      x = m.GetCenter(c);
+      fcu[c] = x[X]*x[Y];
+  }
+
+  auto& bc = m.GetIndexCells();
+
+  const int sw = 1;   // stencil halfwidth
+  // block of offsets
+  GBlock<IdxCell, dim> bo(MIdx(-sw), MIdx(sw * 2 + 1)); 
+
+  MIdx wc = s / 2;
+  std::cout << "neighbors of cell wc=" << wc << std::endl;
+  for (auto c : m.Cells()) {
+    MIdx w = bc.GetMIdx(c);
+    if (w == wc) {
+      for (MIdx wo : bo) {
+        MIdx wn = w + wo;
+        IdxCell cn = bc.GetIdx(wn);
+        std::cout << "wn=" << wn << " u[wn]=" << fcu[cn] << std::endl;
+      }
+    }
+  }
+}
