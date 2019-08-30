e1af3dcabca8d5cea0457f131a406e0b41f2837a
Sergey Litvinov <slitvinov@gmail.com>
2019-08-31 00:32:29 +0200

    poc13: add c++ crap to the header

--- poc/13_march/march.h
+++ poc/13_march/march.h
@@ -1,2 +1,10 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int march_cube(double u[8], /**/ int *ntri, double tri[4 * 3 * 3]);
 int march_tetrahedron(double u[8], /**/ int *ntri, double tri[4 * 3 * 3]);
+
+#ifdef __cplusplus
+}
+#endif
