4106cd7e5cce4b4dd218597e7166750449c262e0
Sergey Litvinov <slitvinov@gmail.com>
2019-08-30 19:40:45 +0200

    poc13: upd sphere

--- poc/13_march/example/sphere.c
+++ poc/13_march/example/sphere.c
@@ -1,30 +1,92 @@
 #include <stdio.h>
 #include <march.h>
 
+enum {X, Y, Z};
+static double r = 0.25;
+static double lo = -0.5, hi = 0.5;
+static int m = 100;
+
+static double
+sq(double x)
+{
+    return x*x;
+}
+
+static double
+f(double x, double y, double z)
+{
+    double ans;
+    if (x > 0)
+	ans = sq(x) + sq(y) + 2*sq(z) - sq(r);
+    else
+	ans = 2*sq(x) + sq(y) + sq(z) - sq(r);
+    //if (ans < 0)
+    //fprintf(stderr, "ans: %g\n", ans);
+    return ans;
+}
+
+static double O[][3] = {
+    {0, 0, 0},
+    {1, 0, 0},
+    {0, 1, 0},
+    {1, 1, 0},
+    {0, 0, 1},
+    {1, 0, 1},
+    {0, 1, 1},
+    {1, 1, 1},
+};
+
+void
+static
+write(double x, double y, double z, double d, int n, double *tri)
+{
+    int i, j, u, v, w;
+    double a, b, c;
+    static int J = 1;
+    
+    if (n == 0)
+	return;
+    fprintf(stderr, "n: %d\n", n);
+    for (i = j = 0; i < 3*n; i++) {
+	a = d*tri[j++] + x;
+	b = d*tri[j++] + y;
+	c = d*tri[j++] + z;
+	printf("v %g %g %g\n", a, b, c);
+    }
+    for (i = 0; i < n; i++) {
+	u = J++;
+	v = J++;
+	w = J++;
+	printf("f %d %d %d\n", u, v, w);
+    }
+}
+
 int
 main()
 {
     static double tri[3*3*5];
-    int n, i, j;
+    int n, i, j, k, l;
     int u, v, w;
-    double a, b, c;
+    double x, y, z, d;
     //double cube[8] = {-1, -1, -1, -1, 1, 1, 1, 1};
-    double cube[8] = {-1, 0, 0, 0, 0, 0, 0, 1};
-    march_cube(cube, &n, tri);
-    if (n == 0) return 0;
-
+    double cube[8];
+    double *o;
+    
     printf("# File type: ASCII OBJ\n");
-    for (i = j = 0; i < 3*n; i++) {
-	a = tri[j++];
-	b = tri[j++];
-	c = tri[j++];
-	printf("v %g %g %g\n", a, b, c);
-    }
-    for (j = 1, i = 0; i < n; i++) {
-	u = j++;
-	v = j++;
-	w = j++;
-	printf("f %d %d %d\n", u, v, w);
-    }
-    fprintf(stderr, "n: %d\n", n);
+
+    d = (hi - lo)/(m - 1);
+    for (i = 0; i < m; i++)
+	for (j = 0; j < m; j++)
+	    for (k = 0; k < m; k++) {
+		x = lo + d*i;
+		y = lo + d*j;
+		z = lo + d*k;
+		//fprintf(stderr, "%g %g %g\n", x, y, z);
+		for (l = 0; l < 8; l++) {
+		    o = O[l];
+		    cube[l] = f(x + d*o[X], y + d*o[Y], z + d*o[Z]);
+		}
+		march_cube(cube, &n, tri);
+		write(x, y, z, d, n, tri);
+	    }
 }
