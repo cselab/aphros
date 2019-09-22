141c5ca1a68618ba74f1970e7e85f720a9d54287
Petr Karnakov <pkarnakov@gmail.com>
2019-09-22 20:35:59 +0200

    init_u: revise with primlist

--- src/test/primlist/main.cpp
+++ src/test/primlist/main.cpp
@@ -9,6 +9,11 @@
 using Scal = double;
 
 int main(int an, char* av[]) {
+  if (an < 5) {
+    std::cerr << "usage: " << av[0]
+        << " name values keys nrequired" << std::endl;
+    return 1;
+  }
   try {
     std::map<std::string, Scal> r =
         UPrimList<Scal>::Parse(av[1], av[2], av[3], atoi(av[4]));
