a3bbca313a7e6af846c04132ed0ab52097ee4a9c
Sergey Litvinov <slitvinov@gmail.com>
2019-09-27 20:56:00 +0200

    minor

--- deploy/lib/march/march.h
+++ deploy/lib/march/march.h
@@ -4,6 +4,8 @@ extern "C" {
 
 enum { MARCH_NTRI = 48 };
 int march_cube(double u[8], /**/ int *ntri, double *tri);
+int march_cube_location(/**/ int*, int*, double*);
+
 int march_tetrahedron(double u[8], /**/ int *ntri, double *tri);
 
 #ifdef __cplusplus
