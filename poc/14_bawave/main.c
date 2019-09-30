b51bc5fed0d8d847561592385bcad54158b62143
Sergey Litvinov <slitvinov@gmail.com>
2019-09-30 12:17:41 +0200

    poc14: add eta

--- poc/14_bawave/main.c
+++ poc/14_bawave/main.c
@@ -1,13 +1,16 @@
 #include ".u/io/io.h"
-
 #include "navier-stokes/centered.h"
 #include "two-phase.h"
+
+#define DUMPDT (0.01)
+#define TMAX (1)
+#define ak  (0.55)
+#define h_   (0.5)
+#define k_  (2.*pi)
 
 double ls(double x, double y) {
   return y - 0.5;
 }
-#define DUMPDT 0.01
-#define TMAX 1
 
 int main() {
   init_grid(64);
@@ -48,4 +51,17 @@ event out (t += DUMPDT ; t <= TMAX + DUMPDT) {
 
 event statout (i += 10 ; t <= TMAX) {
   fprintf(stderr, "step i=%05d t=%g dt=%g \n", i, t ,dt);
+}
+
+static double eta (double x, double y)
+{
+  double a_ = ak/k_;
+  double eta1 = a_*cos(k_*x);
+  double alpa = 1./tanh(k_*h_);
+  double eta2 = 1./4.*alpa*(3.*sq(alpa) - 1.)*sq(a_)*k_*cos(2.*k_*x);
+  double eta3 = -3./8.*(cube(alpa)*alpa - 
+			3.*sq(alpa) + 3.)*cube(a_)*sq(k_)*cos(k_*x) + 
+    3./64.*(8.*cube(alpa)*cube(alpa) + 
+	    (sq(alpa) - 1.)*(sq(alpa) - 1.))*cube(a_)*sq(k_)*cos(3.*k_*x);
+  return eta1 + ak*eta2 + sq(ak)*eta3;
 }
