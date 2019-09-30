3cb30b3e42dd39276a78cae6a9599bb7df46d3ae
Sergey Litvinov <slitvinov@gmail.com>
2019-09-30 17:59:52 +0200

    poc14: fix wave

--- poc/14_bawave/main.c
+++ poc/14_bawave/main.c
@@ -9,10 +9,11 @@
 #define k_  (2.*pi)
 
 static double wave(double, double);
+static double wave0(double, double);
+
 int main() {
   init_grid(64);
-  origin (-L0/2, -L0/2, -L0/2);
-  periodic (right);
+  origin (0.,0.,0.);
 
   rho1 = 1;
   rho2 = 0.01;
@@ -51,19 +52,6 @@ event statout (i += 10 ; t <= TMAX) {
   fprintf(stderr, "step i=%05d t=%g dt=%g \n", i, t ,dt);
 }
 
-static double eta (double x, double y)
-{
-  double a_ = ak/k_;
-  double eta1 = a_*cos(k_*x);
-  double alpa = 1./tanh(k_*h_);
-  double eta2 = 1./4.*alpa*(3.*sq(alpa) - 1.)*sq(a_)*k_*cos(2.*k_*x);
-  double eta3 = -3./8.*(cube(alpa)*alpa - 
-			3.*sq(alpa) + 3.)*cube(a_)*sq(k_)*cos(k_*x) + 
-    3./64.*(8.*cube(alpa)*cube(alpa) + 
-	    (sq(alpa) - 1.)*(sq(alpa) - 1.))*cube(a_)*sq(k_)*cos(3.*k_*x);
-  return eta1 + ak*eta2 + sq(ak)*eta3;
-}
-
 static double
 wave0(double x, double y)
 {
@@ -81,5 +69,5 @@ wave0(double x, double y)
 static double
 wave(double x, double y)
 {
-    return wave0(x, y);
+    return wave0(x - 0.5, y - 0.5);
 }
