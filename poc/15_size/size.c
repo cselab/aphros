671f7ec9d32ead224311bc2f177e92a02d80f9f6
Sergey Litvinov <slitvinov@gmail.com>
2019-10-05 12:50:33 +0200

    poc15: use hash, add -v

--- poc/15_size/size.c
+++ poc/15_size/size.c
@@ -2,17 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <search.h>
 #include "arg.h"
 
 enum { N = 1024 };
 static double pi = 3.141592653589793;
 static char me[] = "ch.size";
+static int UseCl, WriteVolume;
 
 char *argv0;
 static int nv, nt;
 static float *r;
 static int *t, *t0, *c;
 static float *cl;
+static double *dot;
+static double *cx, *cy, *cz, *volume;
+static int *cnt;
 
 static void
 usg(void)
@@ -61,8 +66,8 @@ tri_volume(float *a, float *b, float *c)
     cz = c[Z];
 
     V = (ax * by - ay * bx) * cz + (az * bx - ax * bz) * cy + (ay * bz -
-                                                               az * by) *
-        cx;
+							       az * by) *
+	cx;
     return V / 6;
 }
 
@@ -78,16 +83,16 @@ line(char *s, FILE * f)
     int n;
 
     if (fgets(s, N, f) == NULL)
-        return 1;
+	return 1;
     n = strlen(s);
     if (n > 0 && s[n - 1] == '\n')
-        s[n - 1] = '\0';
+	s[n - 1] = '\0';
     return 0;
 }
 
 static int
 tri_center(float *a, float *b, float *c, /**/ double *x, double *y,
-           double *z)
+	   double *z)
 {
     enum { X, Y, Z };
 
@@ -131,15 +136,15 @@ scalar(FILE * f, int n, char *name, float *c)
 
     line(s, f);
     if (sscanf(s, "SCALARS %s float", name) != 1) {
-        fprintf(stderr, "%s:%d: expect SCALARS, got '%s'\n", __FILE__,
-                __LINE__, s);
-        exit(2);
+	fprintf(stderr, "%s:%d: expect SCALARS, got '%s'\n", __FILE__,
+		__LINE__, s);
+	exit(2);
     }
     line(s, f);
     if (!eq(s, "LOOKUP_TABLE default")) {
-        fprintf(stderr, "%s:%d: expect LOOKUP_TABLE, got '%s'\n", __FILE__,
-                __LINE__, s);
-        exit(2);
+	fprintf(stderr, "%s:%d: expect LOOKUP_TABLE, got '%s'\n", __FILE__,
+		__LINE__, s);
+	exit(2);
     }
     FREAD(n, c, f);
     return 0;
@@ -166,9 +171,9 @@ get3(struct Mesh *mesh, int t, /**/ float *a, float *b, float *c)
     tri = mesh->t;
     r = mesh->r;
     if (t > nt) {
-        fprintf(stderr, "%s:%d: t=%d > nt=%d\n", __FILE__, __LINE__, t,
-                nt);
-        exit(1);
+	fprintf(stderr, "%s:%d: t=%d > nt=%d\n", __FILE__, __LINE__, t,
+		nt);
+	exit(1);
     }
 
     i = tri[3 * t];
@@ -189,10 +194,10 @@ max_arg(int n, int *a)
     j = 0;
     m = a[j];
     for (i = 0; i < n; i++) {
-        if (a[i] > m) {
-            m = a[i];
-            j = i;
-        }
+	if (a[i] > m) {
+	    m = a[i];
+	    j = i;
+	}
     }
     return j;
 }
@@ -202,7 +207,7 @@ u_ini(int n)
 {
     MALLOC(n, &u_root);
     while (n--)
-        u_root[n] = n;
+	u_root[n] = n;
 }
 
 static void
@@ -215,7 +220,7 @@ static int
 u_find(int v)
 {
     if (v == u_root[v])
-        return v;
+	return v;
     return u_root[v] = u_find(u_root[v]);
 }
 
@@ -225,7 +230,7 @@ u_union(int a, int b)
     a = u_find(a);
     b = u_find(b);
     if (a != b)
-        u_root[b] = a;
+	u_root[b] = a;
 }
 
 static int
@@ -236,12 +241,12 @@ swap(int n, int size, void *p0)
 
     p = p0;
     while (n--) {
-        for (i = 0; i < size / 2; i++) {
-            t = p[i];
-            p[i] = p[size - i - 1];
-            p[size - i - 1] = t;
-        }
-        p += size;
+	for (i = 0; i < size / 2; i++) {
+	    t = p[i];
+	    p[i] = p[size - i - 1];
+	    p[size - i - 1] = t;
+	}
+	p += size;
     }
     return 0;
 }
@@ -253,39 +258,39 @@ read_vtk(void)
     char s[N], name[N];
     int i;
     int *a, *b;
-    
+
     f = stdin;
     if (line(s, f) != 0) {
-        fprintf(stderr, "%s:%d: failt to read\n", __FILE__, __LINE__);
-        exit(2);
+	fprintf(stderr, "%s:%d: failt to read\n", __FILE__, __LINE__);
+	exit(2);
     }
 
     if (!eq(s, "# vtk DataFile Version 2.0")) {
-        fprintf(stderr, "%s:%d: not a vtk file: '%s'\n", __FILE__,
-                __LINE__, s);
-        exit(2);
+	fprintf(stderr, "%s:%d: not a vtk file: '%s'\n", __FILE__,
+		__LINE__, s);
+	exit(2);
     }
 
     line(s, f);
     line(s, f);
     if (!eq(s, "BINARY")) {
-        fprintf(stderr, "%s:%d: expect BINARY, got '%s'\n", __FILE__,
-                __LINE__, s);
-        exit(2);
+	fprintf(stderr, "%s:%d: expect BINARY, got '%s'\n", __FILE__,
+		__LINE__, s);
+	exit(2);
     }
 
     line(s, f);
     if (!eq(s, "DATASET POLYDATA")) {
-        fprintf(stderr, "%s:%d: expect DATASET POLYDATA, got '%s'\n",
-                __FILE__, __LINE__, s);
-        exit(2);
+	fprintf(stderr, "%s:%d: expect DATASET POLYDATA, got '%s'\n",
+		__FILE__, __LINE__, s);
+	exit(2);
     }
 
     line(s, f);
     if (sscanf(s, "POINTS %d float", &nv) != 1) {
-        fprintf(stderr, "%s:%d: expect POINTS, got '%s'\n", __FILE__,
-                __LINE__, s);
-        exit(2);
+	fprintf(stderr, "%s:%d: expect POINTS, got '%s'\n", __FILE__,
+		__LINE__, s);
+	exit(2);
     }
     MALLOC(3 * nv, &r);
     FREAD(3 * nv, r, f);
@@ -293,94 +298,102 @@ read_vtk(void)
 
     while (line(s, f) == 0 && s[0] == '\0');
     if (sscanf(s, "POLYGONS %d %*d", &nt) != 1) {
-        fprintf(stderr, "%s:%d: expect POLYGONS, got '%s'\n", __FILE__,
-                __LINE__, s);
-        exit(2);
+	fprintf(stderr, "%s:%d: expect POLYGONS, got '%s'\n", __FILE__,
+		__LINE__, s);
+	exit(2);
     }
     MALLOC(4 * nt, &t0);
     MALLOC(3 * nt, &t);
     FREAD(4 * nt, t0, f);
     for (i = 0, a = t, b = t0; i < nt; i++) {
-        b++;
-        *a++ = *b++;
-        *a++ = *b++;
-        *a++ = *b++;
+	b++;
+	*a++ = *b++;
+	*a++ = *b++;
+	*a++ = *b++;
     }
     swap(3 * nt, sizeof(*t), t);
     while (line(s, f) == 0 && s[0] == '\0');
     if (sscanf(s, "CELL_DATA %*d") != 0) {
-        fprintf(stderr, "%s:%d: expect CELL_DATA, got '%s'\n", __FILE__,
-                __LINE__, s);
-        exit(2);
+	fprintf(stderr, "%s:%d: expect CELL_DATA, got '%s'\n", __FILE__,
+		__LINE__, s);
+	exit(2);
     }
     MALLOC(nt, &cl);
     for (;;) {
-        scalar(f, nt, name, cl);
-        if (eq(name, "cl"))
-            break;
+	scalar(f, nt, name, cl);
+	if (eq(name, "cl"))
+	    break;
     }
     swap(nt, sizeof(*cl), cl);
     return 0;
 }
 
-int
-main(int argc, char **argv)
+static int
+color(int *pnb)
 {
     int nb, u, v, w;
-    double *cx, *cy, *cz, *dot, *volume, R, V;
+    int *id;
     int i, j, k;
-    int *id, *cnt;    
-    float x[3], y[3], z[3];
     struct Mesh mesh;
-    int UseCl;
-
-    UseCl = 0;
-    ARGBEGIN {
-	case 'c':
-	    UseCl = 1;
-	    break;
-	case 'h':
-	    usg();
-    } ARGEND;
-
-    read_vtk();
+    float x[3], y[3], z[3];
 
     MALLOC(nt, &c);
     MALLOC(nt, &id);
-    u_ini(nv);
-    for (i = 0; i < nt; i++) {
-        u = t[3 * i];
-        v = t[3 * i + 1];
-        w = t[3 * i + 2];
-        u_union(u, v);
-        u_union(v, w);
+
+    if (UseCl) {
+	ENTRY e, *p;
+	char s[42];
+	size_t j;
+	hcreate(nt);
+	for (i = j = 0; i < nt; i++) {
+	    sprintf(s, "%ld", (size_t)cl[i]);
+	    e.key = s;
+	    if (hsearch(e, FIND) == NULL) {
+		e.data = (void*)(j++);
+		hsearch(e, ENTER);
+	    }
+	    p = hsearch(e, FIND);
+	    c[i] = (size_t)(p->data);
+	}
+	hdestroy();
+    } else {
+	u_ini(nv);
+	for (i = 0; i < nt; i++) {
+	    u = t[3 * i];
+	    v = t[3 * i + 1];
+	    w = t[3 * i + 2];
+	    u_union(u, v);
+	    u_union(v, w);
+	}
+	for (i = 0; i < nt; i++) {
+	    u = t[3 * i];
+	    c[i] = u_find(u);
+	}
+	u_fin();
     }
-    for (i = 0; i < nt; i++) {
-        u = t[3 * i];
-        c[i] = u_find(u);
-    }
+
     for (i = 0; i < nt; i++)
-        id[i] = -1;
+	id[i] = -1;
     for (j = i = 0; i < nt; i++) {
-        k = c[i];
-        if (id[k] == -1) {
-            id[k] = j++;
-        }
-        c[i] = id[k];
+	k = c[i];
+	if (id[k] == -1) {
+	    id[k] = j++;
+	}
+	c[i] = id[k];
     }
     nb = j;
     MALLOC(nb, &cnt);
     for (i = 0; i < nb; i++)
-        cnt[i] = 0;
+	cnt[i] = 0;
     for (i = 0; i < nt; i++)
-        cnt[c[i]]++;
+	cnt[c[i]]++;
 
     k = max_arg(nb, cnt);       /* water = 0 */
     for (i = 0; i < nt; i++) {
-        if (c[i] == k)
-            c[i] = 0;
-        else if (c[i] == 0)
-            c[i] = k;
+	if (c[i] == k)
+	    c[i] = 0;
+	else if (c[i] == 0)
+	    c[i] = k;
     }
 
     mesh.r = r;
@@ -392,59 +405,93 @@ main(int argc, char **argv)
     MALLOC(nb, &cz);
     MALLOC(nb, &dot);
     for (i = 0; i < nb; i++)
-        cx[i] = cy[i] = cz[i] = dot[i] = 0;
+	cx[i] = cy[i] = cz[i] = dot[i] = 0;
     for (i = 0; i < nt; i++) {
-        k = c[i];
-        get3(&mesh, i, x, y, z);
-        tri_center(x, y, z, &cx[k], &cy[k], &cz[k]);
+	k = c[i];
+	get3(&mesh, i, x, y, z);
+	tri_center(x, y, z, &cx[k], &cy[k], &cz[k]);
     }
     for (i = 0; i < nb; i++) {
-        cx[i] /= cnt[i];
-        cy[i] /= cnt[i];
-        cz[i] /= cnt[i];
+	cx[i] /= cnt[i];
+	cy[i] /= cnt[i];
+	cz[i] /= cnt[i];
     }
     for (i = 0; i < nt; i++) {
-        k = c[i];
-        get3(&mesh, i, x, y, z);
-        dot[k] += tri_dot(x, y, z, cx[k], cy[k], cz[k]);
+	k = c[i];
+	get3(&mesh, i, x, y, z);
+	dot[k] += tri_dot(x, y, z, cx[k], cy[k], cz[k]);
     }
-
     for (i = 0; i < nt; i++) {
-        k = c[i];
-        if (k != 0 && dot[k] > 0)
-            c[i] = 0;
+	k = c[i];
+	if (k != 0 && dot[k] > 0)
+	    c[i] = 0;
     }
-
     for (i = 0; i < nt; i++)
-        if ((long) cl[i] == -1)
-            c[i] = -1;
+	if ((long) cl[i] == -1)
+	    c[i] = -1;
+    free(id);
+    *pnb = nb;
+    return 0;
+}
 
+int
+main(int argc, char **argv)
+{
+    double R, V;
+    int i, j, k, nb;
+    struct Mesh mesh;
+    float x[3], y[3], z[3];
+
+    UseCl = WriteVolume = 0;
+    ARGBEGIN {
+	case 'c':
+	    UseCl = 1;
+	    break;
+	case 'v':
+	    WriteVolume = 1;
+	    break;
+	case 'h':
+	    usg();
+    } ARGEND;
+
+    read_vtk();
+    color(&nb);
     MALLOC(nb, &volume);
+    mesh.r = r;
+    mesh.t = t;
+    mesh.nt = nt;
+    mesh.nv = nv;
     for (i = 0; i < nb; i++)
-        volume[i] = 0;
-
+	volume[i] = 0;
     for (i = 0; i < nt; i++) {
-        if (c[i] == -1)
-            continue;
-        k = c[i];
-        get3(&mesh, i, x, y, z);
-        volume[k] += tri_volume(x, y, z);
+	if (c[i] == -1)
+	    continue;
+	k = c[i];
+	get3(&mesh, i, x, y, z);
+	volume[k] += tri_volume(x, y, z);
     }
 
-    printf("x y z r\n");
-    for (i = 0; i < nb; i++) {
-        if ((V = volume[i]) < 0 || dot[i] > 0)
-            continue;
-        R = pow(3 * V / (4 * pi), 1.0 / 3.0);
-        printf("%.16e %.16e %.16e %.16e %d\n",
-               cx[i], cy[i], cz[i], R, cnt[i]);
+    if (WriteVolume) {
+	V = 0;
+	for (i = 0; i < nb; i++) {
+	    if ((volume[i]) < 0 || dot[i] > 0)
+		V += (-volume[i]);
+	}
+	printf("%.16g\n", V);
+    } else {
+	printf("x y z r n\n");
+	for (i = 0; i < nb; i++) {
+	    if ((V = volume[i]) < 0 || dot[i] > 0)
+		continue;
+	    R = pow(3 * V / (4 * pi), 1.0 / 3.0);
+	    printf("%.16e %.16e %.16e %.16e %d\n",
+		   cx[i], cy[i], cz[i], R, cnt[i]);
+	}
     }
 
-    u_fin();
     free(c);
     free(cl);
     free(cnt);
-    free(id);
     free(cx);
     free(cy);
     free(cz);
