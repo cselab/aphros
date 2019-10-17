e54e51ca94384c90a82a9284187e4857b400e516
Petr Karnakov <kpetr@ethz.ch>
2019-10-17 13:59:39 +0200

    test/suspender: split to files

---
+++ src/test/suspender/simple.h
@@ -0,0 +1,36 @@
+namespace simple {
+
+void B (Suspender& s) {
+  Suspender::Sem sem = s.GetSem();
+  if (sem()) {
+    std::cerr << "B1" << std::endl;
+  }
+  if (sem()) {
+    std::cerr << "B2" << std::endl;
+  }
+}
+
+void A (Suspender& s) {
+  Suspender::Sem sem = s.GetSem();
+  if (sem()) {
+    std::cerr << "A1" << std::endl;
+  }
+  if (sem.Nested()) {
+    B(s);
+  }
+  if (sem()) {
+    std::cerr << "A2" << std::endl;
+  }
+}
+
+
+void Simple() {
+  Suspender s;
+
+  do {
+    A(s);
+  } while (s.Pending());
+}
+
+} // namespace simple
+
