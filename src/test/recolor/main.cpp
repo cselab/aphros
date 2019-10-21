2b768f3ba106f7ee5b3a5bbcb9374e908a24defd
Petr Karnakov <kpetr@ethz.ch>
2019-10-21 17:12:15 +0200

    test/recolor: fix trackerm call

--- src/test/recolor/main.cpp
+++ src/test/recolor/main.cpp
@@ -12,7 +12,7 @@
 using M = MeshStructured<double, 3>;
 using Scal = typename M::Scal;
 using Vect = typename M::Vect;
-using TR = solver::Trackerm<M>;
+using TRM = solver::Trackerm<M>;
 
 struct State {};
 
@@ -30,27 +30,32 @@ void Run(M& m, State&, Vars& var) {
   struct {
     FieldCell<Scal> fcu;
     FieldCell<Scal> fccl;
+    FieldCell<Scal> fcclm;
     solver::UVof<M> uvof;
     MapFace<std::shared_ptr<solver::CondFace>> mfc;
-    std::unique_ptr<TR> tr;
+    std::unique_ptr<TRM> trm;
     FieldCell<Scal> fcim;
   }* ctx(sem);
+
   auto& fcu = ctx->fcu;
   auto& fccl = ctx->fccl;
+  auto& fcclm = ctx->fcclm;
   auto& uvof = ctx->uvof;
   auto& mfc = ctx->mfc;
   constexpr Scal kClNone = -1;
   GRange<size_t> layers(1);
+
   auto Recolor = [&]() {
+    if (sem.Nested()) {
+      ctx->trm->Update(&fccl, &fcclm);
+    }
     if (sem.Nested()) {
       const bool unionfind = true;
       const bool reduce = true;
       const bool grid = true;
       uvof.Recolor(layers, &fcu, &fccl, -1, Vect(0), 1e10, mfc, true, true,
           unionfind, reduce, grid, m);
-    }
-    if (sem.Nested()) {
-      ctx->tr->Update(&fccl);
+      fcclm = fccl;
     }
   };
 
@@ -63,18 +68,18 @@ void Run(M& m, State&, Vars& var) {
     for (auto c : m.Cells()) {
       fccl[c] = (fcu[c] == 0 ? kClNone : 1);
     }
+    fcclm = fccl;
     m.Comm(&fcu);
     m.Comm(&fccl);
-    ctx->tr = std::unique_ptr<TR>(new TR(m, layers));
+    ctx->trm = std::unique_ptr<TRM>(new TRM(m, layers));
   }
   Recolor();
   for (size_t i = 0; i < 5; ++i) {
     if (sem()) {
       m.Dump(&fcu, "u");
       m.Dump(&fccl, "cl");
-      auto& im = *ctx->tr->GetImage()[0];
       for (auto c : m.Cells()) {
-        ctx->fcim[c] = TR::Unpack(im[c])[1];
+        ctx->fcim[c] = ctx->trm->GetImage(0, c)[1];
       }
       m.Dump(&ctx->fcim, "im");
     }
