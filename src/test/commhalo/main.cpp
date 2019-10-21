1a3ba2a78738767658aef5b7e23fbfff7326554d
Petr Karnakov <kpetr@ethz.ch>
2019-10-21 15:17:32 +0200

    test/commhalo: add timing

--- src/test/commhalo/main.cpp
+++ src/test/commhalo/main.cpp
@@ -18,7 +18,7 @@ void Run(M& m, State& s, Vars&) {
   auto& out = s.out;
   auto& ic = m.GetIndexCells();
   auto& bc = m.GetAllBlockCells();
-  if (sem()) {
+  if (sem("local")) {
     out.open("o_" + std::to_string(m.GetId()) + ".log");
     fc.Reinit(m, -1);  // allocate memory for field fc
     out << "before id=" << m.GetId() << std::endl;
@@ -32,7 +32,7 @@ void Run(M& m, State& s, Vars&) {
     out << std::endl;
     m.Comm(&fc);      // exchange halo cells
   }
-  if (sem()) {
+  if (sem("comm")) {
     out << "after id=" << m.GetId() << std::endl;
     for (auto w : bc) {
       IdxCell c = ic.GetIdx(w);
@@ -40,23 +40,30 @@ void Run(M& m, State& s, Vars&) {
     }
     out << std::endl;
   }
+  if (sem()) {}
 }
 
 int main(int argc, const char** argv) {
   std::string conf = R"EOF(
+# ranks
 set int px 2
 set int py 1
 set int pz 1
 
+# blocks per rank
 set int bx 2
 set int by 1
 set int bz 1
 
+# cells per block
 set int bsx 8
 set int bsy 8
 set int bsz 1
 
 set int hl 1
+
+set int verbose_time 1
+set int verbose_stages 1
 )EOF";
 
   return RunMpiBasic<M, State>(argc, argv, Run, conf);
