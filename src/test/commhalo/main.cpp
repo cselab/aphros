080efd16e679e2d455b3cb9f8ba48e2c74153cdb
Petr Karnakov <kpetr@ethz.ch>
2019-10-21 15:13:23 +0200

    test/commhalo: add multi-index

--- src/test/commhalo/main.cpp
+++ src/test/commhalo/main.cpp
@@ -16,22 +16,27 @@ void Run(M& m, State& s, Vars&) {
   auto sem = m.GetSem();
   auto& fc = s.fc;
   auto& out = s.out;
+  auto& ic = m.GetIndexCells();
+  auto& bc = m.GetAllBlockCells();
   if (sem()) {
     out.open("o_" + std::to_string(m.GetId()) + ".log");
     fc.Reinit(m, -1);  // allocate memory for field fc
     out << "before id=" << m.GetId() << std::endl;
-    for (auto c : m.AllCells()) {
+    for (auto c : m.Cells()) { // traverse internal cells
       fc[c] = m.GetId();
-      out << fc[c] << " ";
+    }
+    for (auto w : bc) { // traverse all cells with multi-index
+      IdxCell c = ic.GetIdx(w);
+      out << w << " " << fc[c] << std::endl;
     }
     out << std::endl;
     m.Comm(&fc);      // exchange halo cells
   }
   if (sem()) {
     out << "after id=" << m.GetId() << std::endl;
-    for (auto c : m.AllCells()) {
-      fc[c] = m.GetId();
-      out << fc[c] << " ";
+    for (auto w : bc) {
+      IdxCell c = ic.GetIdx(w);
+      out << w << " " << fc[c] << std::endl;
     }
     out << std::endl;
   }
@@ -50,6 +55,8 @@ set int bz 1
 set int bsx 8
 set int bsy 8
 set int bsz 1
+
+set int hl 1
 )EOF";
 
   return RunMpiBasic<M, State>(argc, argv, Run, conf);
