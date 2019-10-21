a85c23dbc45bd7b00bca9240fa4060bffbaab741
Petr Karnakov <kpetr@ethz.ch>
2019-10-21 15:05:47 +0200

    test/commhalo: output log

--- src/test/commhalo/main.cpp
+++ src/test/commhalo/main.cpp
@@ -1,5 +1,6 @@
 #undef NDEBUG
 #include <iostream>
+#include <fstream>
 
 #include "distr/distrbasic.h"
 
@@ -8,27 +9,31 @@ using Scal = typename M::Scal;
 
 struct State {
   FieldCell<Scal> fc;
+  std::ofstream out;
 };
 
 void Run(M& m, State& s, Vars&) {
   auto sem = m.GetSem();
+  auto& fc = s.fc;
+  auto& out = s.out;
   if (sem()) {
-    fc.Reinit(m, 0);  // allocate memory for field fc
-    std::cout << "before id=" << m.GetId() << std::endl;
-    for (auto c : m.Cells()) {
+    out.open("o_" + std::to_string(m.GetId()) + ".log");
+    fc.Reinit(m, -1);  // allocate memory for field fc
+    out << "before id=" << m.GetId() << std::endl;
+    for (auto c : m.AllCells()) {
       fc[c] = m.GetId();
-      std::cout << fc[c] << " ";
+      out << fc[c] << " ";
     }
-    std::cout << std::endl;
+    out << std::endl;
     m.Comm(&fc);      // exchange halo cells
   }
   if (sem()) {
-    std::cout << "after id=" << m.GetId() << std::endl;
-    for (auto c : m.Cells()) {
+    out << "after id=" << m.GetId() << std::endl;
+    for (auto c : m.AllCells()) {
       fc[c] = m.GetId();
-      std::cout << fc[c] << " ";
+      out << fc[c] << " ";
     }
-    std::cout << std::endl;
+    out << std::endl;
   }
 }
 
@@ -44,7 +49,7 @@ set int bz 1
 
 set int bsx 8
 set int bsy 8
-set int bsz 8
+set int bsz 1
 )EOF";
 
   return RunMpiBasic<M, State>(argc, argv, Run, conf);
