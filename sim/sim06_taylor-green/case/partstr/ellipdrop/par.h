8a73c09f3c11c796c63bed78d37c4e01664bc072
Petr Karnakov <kpetr@ethz.ch>
2019-10-27 22:13:16 +0100

    sim06/cases: add ellipdrop

---
+++ sim/sim06_taylor-green/case/partstr/ellipdrop/par.h
@@ -0,0 +1,28 @@
+#define TMAX (10.0)
+#define DUMPDT (0.1)
+#define SIGMA (0.1)
+#define RHO1 (1.0)
+#define RHO2 (10)
+#define MU1 (0.00125)
+#define MU2 (0.0125)
+#define EXTENT (6.283185307179586)
+
+#define BCX 2.
+#define BCY 2.
+#define BCZ 2.
+#define BR 0.1
+#define BRZ 0.5
+
+#ifndef NX
+#define NX 64
+#endif
+
+//#define MPIDIM dimensions(nx = 4, ny = 4, nz = 4);
+
+#ifndef MPIDIM
+#define MPIDIM 
+#endif
+
+#ifndef NOIO
+#define NOIO 0
+#endif
