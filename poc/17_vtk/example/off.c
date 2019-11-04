2a74344ba00e5c1ddf5ac373ebd5fc4471cc4a4f
Sergey Litvinov <slitvinov@gmail.com>
2019-11-04 12:35:20 +0100

    poc/17_vtk: add argmetnts for vtk/off

--- poc/17_vtk/example/off.c
+++ poc/17_vtk/example/off.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <vtk.h>
+#define	USED(x)		if(x);else{}
+static char me[] = "vtk/off";
+static void
+usg()
+{
+    fprintf(stderr, "%s < VTK > OFF\n", me);
+    exit(1);
+}
 
-static char me[] = "vtk/write";
 int
-main(void)
+main(int argc, char **argv)
 {
   struct VTK *vtk;
+
+  USED(argc);
+  while (*++argv != NULL && argv[0][0] == '-' )
+      switch (argv[0][1]) {
+      case 'h':
+	  usg();
+	  break;
+      default:
+	  fprintf(stderr, "%s: unknown option '%s'\n", me, argv[0]);
+	  exit(1);
+      }
 
   vtk = vtk_read(stdin);
   if (vtk == NULL) {
