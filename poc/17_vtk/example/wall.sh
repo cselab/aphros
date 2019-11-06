d6213d55c7f5f71c40ed2a370bb95238a0453180
Sergey Litvinov <slitvinov@gmail.com>
2019-11-06 10:58:45 +0100

    poc/17_vtk: minor

--- poc/17_vtk/example/wall.sh
+++ poc/17_vtk/example/wall.sh
@@ -7,7 +7,7 @@ trap 'rm $t; exit 1' 1 2 3 4 15
 for i
 do
     echo >&2 "$i"
-    <"$i" | ./wall | ./remove nn l c > $t &&
+    <"$i" ./wall | ./remove nn l c > $t &&
         mv $t "$i"
 done
 rm -f $t
