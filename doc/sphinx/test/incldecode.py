f8e2fc2b08d9d83bce0e0e7e95ccd8ecb43921cb
Sergey Litvinov <slitvinov@gmail.com>
2019-11-19 14:32:19 +0100

    doc/sphinx: minor

--- doc/sphinx/test/incldecode.py
+++ doc/sphinx/test/incldecode.py
@@ -1,4 +1,4 @@
-#/usr/bin/python
+#!/usr/bin/env python
 
 import sys
 import includecode
@@ -6,16 +6,15 @@ import includecode
 me = "test/includecode.py"
 def usg():
     sys.stderr.write("%s [-s] name file\n" % me)
+    sys.stderr.write("%s Simple /u/hydro/src/solver/simple.h\n" % me)
     sys.exit(1)
 
 Get = includecode.GetFunc
 if "-h" in sys.argv:
     usg()
-
 if "-s" in sys.argv:
     Get = includecode.GetStruct
     sys.argv.remove("-s")
-
 name = sys.argv[1]
 filename = sys.argv[2]
 
