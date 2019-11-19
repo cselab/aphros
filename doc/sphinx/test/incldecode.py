ab845b30adb9046e7772fcb7ce67a4f6afb55ba7
Sergey Litvinov <slitvinov@gmail.com>
2019-11-19 14:29:29 +0100

    add a client for incldecode.py

---
+++ doc/sphinx/test/incldecode.py
@@ -0,0 +1,25 @@
+#/usr/bin/python
+
+import sys
+import includecode
+
+me = "test/includecode.py"
+def usg():
+    sys.stderr.write("%s [-s] name file\n" % me)
+    sys.exit(1)
+
+Get = includecode.GetFunc
+if "-h" in sys.argv:
+    usg()
+
+if "-s" in sys.argv:
+    Get = includecode.GetStruct
+    sys.argv.remove("-s")
+
+name = sys.argv[1]
+filename = sys.argv[2]
+
+with open(filename, 'r') as f:
+    includecode.filename = filename
+    f = f.read()
+    print(Get(f, name))
