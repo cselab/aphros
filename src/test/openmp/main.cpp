fbb9999a5aabc28049b0060dcee622c9fe25daef
Petr Karnakov <kpetr@ethz.ch>
2019-11-25 10:53:41 +0100

    distr: add openmp to cubism

--- src/test/openmp/main.cpp
+++ src/test/openmp/main.cpp
@@ -1,5 +1,6 @@
 #undef NDEBUG
 #include <iostream>
+#include <omp.h>
 
 #include "distr/distrbasic.h"
 
@@ -15,65 +16,13 @@ struct State {
 void Run(M& m, State& s, Vars& var) {
   (void) var;
   auto sem = m.GetSem();
-  for (auto i = 0; i < 10; ++i) {
-    if (sem("std")) {
-      s.v = {Scal(m.GetId())};
-      using T = typename M::template OpCatT<Scal>;
-      m.Reduce(std::make_shared<T>(&s.v));
-    }
-  }
-
-  for (auto i = 0; i < 10; ++i) {
-    if (sem("mpi")) {
-      auto comm = MPI_COMM_WORLD;
-      if (m.IsLead()) {
-        auto& r = s.r;
-        r = {0};
-        int s = r.size(); // size local
-        if (m.IsRoot()) {
-          int sc; // size of communicator
-          MPI_Comm_size(comm, &sc);
-
-          std::vector<int> ss(sc); // size of r on all ranks
-
-          MPI_Gather(&s, 1, MPI_INT, 
-                     ss.data(), 1, MPI_INT, 
-                     0, comm);
-
-          int sa = 0; // size all
-          std::vector<int> oo = {0}; // offsets
-          for (auto& q : ss) {
-            sa += q;
-            oo.push_back(oo.back() + q);
-          }
-          oo.pop_back();
-          assert(ss.size() == oo.size());
-
-          std::vector<char> ra(sa); // result all
-
-          MPI_Gatherv(r.data(), r.size(), MPI_CHAR,
-                      ra.data(), ss.data(), oo.data(), MPI_CHAR,
-                      0, comm);
-        } else {
-          MPI_Gather(&s, 1, MPI_INT, nullptr, 0, MPI_INT, 0, comm);
-          MPI_Gatherv(r.data(), r.size(), MPI_CHAR,
-                      nullptr, nullptr, nullptr, MPI_CHAR,
-                      0, comm);
-        }
-      }
-    }
-
-
-
-
-
-  }
   if (sem()) {
-    if (m.IsRoot()) {
-      m.TimerReport("timer.log");
+    volatile double a = 10.;
+    for (size_t i = 0; i < (1 << 28); ++i) {
+      a = std::sqrt(a);
     }
+    std::cout << "thread=" << omp_get_thread_num() << std::endl;
   }
-  if (sem()) {}
 }
 
 int main(int argc, const char** argv) {
@@ -86,9 +35,11 @@ set int bsx 16
 set int bsy 16
 set int bsz 16
 
-set int px 6
-set int py 4
-set int pz 8
+set int px 1
+set int py 1
+set int pz 1
+
+set int loc 0
 )EOF";
 
   return RunMpiBasic<M, State>(argc, argv, Run, conf);
