6b2fd7e72a8303c41d6b5c812e3fa6776cd6143e
Sergey Litvinov <slitvinov@gmail.com>
2019-11-27 20:54:30 +0100

    poc/16_csv: wip

--- poc/16_csv/example/split.c
+++ poc/16_csv/example/split.c
@@ -15,42 +15,22 @@ usg()
 }
 
 static const char *name;
-
-static struct Table *
-file2table(const char *fname)
-{
-  int nr, i;
-  double *field;
-  FILE *file;
+struct Data {
   struct CSV *csv;
-  struct Table *t;
-
-  if ((file = fopen(fname, "r")) == NULL) {
-    fprintf(stderr, "%s: fail to open '%s'\n", me, fname);
-    exit(1);
-  }
-  if ((csv = csv_read(file)) == NULL) {
-    fprintf(stderr, "%s: not a cvs file '%s'\n", me, fname);
-    exit(1);
-  }
-  fclose(file);
-  if ((field = csv_field(csv, name)) == NULL) {
-    fprintf(stderr, "%s: no field '%s' in file '%s'\n", me, name, fname);
-    exit(1);
-  }
-  nr = csv_nr(csv);
-  t = table_ini(9999);
-  for (i = 0; i < nr; i++) {
-    table_put(t, field[i], i);
-  }
-  csv_fin(csv);
-  return t;
-}
+  struct Table *table;
+  double *x;
+  double *y;
+  double *z;
+  double *r;
+};
+static int data_fin(struct Data *);
+static struct Data data_ini(const char *);
 
 int
 main(int argc, char **argv)
 {
-  struct Table *a, *b;
+  struct Data a;
+  struct Data b;
   int x, *y, n, i;
 
   USED(argc);
@@ -77,18 +57,64 @@ main(int argc, char **argv)
     exit(1);
   }
 
-  a = file2table(*argv);
+  a = data_ini(*argv);
   while (*++argv != NULL) {
-      b = file2table(*argv);
-      y = table_array(b);
-      n = table_length(b);
+      b = data_ini(*argv);
+      y = table_array(b.table);
+      n = table_length(b.table);
       for (i = 0; i < 2*n; i += 2) {
-	  if (table_get(a, y[i], &x) == TABLE_EMPY)
+	  if (table_get(a.table, y[i], &x) == TABLE_EMPY)
 	      printf("%s %d\n", *argv, y[i]);
       }
       free(y);
-      table_fin(a);
+      data_fin(&a);
       a = b;
   }
-  table_fin(a);
+  data_fin(&a);
+}
+
+static struct Data
+data_ini(const char *fname)
+{
+  int nr, i;
+  double *field;
+  FILE *file;
+  struct CSV *csv;
+  struct Table *t;
+  struct Data q;
+
+  if ((file = fopen(fname, "r")) == NULL) {
+    fprintf(stderr, "%s: fail to open '%s'\n", me, fname);
+    exit(1);
+  }
+  if ((csv = csv_read(file)) == NULL) {
+    fprintf(stderr, "%s: not a cvs file '%s'\n", me, fname);
+    exit(1);
+  }
+  fclose(file);
+  if ((field = csv_field(csv, name)) == NULL) {
+    fprintf(stderr, "%s: no field '%s' in file '%s'\n", me, name, fname);
+    exit(1);
+  }
+  nr = csv_nr(csv);
+  t = table_ini(9999);
+  for (i = 0; i < nr; i++) {
+    table_put(t, field[i], i);
+  }
+
+  q.x = csv_field(csv, "x");
+  q.y = csv_field(csv, "y");
+  q.z = csv_field(csv, "z");
+  q.r = csv_field(csv, "r");
+  q.table = t;
+  q.csv = csv;
+  
+  return q;
+}
+
+static int
+data_fin(struct Data *q)
+{
+  csv_fin(q->csv);
+  return table_fin(q->table);
 }
