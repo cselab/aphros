3d18e181eb52138ee472a8aca8932795e5c6009b
Fabian Wermelinger <fabianw@mavt.ethz.ch>
2019-11-27 20:26:43 +0100

    util/sysinfo: add utility to check for hyper-threads

--- src/util/sysinfo.cpp
+++ src/util/sysinfo.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <fstream>
+#include <sstream>
 
 #include "sysinfo.h"
 
@@ -26,4 +27,29 @@ size_t GetMem() {
   return GetProcStat("VmRSS") << 10;
 }
 
-} // namespace sysinfo 
+bool HasHyperthreads() {
+  bool has_ht = false;
+  std::ifstream f("/proc/cpuinfo");
+  f >> std::skipws;
+  while (f) {
+    std::string s;
+    f >> s;
+    if (s == "flags") {
+        break;
+    }
+  }
+  std::string line;
+  std::getline(f, line);
+  std::istringstream is(line);
+  while (!is.eof()) {
+      std::string s;
+      is >> s;
+      if (s == "ht") {
+          has_ht = true;
+          break;
+      }
+  }
+  return has_ht;
+}
+
+} // namespace sysinfo
--- src/util/sysinfo.h
+++ src/util/sysinfo.h
@@ -4,5 +4,7 @@ namespace sysinfo {
 
 // Returns resident memory [bytes]
 size_t GetMem();
+// Returns true if Intel Hyperthreads are enabled
+bool HasHyperthreads();
 
 } // namespace sysinfo
