09cddab3fa728a117c0aeafe73e6a81ef8de096a
Petr Karnakov <kpetr@ethz.ch>
2019-11-27 14:00:02 +0100

    hypresub: add GetCmd and GetString

--- src/linear/hypresub.cpp
+++ src/linear/hypresub.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdexcept>
 
 #include "hypresub.h"
 
@@ -7,9 +8,35 @@ struct HypreSub::Imp {
     std::vector<int> v;
   };
   enum class Cmd {
-    construct, destruct, update, solve, get_residual, get_iter};
+    construct, destruct, update, solve, get_residual, get_iter, exit};
 
   static ServerState server;
+
+  static Cmd GetCmd(std::string s) {
+    #define GETCMD(x) if (s == #x) return Cmd::x;
+    GETCMD(construct);
+    GETCMD(destruct);
+    GETCMD(update);
+    GETCMD(solve);
+    GETCMD(get_residual);
+    GETCMD(get_iter);
+    GETCMD(exit);
+    throw std::runtime_error("GetCmd: unknown name '" + s + "'");
+  }
+  static std::string GetString(Cmd cmd) {
+    #define GETSTR(x) if (cmd == Cmd::x) return #x;
+    GETSTR(construct);
+    GETSTR(destruct);
+    GETSTR(update);
+    GETSTR(solve);
+    GETSTR(get_residual);
+    GETSTR(get_iter);
+    GETSTR(exit);
+    throw std::runtime_error("GetString: unknown cmd");
+  }
+  static void Execute(Cmd cmd) {
+    std::cout << GetString(cmd) + " received" << std::endl;
+  }
 };
 
 HypreSub::Imp::ServerState HypreSub::Imp::server;
@@ -24,4 +51,9 @@ void HypreSub::InitServer(MPI_Comm comm, MPI_Comm commsub) {
 
 void HypreSub::RunServer() {
   std::cout << Imp::server.v.size() << std::endl;
+}
+
+void HypreSub::Send(std::string cmd) {
+  std::cout << cmd << std::endl;
+  Imp::Execute(Imp::GetCmd(cmd));
 }
