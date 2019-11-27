5c95f8a1e8d00f9a41321ad34e3d075d22d1c527
Petr Karnakov <kpetr@ethz.ch>
2019-11-27 12:34:04 +0100

    hypresub: add server to cpp

--- src/linear/hypresub.cpp
+++ src/linear/hypresub.cpp
@@ -1,10 +1,20 @@
+#include <iostream>
+
 #include "hypresub.h"
 
 struct HypreSub::ServerState {
+  std::vector<int> v;
 };
+
+HypreSub::ServerState HypreSub::server;
 
 void HypreSub::InitServer(MPI_Comm comm, MPI_Comm commsub) {
   (void) comm;
   (void) commsub;
+
+  std::cout << "init" << std::endl;
+  server.v.resize(5);
 }
-void HypreSub::RunServer() {}
+void HypreSub::RunServer() {
+  std::cout << server.v.size() << std::endl;
+}
