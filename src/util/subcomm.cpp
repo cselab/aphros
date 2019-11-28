05c40011b2a57133413e24fa03d8e19fb33b4ebf
Petr Karnakov <kpetr@ethz.ch>
2019-11-28 11:36:32 +0100

    util/subcomm: add example

--- src/util/subcomm.cpp
+++ src/util/subcomm.cpp
@@ -1,4 +1,11 @@
 #include "subcomm.h"
 #include "sysinfo.h"
 
+// Example for two nodes each with two threads.
+// Ranks:
+// comm_world:  | 0 1 | 2 3 |
+// comm_omp:    | 0 1 | 0 1 |
+// comm_master: | 0 - | 1 - |
+
+
 
