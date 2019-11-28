aad4c5004f720450c023f7a34525846adea77cdb
Petr Karnakov <pkarnakov@gmail.com>
2019-11-28 06:37:40 +0100

    hypresub: add StopServer

--- src/linear/hypre.cpp
+++ src/linear/hypre.cpp
@@ -69,11 +69,6 @@ Hypre::Imp::Imp(MPI_Comm comm, const std::vector<Block>& bb0,
     for (size_t i = 0; i < dim; ++i) {
       n *= b.u[i] - b.l[i] + 1;
     }
-    std::cerr
-        << EV(b.a->size())
-        << EV(n)
-        << EV(st.size())
-        << std::endl;
     assert(b.a->size() == n * st.size());
     assert(b.r->size() == n);
     assert(b.x->size() == n);
