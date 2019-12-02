aafa64376d76db485c5a3d297539390c77e238c1
Sergey Litvinov <slitvinov@gmail.com>
2019-12-02 15:19:02 +0100

    lib/march: march_cube_location2 -> march_cube_location

--- deploy/lib/march/march.h
+++ deploy/lib/march/march.h
@@ -4,9 +4,7 @@ extern "C" {
 
   enum { MARCH_NTRI = 48 };
   int march_cube(double u[8], /**/ int *ntri, double *tri);
-  int march_cube_location( /**/ int *, int *, double *);
-  int march_cube_location2(double u[8], /**/ int *ntri, double *tri,
-                           int *x, int *y, double *a);
+  int march_cube_location(double u[8], /**/ int *ntri, double *tri, int *x, int *y, double *a);
 #ifdef __cplusplus
 }
 #endif
