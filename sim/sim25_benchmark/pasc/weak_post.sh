251884f788f7bc293c560e3cd6cc1ee0a31b7618
Fabian Wermelinger <fabianw@mavt.ethz.ch>
2019-12-08 01:33:06 +0100

    sim25/pasc: single hist_synch.bin file

--- sim/sim25_benchmark/pasc/weak_post.sh
+++ sim/sim25_benchmark/pasc/weak_post.sh
@@ -2,18 +2,18 @@
 cores_per_node=12
 
 # Histogram metrics
-SYNCHID=2
 stats=(
     "hist_distrmesh.bin Run"
     "hist_distrmesh.bin RunKernels(inner)"
     "hist_distrmesh.bin RunKernels(halo)"
-    "hist_synch${SYNCHID}.bin avail_halo"
-    "hist_synch${SYNCHID}.bin avail_inner"
-    "hist_synch${SYNCHID}.bin halo_waitall"
-    "hist_synch${SYNCHID}.bin init_maps"
-    "hist_synch${SYNCHID}.bin pack"
-    "hist_synch${SYNCHID}.bin compress_face"
-    "hist_synch${SYNCHID}.bin halo_decompress"
+    "hist_synch.bin avail_halo"
+    "hist_synch.bin avail_inner"
+    "hist_synch.bin waitall_avail_halo"
+    "hist_synch.bin init_maps"
+    "hist_synch.bin pack"
+    "hist_synch.bin compress"
+    "hist_synch.bin decompress_avail_halo"
+    "hist_synch.bin send_req"
 )
 
 get_fname()
@@ -26,36 +26,36 @@ get_fname()
 
 fheaders()
 {
-    header="# kp\tnodes\tcores\tmean\tsdev\tsum"
-    cwd="$1"
+    local header="# kp\tnodes\tcores\tmean\tsdev\tsum"
+    local cwd="$1"
     for s in "${stats[@]}"; do
         IFS=' ' read -r -a file_stat <<< "${s}"
-        fname=$(get_fname "${file_stat[@]}")
+        local fname=$(get_fname "${file_stat[@]}")
         echo -e ${header} > "${cwd}/$fname"
     done
 }
 
 stats()
 {
-    cwd="$1"
-    root="$(pwd -P)"
+    local cwd="$1"
+    local root="$(pwd -P)"
     for s in "${stats[@]}"; do
         IFS=' ' read -r -a file_stat <<< "${s}"
-        fhist="${file_stat[0]}"
-        stat="${file_stat[1]}"
-        fname="$(get_fname "${file_stat[@]}")"
+        local fhist="${file_stat[0]}"
+        local stat="${file_stat[1]}"
+        local fname="$(get_fname "${file_stat[@]}")"
         cd "${cwd}"
         for n in $(ls -d kp* | sort -t '=' -k2n); do
-            kp=${n##*=}
-            nodes=$((kp * kp * kp))
-            cores=$((nodes * cores_per_node))
+            local kp=${n##*=}
+            local nodes=$((kp * kp * kp))
+            local cores=$((nodes * cores_per_node))
             if [[ ! -f "${n}/${fhist}" ]]; then
                 continue
             fi
             ch.histbin -f "${n}/${fhist}" -rs -sn ${stat} > 000_tmp.dat
-            mean="$(cat 000_tmp.dat | awk '/^\s*mean/{ print $3; exit }')"
-            sdev="$(cat 000_tmp.dat | awk '/^\s*sdev/{ print $3; exit }')"
-            sum="$(cat 000_tmp.dat | awk '/^\s*sum/{ print $3; exit }')"
+            local mean="$(cat 000_tmp.dat | awk '/^\s*mean/{ print $3; exit }')"
+            local sdev="$(cat 000_tmp.dat | awk '/^\s*sdev/{ print $3; exit }')"
+            local sum="$(cat 000_tmp.dat | awk '/^\s*sum/{ print $3; exit }')"
             echo -e "${kp}\t${nodes}\t${cores}\t${mean}\t${sdev}\t${sum}" >> $fname
             rm 000_tmp.dat
         done
@@ -70,16 +70,18 @@ if [[ $# -eq 1 ]]; then
 fi
 
 for c in ${cwd}/omp*; do
+    echo "Generating data: ${c}"
     fheaders ${c}
     stats ${c}
 done
 
 # gnuplot scripts
-cd ${cwd}
+cd "${root}/${cwd}"
 for s in "${stats[@]}"; do
     IFS=' ' read -r -a file_stat <<< "${s}"
     fname=$(get_fname "${file_stat[@]}")
     fgp="${fname%.*}.gp"
+    echo "Generating gp script: $(pwd -P)/${fgp}"
     fhist="${file_stat[0]}"
     stat="${file_stat[1]}"
     cat <<EOF > ${fgp}
@@ -101,6 +103,7 @@ EOF
     sed -i '0,/replot/s/replot/plot/' "${fgp}"
     tac ${fgp}  | sed '0,/replot/s/, \\$//' > 000_tmp
     tac 000_tmp | sed 's/replot/ /' > "${fgp}"
+    rm 000_tmp
     cat <<EOF >> "${fgp}"
 pause(-1)
 EOF
