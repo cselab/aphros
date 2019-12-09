dfb12e6aa8e75529652b1fd38406debe559025ac
Petr Karnakov <kpetr@ethz.ch>
2019-12-09 16:08:47 +0100

    sim24/traj_recover: cleanup

--- sim/sim24_fountain/case/pasc/tool/traj_recover.py
+++ sim/sim24_fountain/case/pasc/tool/traj_recover.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import numpy as np
-import matplotlib.pyplot as plt
 import re
 import sys
 import pickle
@@ -56,66 +55,7 @@ def StrRow(row):
 def WriteLine(s, out):
     out.write(s + '\n')
 
-def Get(m, ccl, field):
-    return np.array([m[cl][field] for cl in ccl if cl in m])
-
-def ApplyRemap(ccl, rmp):
-    return [rmp.get(ccl[i], ccl[i]) for cl in ccl]
-
-def GetGood(p0, p1):
-    print(p0, p1)
-    d0 = Read(p0)
-    d1 = Read(p1)
-
-    m0 = GetMap(d0)
-    m1 = GetMap(d1)
-
-    ccl0 = set(m0.keys())
-    ccl1 = set(m1.keys())
-
-    dis = ccl0 - ccl1
-    new = ccl1 - ccl0
-    diff = {cl for cl in ccl0 & ccl1 if DiffRel(m0[cl]['vf'],  m1[cl]['vf'])}
-    good = ccl0 & ccl1 - diff
-    bad = diff | new
-    Plot(m0, m1, bad)
-    Plot(m0, m1, dis)
-    rmp = dict()
-    def Cost(c0, c1):
-        pass
-    print(dis)
-    print(new)
-
-    #Plot(m0, m1, dis.union(new))
-    exit()
-    return good, rmp
-
-def Plot(m0, m1, diff):
-    v='x'
-    vv='y'
-    plt.scatter(
-            Get(m1, diff, v),
-            Get(m1, diff, vv),
-            #Get(m1, diff, 'cl'),
-            #Get(m1, diff, 'y'),
-            s=Get(m1, diff, 'r')**2*2e5)
-    plt.scatter(
-            #Get(m0, diff, v) + Get(m0, diff, 'v'+v)*dt,
-            #Get(m0, diff, vv) + Get(m0, diff, 'v'+vv)*dt,
-            Get(m0, diff, v),
-            Get(m0, diff, vv),
-            #Get(m0, diff, 'cl'),
-            #Get(m0, diff, 'y') + Get(m0, diff, 'vy')*dt,
-            s=Get(m0, diff, 'r')**2*2e5, edgecolor='red', facecolor='none')
-    #plt.scatter(Get(m0, dis, 'x'), Get(m0, dis, 'y'))
-    #plt.scatter(Get(m1, new, 'z'), Get(m1, new, 'y'), s=15)
-    plt.savefig("a.pdf")
-
-# p: path traj_0000.csv
-def GetIndex(path):
-    return int(re.findall(".*_(\d*)", path)[0])
-
-cl0 = float(sys.argv[1])
+clinit = float(sys.argv[1])
 pp = sys.argv[2:]
 
 out = sys.stdout
@@ -127,7 +67,7 @@ m0 = None
 WriteLine('n,' + StrHeader(Read(pp[0])), out)
 
 for i in range(0, len(pp)):
-    print(i)
+    std.stderr.write("{:}".format(i) + '\n')
     m0 = m1
     m1 = GetMap(Read(pp[i]))
     if cl in m1 and (m0 is None or cl not in m0
@@ -151,6 +91,5 @@ for i in range(0, len(pp)):
             if Cost(cln) < Cost(clbest):
                 clbest = cln
         cl = clbest
-        print('cl=', cl)
         WriteLine(str(i) + ',' + StrRow(m1[cl]), out)
 
