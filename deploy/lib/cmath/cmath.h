3b81816bbb246cf98d14b2749eac4bd82d0aef6c
Sergey Litvinov <slitvinov@gmail.com>
2019-12-09 10:51:03 +0100

    lib/cmath: indent

--- deploy/lib/cmath/cmath.h
+++ deploy/lib/cmath/cmath.h
@@ -1,2 +1,3 @@
 int cmath_eig_values(const double[6], /**/ double[3]);
-int cmath_eig_vectors(const double[6], /**/ double[3], double[3], double[3]);
+int cmath_eig_vectors(const double[6], /**/ double[3], double[3],
+                      double[3]);
