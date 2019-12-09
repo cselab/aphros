6d2b94180fa0c9f44a664b8fd3c88779903c2954
Sergey Litvinov <slitvinov@gmail.com>
2019-12-09 10:50:36 +0100

    initial import of deploy/lib/cmath

---
+++ deploy/lib/cmath/err.c
@@ -0,0 +1,21 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "err.h"
+
+int
+err_print(const char *fmt, ...)
+{
+  va_list ap;
+
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  va_end(ap);
+  return 0;
+}
+
+void
+err_exit(int status)
+{
+  exit(status);
+}
