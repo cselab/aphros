5eb6e77fe912f078c14edb4bc4faf8240bcd198d
Petr Karnakov <kpetr@ethz.ch>
2019-12-11 00:45:35 +0100

    sim24/pasc: add vis orange

---
+++ sim/sim24_fountain/case/pasc/vis/view.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env pvbatch
+
+from paraview.simple import *
+
+from glob import glob
+import sys
+import re
+import math
+import os
+import numpy as np
+
+def Log(s):
+    s += "\n"
+    o = sys.stderr
+    o.write(s)
+    o.flush()
+
+# natural sort
+def natkey(s, _nsre=re.compile('([0-9]+)')):
+    return [int(text) if text.isdigit() else text.lower()
+            for text in re.split(_nsre, s)]
+
+def natsorted(v):
+    return sorted(v, key=natkey)
+# Returns sorted list of files in base by pattern pre_*.xmf
+
+# Sets time of datasets to step i
+def SetTime(i):
+    global vft, vt
+    for j in range(len(vft)):
+        s = vft[j]
+        s.ForcedTime = vt[j][i]
+        s.UpdatePipeline()
+
+# Returns bounding box of object o
+def GetBox(o):
+    o.UpdatePipeline()
+    di = o.GetDataInformation()
+    lim = di.DataInformation.GetBounds()
+    lim0 = np.array(lim[::2])
+    lim1 = np.array(lim[1::2])
+    return lim0, lim1
+
+
+
+av = sys.argv
+if len(av) < 2 or av[1] == '-h':
+    sys.stderr.write('''usage: {:} [sm_*.vtk]
+Plots isosurface.
+# Output:
+# a_*.png in current folder
+'''.format(av[0]))
+    exit(1)
+
+def CheckFlag(name):
+    if name in av:
+        av.remove(name)
+        return True
+    return False
+
+cam = 1  # view from side perspective
+if CheckFlag('-C1'):
+    cam = 1
+if CheckFlag('-C2'):
+    cam = 2
+
+# vf input
+ff = natsorted(av[1:])
+# vf basename
+ffb = list(map(os.path.basename, ff))
+# vf dirname
+ffd = list(map(os.path.dirname, ff))
+# steps
+ss = [int(re.findall("_([0-9]*)", fb)[0]) for fb in ffb]
+
+# output pattern (:0 substituted by frame number)
+bo = "a_{:}.png"
+
+#####################################################
+### BEGIN OF STATE FILE
+#####################################################
+
+#### disable automatic camera reset on 'Show'
+paraview.simple._DisableFirstRenderCameraReset()
+
+# ----------------------------------------------------------------
+# RENDER
+# ----------------------------------------------------------------
+
+# get the material library
+materialLibrary1 = GetMaterialLibrary()
+
+
+# view
+C1 = [
+[1.673947199541654, 1.155534385955274, 3.017460244409912],
+[0.49676167169496643, -0.25002651930486053, -1.3449860440897603],
+[-0.0893981734829278, 0.9547941520217919, -0.2835067791833273]
+    ]
+
+C2 = [
+[2.1915305271955976, 2.3226888513602653, 4.915606378976366],
+[1.0, 0.9, 0.5],
+[-0.09878153704372385, 0.9445146013953416, -0.31326406702697107],
+    ]
+
+CC = [C1, C2]
+C = CC[cam - 1]
+
+# Create a new 'Render View'
+renderView1 = CreateView('RenderView')
+renderView1.ViewSize = [1920, 1080]
+renderView1.OrientationAxesVisibility = 0
+renderView1.CameraPosition = C[0]
+renderView1.CameraFocalPoint = C[1]
+renderView1.CameraViewUp = C[2]
+renderView1.Background = [1.0]*3
+renderView1.OSPRayMaterialLibrary = materialLibrary1
+renderView1.KeyLightWarmth = 0.5
+renderView1.FillLightWarmth = 0.5
+renderView1.KeyLightIntensity = 0.85
+
+# ----------------------------------------------------------------
+# END RENDER
+# ----------------------------------------------------------------
+
+# ----------------------------------------------------------------
+# BEGIN READERS
+# ----------------------------------------------------------------
+
+# create a new 'XDMF Reader'
+surf = LegacyVTKReader(FileNames=ff)
+
+# list of all sources
+vs = [surf]
+
+# time steps
+vt = [np.array(s.TimestepValues) for s in vs]
+
+# replace with ForceTime
+surf = ForceTime(surf)
+
+# all ForceTime
+vft = [surf]
+
+# ----------------------------------------------------------------
+# END READERS
+# ----------------------------------------------------------------
+
+surf = Calculator(Input=surf)
+surf.ResultNormals = 1
+surf.AttributeType = 'Point Data'
+surf.ResultArrayName = 'normals'
+surf.Function = 'nn'
+
+water = Clip(Input=surf)
+water.ClipType = 'Scalar'
+water.Scalars = ['CELLS', 'cl']
+water.Value = -0.1
+water.Invert = 1
+waterDisplay = Show(water, renderView1)
+waterDisplay.Representation = 'Surface'
+waterDisplay.ColorArrayName = [None, '']
+waterDisplay.Opacity = 0.25
+
+def rgb(r, g, b):
+    m = 255.
+    return [r/m, g/m, b/m]
+
+bubbles = Clip(Input=surf)
+bubbles.ClipType = 'Scalar'
+bubbles.Scalars = ['CELLS', 'cl']
+bubbles.Value = -0.1
+bubbles.Invert = 0
+bubblesDisplay = Show(bubbles, renderView1)
+bubblesDisplay.Representation = 'Surface'
+bubblesDisplay.ColorArrayName = [None, '']
+bubblesDisplay.DiffuseColor = rgb(255, 127, 14)
+bubblesDisplay.Ambient = 0.2
+
+
+#####################################################
+### END OF STATE FILE
+#####################################################
+
+for i in list(range(len(ss))):
+    fn = bo.format("{:04d}".format(ss[i]))
+    if os.path.isfile(fn):
+        Log("skip existing {:}".format(fn))
+        continue
+
+    SetTime(i)
+
+    Log("{:}/{:}: {:}".format(i + 1, len(ss), fn))
+    SaveScreenshot(fn, renderView1)
