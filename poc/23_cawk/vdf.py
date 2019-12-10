ad58862a775dd9844585f5c1ae5617c015cae982
Sergey Litvinov <slitvinov@gmail.com>
2019-12-10 18:36:25 +0100

    poc/23_cawk: add vdf (velocity correlation)

--- poc/23_cawk/vdf.py
+++ poc/23_cawk/vdf.py
@@ -6,7 +6,7 @@ import numpy as np
 def Read(p):
     return np.genfromtxt(p, delimiter=',', names=True)
 def msg(s):
-  sys.stderr.write(str(s) + "\n")
+    sys.stderr.write(str(s) + "\n")
 
 rmin = 1.0/192
 rmax = 0.1
@@ -21,22 +21,28 @@ for f in sys.argv[1:]:
     x = d["x"]
     y = d["y"]
     z = d["z"]
+    vx = d["vx"]
+    vy = d["vy"]
+    vz = d["vz"]
     r = d["r"]
     n = x.size
     for i in range(n):
         dx = x - x[i]
         dy = y - y[i]
         dz = z - z[i]
+        dv = vx[i] * vx + vy[i] * vy + vz[i] * vz
         dr = np.array([dx, dy, dz])**2
         dr = np.mean(dr, axis = 0)**0.5
-        dr = dr[np.where(dr > rmin)]
-        val, edge = np.histogram(dr, bins=bins, weights = 1/dr**2, range=rng)
+        idx = np.where(dr > rmin)
+        dr = dr[idx]
+        dv = dv[idx]
+        val, edge = np.histogram(dr, bins=bins, weights = dv/dr**2, range=rng)
         sval += val
 
-sval /= sval.mean()        
+sval /= sval.mean()
 left = edge[:-1]
 right = edge[1:]
 center = (left + right) * 0.5
 np.savetxt(sys.stdout, np.transpose([center, sval, left, right]))
-    
+
 EOF
