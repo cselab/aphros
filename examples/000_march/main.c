0532fb3ffb1119fd8d08b093ebecd5f9783913bb
Sergey Litvinov <slitvinov@gmail.com>
2019-12-24 19:42:23 +0100

    examples/000_march/main: do not use av()

--- examples/000_march/main.c
+++ examples/000_march/main.c
@@ -3,18 +3,17 @@
 #include <math.h>
 #include <march.h>
 
-enum { X, Y, Z };
-enum {N = 3 * 3 * MARCH_NTRI};
-static double av(double a, double b, double o) {
-  return a + (b - a) * o;
-}
 
 int main() {
-  double u[] = {-1, 1, 1, 1, 1, 1, 1, -1}, tri[N], offset[N], x;
+  enum { X, Y, Z };
+  enum {N = 3 * 3 * MARCH_NTRI};
+  double u[] = {-1, 1, 1, 1, 1, 1, 1, -1}, tri[N], offset[N], x, a, b;
   int first[N], second[N], i, n;
 
   i = 0;
   march_cube_location(u, &n, tri, first, second, offset);
-  x = av(MARCH_O[first[i]][X], MARCH_O[second[i]][X], offset[i]);
+  a = MARCH_O[first[i]][X];
+  b = MARCH_O[second[i]][X];
+  x = a + (b - a) * offset[i];
   assert(fabs(x - tri[3 * i + X] < 1e-12));
 }
