cac23eabbf6220a2cefefe96997358f9ef1af7b7
Sergey Litvinov <slitvinov@gmail.com>
2019-12-24 13:06:16 +0100

    add 000_march

---
+++ examples/000_march/main.c
@@ -0,0 +1,25 @@
+#undef NDEBUG
+#include <assert.h>
+#include <math.h>
+#include <march.h>
+
+enum { X, Y, Z };
+enum {N = MARCH_NTRI};
+static double av(double a, double b, double o) {
+  return a + (b - a) * o;
+}
+
+int main() {
+  double u[] = {-1, 0, 0, 0, 0, 0, 0, 1};
+  double tri[3 * 3 * N];
+  int first[3 * N], second[3 * N];
+  double offset[3 * N];
+  double x;
+  int i;
+  int n;
+
+  i = 0;
+  march_cube_location(u, &n, tri, first, second, offset);
+  x = av(MARCH_O[first[i]][X], MARCH_O[second[i]][X], offset[i]);
+  assert(fabs(x - tri[3 * i + X] < 1e-12));
+}
