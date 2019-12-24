82f1c9851d1559002459ae3712f9601985ec16f7
Petr Karnakov <kpetr@ethz.ch>
2019-12-24 19:26:18 +0100

    doc/sphinx: add linkpath role

--- doc/sphinx/linkpath.py
+++ doc/sphinx/linkpath.py
@@ -20,12 +20,23 @@ class LinkPath(SphinxDirective):
         abspath = FindPath(relpath, location)
         Assert(abspath, "Target not found '{:}'".format(relpath), location)
         text = abspath
-        p = nodes.problematic(text=text)
-        return [p]
+        return [nodes.problematic(text=text, refuri=abspath)]
+
+def linkpath_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
+    docname = os.path.splitext(inliner.document.attributes["source"])[0]
+    location = (docname, lineno)
+    relpath = text
+    abspath = FindPath(relpath, location)
+    Assert(abspath, "Target not found '{:}'".format(relpath), location)
+    text = abspath
+    return [nodes.reference(text=text, refuri=abspath)], []
+
+
 
 
 def setup(app):
     app.add_directive("linkpath", LinkPath)
+    app.add_role("linkpath", linkpath_role)
 
     return {
         'version': '0.1',
