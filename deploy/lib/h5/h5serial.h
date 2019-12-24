6d6562a956504cdde80a75052770fe7b76804a88
Sergey Litvinov <slitvinov@gmail.com>
2019-12-24 17:23:16 +0100

    h5: add 003_h5_serial_write

--- deploy/lib/h5/h5serial.h
+++ deploy/lib/h5/h5serial.h
@@ -1,3 +1,7 @@
+int h5_xmf(
+    const char* path, const char* name, const double origin[3], double spacing,
+    const int size[3]);
+int h5_silence(void);
 int h5_serial_hdf(const char *path, const int size[3], const double* data);
 int h5_read_hdf(const char *path, /**/ int size[3], double**);
 int h5_read_xmf(
