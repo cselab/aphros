eaac5a9d8a2f7f12198cef356e2b6f30b8024d80
Petr Karnakov <kpetr@ethz.ch>
2019-12-25 12:56:56 +0100

    multi: remove MultiMask

--- src/solver/multi.h
+++ src/solver/multi.h
@@ -9,31 +9,6 @@
 
 namespace solver {
 
-// Assign colors to connected sets with u > 0.
-template <class M_>
-class MultiMask {
-  using M = M_;
-  using Scal = typename M::Scal;
-  using Vect = typename M::Vect;
-  static constexpr size_t dim = M::dim;
-
- public:
-  // fccl: color [a]
-  MultiMask(M& m, const FieldCell<Scal>* fccl, size_t edim);
-  ~MultiMask();
-  // Propagates color.
-  // fcu: volume fraction [a]
-  void Update(const FieldCell<Scal>& fcu);
-  // Returns color [a]
-  const FieldCell<Scal>& GetMask() const;
-  const FieldCell<Scal>& GetMask2() const;
-  static constexpr Scal kNone = -1.; // no color
-
- private:
-  struct Imp;
-  std::unique_ptr<Imp> imp;
-};
-
 template <class T>
 class Multi {
  public:
