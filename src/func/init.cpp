ef9080edebc99b01b78b50cd02f6ad12763a3ba2
Petr Karnakov <pkarnakov@gmail.com>
2019-12-28 23:29:47 +0100

    hydro: use init, init: add init_cl

--- src/func/init.cpp
+++ src/func/init.cpp
@@ -1,6 +1,15 @@
+#include "init.h"
 #include "init_u.h"
+#include "init_cl.h"
 #include "geom/mesh.h"
 
 using M = MeshStructured<double, 3>;
 
 template void InitVf(FieldCell<typename M::Scal>& fcu, const Vars& var, M& m);
+
+template std::function<void(
+    FieldCell<typename M::Scal>&, const FieldCell<typename M::Scal>&, const M&)>
+CreateInitCl(const Vars& par, bool verb);
+
+template std::function<void(FieldCell<typename M::Scal>&, const M&)>
+CreateInitU(const Vars& par, bool verb);
--- src/func/init.h
+++ src/func/init.h
@@ -1,7 +1,18 @@
 #pragma once
 
+#include <functional>
+
 #include "geom/field.h"
 #include "parse/vars.h"
 
 template <class M>
 void InitVf(FieldCell<typename M::Scal>& fcu, const Vars& var, M& m);
+
+template <class M>
+std::function<void(
+    FieldCell<typename M::Scal>&, const FieldCell<typename M::Scal>&, const M&)>
+CreateInitCl(const Vars& par, bool verb);
+
+template <class M>
+std::function<void(FieldCell<typename M::Scal>&, const M&)> CreateInitU(
+    const Vars& par, bool verb);
