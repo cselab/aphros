7854601ba0ba22fa5d6dadcc66e38d1844cd66f7
Petr Karnakov <kpetr@ethz.ch>
2020-01-17 17:22:02 +0100

    test/embed_convdiff: add div

--- src/test/embed_convdiff/main.cpp
+++ src/test/embed_convdiff/main.cpp
@@ -12,6 +12,7 @@
 #include "distr/distrbasic.h"
 #include "solver/convdiffv_eb.h"
 #include "solver/embed.h"
+#include "solver/fluid.h"
 #include "solver/reconst.h"
 
 using M = MeshStructured<double, 3>;
@@ -21,40 +22,152 @@ using EB = Embed<M>;
 using Type = typename EB::Type;
 using CD = ConvDiffVectEmbed<M>;
 
+template <class M>
+FieldCell<typename M::Scal> GetDivergence(
+    FieldEmbed<typename M::Scal>& fev, const M& m, const Embed<M>& eb) {
+  using Scal = typename M::Scal;
+  FieldCell<Scal> fcdiv(m, 0);
+  for (auto c : eb.Cells()) {
+    Scal div = fev[c];
+    for (auto q : m.Nci(c)) {
+      div += fev[m.GetFace(c, q)] * m.GetOutwardFactor(c, q);
+    }
+    fcdiv[c] = div / eb.GetVolume(c);
+  }
+  return fcdiv;
+}
+
+/*
+template <class M>
+void ProjectVolumeFlux(
+    FieldFace<typename M::Scal>& ffv, const MapCondFaceFluid& mfc, M& m,
+    const Embed<M>& eb) {
+  using Scal = typename M::Scal;
+  using ExprFace = GVect<Scal, 3>;
+  using Expr = GVect<Scal, M::dim * 2 + 2>;
+
+  auto sem = m.GetSem();
+  struct {
+    FieldEmbed<ExprFace> fee; // expression for corrected volume flux [i]
+    FieldCell<Expr> fce; // linear system for pressure [i]
+    FieldFace<bool> ffbd; // true for faces with boundary conditions
+    FieldCell<Scal> fcp; // pressure (up to a constant)
+  } * ctx(sem);
+  auto& fee = ctx->fee;
+  auto& fce = ctx->fce;
+  auto& ffbd = ctx->ffbd;
+  auto& fcp = ctx->fcp;
+
+  if (sem("init")) {
+    ffbd.Reinit(m, false);
+    for (auto p : mfc) {
+      ffbd[p.GetIdx()] = true;
+    }
+
+    fee.Reinit(m);
+    for (auto f : eb.Faces()) {
+      auto& e = fee[f];
+      if (!ffbd[f]) { // inner
+        Scal a = -1; // XXX adhoc uniform
+        e[0] = -a;
+        e[1] = a;
+      } else { // boundary
+        e[0] = 0;
+        e[1] = 0;
+      }
+      e[2] = ffv[f];
+    }
+
+    fce.Reinit(m, Expr(0));
+    for (auto c : m.Cells()) {
+      auto& e = fce[c];
+      for (auto q : m.Nci(c)) {
+        const IdxFace f = m.GetFace(c, q);
+        const ExprFace v = ffe[f] * m.GetOutwardFactor(c, q);
+        e[0] += v[1 - q % 2];
+        e[1 + q] += v[q % 2];
+        e[Expr::dim - 1] += v[2];
+      }
+    }
+  }
+  if (sem("solve")) {
+    std::vector<Scal>* lsa;
+    std::vector<Scal>* lsb;
+    std::vector<Scal>* lsx;
+    m.GetSolveTmp(lsa, lsb, lsx);
+    lsx->resize(m.GetInBlockCells().size());
+    size_t i = 0;
+    for (auto c : m.Cells()) {
+      (void)c;
+      (*lsx)[i++] = 0;
+    }
+    auto l = ConvertLsCompact(fce, *lsa, *lsb, *lsx, m);
+    using T = typename M::LS::T;
+    l.t = T::symm;
+    m.Solve(l);
+  }
+  if (sem("copy")) {
+    std::vector<Scal>* lsa;
+    std::vector<Scal>* lsb;
+    std::vector<Scal>* lsx;
+    m.GetSolveTmp(lsa, lsb, lsx);
+
+    fcp.Reinit(m);
+    size_t i = 0;
+    for (auto c : m.Cells()) {
+      fcp[c] = (*lsx)[i++];
+    }
+    m.Comm(&fcp);
+  }
+  if (sem("apply")) {
+    for (auto f : m.Faces()) {
+      const IdxCell cm = m.GetCell(f, 0);
+      const IdxCell cp = m.GetCell(f, 1);
+      const auto& e = ffe[f];
+      ffv[f] = e[0] * fcp[cm] + e[1] * fcp[cp] + e[2];
+    }
+  }
+}
+*/
+
 void Run(M& m, Vars& var) {
   auto sem = m.GetSem("Run");
   struct {
     std::unique_ptr<EB> eb;
     std::unique_ptr<CD> cd;
-    FieldCell<Vect> fcvel;
     MapCondFace mfc;
     FieldCell<Scal> fcr;
     FieldEmbed<Scal> fed;
     FieldCell<Vect> fcs;
     FieldFace<Scal> ffv;
+    FieldCell<Scal> fcdiv;
     size_t frame = 0;
   } * ctx(sem);
-  auto& eb = ctx->eb;
   auto& cd = ctx->cd;
-  auto& fcvel = ctx->fcvel;
   auto& ffv = ctx->ffv;
+  auto& fcdiv = ctx->fcdiv;
   auto& mfc = ctx->mfc;
   auto& frame = ctx->frame;
+
+  const Vect vel(1., 0.5, 0.25);
+  const size_t bc = 0;
+  const Vect bcvel(1);
 
   if (sem("initeb")) {
     auto fnl = InitEmbed(m, var);
     ctx->eb.reset(new EB(m, fnl));
   }
   if (sem("init")) {
+    auto& eb = *ctx->eb;
     ctx->fcr.Reinit(m, 1);
     ctx->fed.Reinit(m, 0);
     ctx->fcs.Reinit(m, Vect(0));
     ffv.Reinit(m, 0);
-    const Vect vel(1., 0.5, 0.25);
+
     for (auto f : m.Faces()) {
       ffv[f] = vel.dot(m.GetSurface(f));
     }
-    fcvel.Reinit(m, Vect(1., 1., 1.));
+    FieldCell<Vect> fcvel(m, Vect(1., 1., 1.));
     /*
     for (auto c : eb->AllCells()) {
       const Scal a = 12;
@@ -62,11 +175,9 @@ void Run(M& m, Vars& var) {
           std::sin(m.GetCenter(c)[0] * a) * std::sin(m.GetCenter(c)[1] * a);
     }
     */
-    const size_t bc = 0;
-    const Vect bcvel(1);
     const Scal dt0 = 0.5 * sqr(m.GetCellSize()[0]);
     const Scal dt0a = 1 / vel.abs().max() * m.GetCellSize()[0];
-    //const Scal dt = dt0 * 0.5;
+    // const Scal dt = dt0 * 0.5;
     const Scal dt = dt0a * 0.05;
     std::cout << "dt0=" << dt0 << " "
               << "dt0a=" << dt0a << " "
@@ -75,15 +186,27 @@ void Run(M& m, Vars& var) {
               << std::endl;
     typename CD::Par par;
     cd.reset(new CD(
-        m, *eb, fcvel, mfc, bc, bcvel, &ctx->fcr, &ctx->fed, &ctx->fcs,
+        m, eb, fcvel, mfc, bc, bcvel, &ctx->fcr, &ctx->fed, &ctx->fcs,
         &ctx->ffv, 0, dt, par));
   }
   if (sem.Nested("dumppoly")) {
-    eb->DumpPoly();
+    ctx->eb->DumpPoly();
   }
   const size_t maxt = 100;
   const size_t nfr = 100;
   for (size_t t = 0; t < maxt; ++t) {
+    if (sem("div")) {
+      auto& eb = *ctx->eb;
+      auto fevel = eb.Interpolate(cd->GetVelocity(), bc, bcvel);
+      FieldEmbed<Scal> fev(m, 0);
+      for (auto f : eb.Faces()) {
+        fev[f] = fevel[f].dot(eb.GetNormal(f) * eb.GetArea(f));
+      }
+      for (auto c : eb.CFaces()) {
+        fev[c] = fevel[c].dot(eb.GetNormal(c) * eb.GetArea(c));
+      }
+      fcdiv = GetDivergence(fev, m, eb);
+    }
     if (sem.Nested("convdiff-start")) {
       cd->StartStep();
     }
@@ -100,6 +223,7 @@ void Run(M& m, Vars& var) {
       m.Dump(&cd->GetVelocity(), 0, "vx");
       m.Dump(&cd->GetVelocity(), 1, "vy");
       m.Dump(&cd->GetVelocity(), 2, "vz");
+      m.Dump(&fcdiv, "div");
       ++frame;
     }
   }
