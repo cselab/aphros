912bf16d8d1ec4cd458305d1b36a5485134e8b6c
Petr Karnakov <kpetr@ethz.ch>
2020-01-29 11:59:04 +0100

    ex103: add plot

--- examples/103_embed_advection/main.cpp
+++ examples/103_embed_advection/main.cpp
@@ -16,17 +16,17 @@ using Vect = typename M::Vect;
 // vel: advection velocity
 // dt: time step
 void Advection0(
-    FieldCell<Scal>& fcu, const MapCondFace& mec, Vect vel, Scal dt,
-    const Embed<M>& eb) {
+    FieldCell<Scal>& fcu, const MapCondFace& mec, const FieldEmbed<Scal>& fev,
+    Scal dt, const Embed<M>& eb) {
   const auto& m = eb.GetMesh();
-  const auto feu = eb.Interpolate(fcu, mec, 0, 0.);
+  const auto feu = eb.InterpolateUpwind(fcu, fev.GetFieldFace(), mec, 0, 1.);
   // Compute flux.
-  FieldEmbed<Scal> fevu(m, 0); // quantity flux
+  FieldEmbed<Scal> fevu(m, 0);
   for (auto f : eb.Faces()) {
-    fevu[f] = feu[f] * vel.dot(eb.GetSurface(f));
+    fevu[f] = feu[f] * fev[f];
   }
   for (auto c : eb.CFaces()) {
-    fevu[c] = feu[c] * vel.dot(eb.GetSurface(c));
+    fevu[c] = feu[c] * fev[c];
   }
   // Advance in time.
   for (auto c : eb.Cells()) {
@@ -34,7 +34,7 @@ void Advection0(
     for (auto q : eb.Nci(c)) {
       sum += fevu[m.GetFace(c, q)] * m.GetOutwardFactor(c, q);
     }
-    fcu[c] += sum * dt / eb.GetVolume(c);
+    fcu[c] -= sum * dt / eb.GetVolume(c);
   }
 }
 
@@ -43,53 +43,75 @@ void Run(M& m, Vars& var) {
   struct {
     FieldNode<Scal> fnl;
     FieldCell<Scal> fcu;
+    FieldEmbed<Scal> fev;
     MapCondFace mfc; // boundary conditions
     Scal t = 0;
+    Vect vel;
+    Scal dt;
     std::unique_ptr<Embed<M>> eb_;
   } * ctx(sem);
+  auto& fnl = ctx->fnl;
+  auto& fcu = ctx->fcu;
+  auto& fev = ctx->fev;
+  auto& mfc = ctx->mfc;
+  auto& t = ctx->t;
+  auto& dt = ctx->dt;
+  auto& vel = ctx->vel;
 
   if (sem("init")) {
-    ctx->fcu.Reinit(m, 0);
+    vel = Vect(var.Vect["vel"]);
+    dt = var.Double["cfl"] * m.GetCellSize()[0] / vel.norm1();
+    fcu.Reinit(m, 0);
     for (auto c : m.AllCells()) {
       auto x = m.GetCenter(c);
-      ctx->fcu[c] = ((Vect(0.5, 0.5, 0) - x).norminf() < 0.2);
+      fcu[c] = ((Vect(0.4, 0.4, 0) - x).norminf() < 0.2);
     }
     ctx->eb_.reset(new Embed<M>(m));
-    ctx->fnl.Reinit(m, 0);
+    fnl.Reinit(m, 0);
     for (auto n : m.AllNodes()) {
       auto x = m.GetNode(n);
-      ctx->fnl[n] = (0.4 - (Vect(0.5, 0.5, 0) - x).norm());
+      fnl[n] = (0.4 - (Vect(0.5, 0.5, 0) - x).norm());
     }
+    m.Dump(&fcu, "u");
   }
   if (sem.Nested("eb-init")) {
-    ctx->eb_->Init(ctx->fnl);
+    ctx->eb_->Init(fnl);
+  }
+  if (sem("flux")) {
+    auto& eb = *ctx->eb_;
+    fev.Reinit(m, 0);
+    for (auto f : eb.Faces()) {
+      fev[f] = eb.GetSurface(f).dot(vel);
+    }
+    for (auto c : eb.CFaces()) {
+      fev[c] = eb.GetSurface(c).dot(vel);
+    }
   }
   if (sem.Nested("eb-dump")) {
     ctx->eb_->DumpPoly();
   }
   sem.LoopBegin();
   if (sem("step")) {
-    const Vect vel(var.Vect["vel"]);
-    const Scal dt = var.Double["cfl"] * m.GetCellSize()[0] / vel.norm1();
     switch (var.Int["example"]) {
       case 0:
-        Advection0(ctx->fcu, ctx->mfc, vel, dt, *ctx->eb_);
+        Advection0(fcu, mfc, fev, dt, *ctx->eb_);
         break;
       default:
         throw std::runtime_error(
             "Unknown example=" + std::to_string(var.Int["example"]));
     }
-    ctx->t += dt;
+    t += dt;
+    m.Comm(&fcu);
   }
   if (sem("loopbreak")) {
-    if (ctx->t >= var.Double["tmax"]) {
+    if (t >= var.Double["tmax"]) {
       sem.LoopBreak();
+      m.Dump(&fcu, "u");
     }
   }
-  if (sem("dump")) {
-    m.Dump(&ctx->fcu, "u");
-  }
   sem.LoopEnd();
+  if (sem()) {
+  }
 }
 
 int main(int argc, const char** argv) {
@@ -106,8 +128,8 @@ set int bsz 1
 
 set string dumpformat plain
 
-set double tmax 1
-set vect vel 0.5 0.3 0
+set double tmax 0.2
+set vect vel 1 1 0
 set double cfl 0.5
 
 set int example 0
