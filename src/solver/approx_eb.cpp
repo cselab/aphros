da2995898f38b87331c332db8446f0767b0b984a
Petr Karnakov <pkarnakov@gmail.com>
2020-02-11 07:23:47 +0100

    solver: add dummy approx_eb

---
+++ src/solver/approx_eb.cpp
@@ -0,0 +1,13 @@
+// Created by Petr Karnakov on 11.02.2020
+// Copyright 2020 ETH Zurich
+
+#include "approx_eb.ipp"
+#include "linear/linear.h"
+
+using M = MeshStructured<double, 3>;
+using EB = Embed<M>;
+using Scal = typename M::Scal;
+using Vect = typename M::Vect;
+using T = Scal;
+using TV = Vect;
+constexpr size_t dim = M::dim;
---
+++ src/solver/approx_eb.h
@@ -0,0 +1,15 @@
+// Created by Petr Karnakov on 11.02.2020
+// Copyright 2020 ETH Zurich
+
+#pragma once
+
+#include <array>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#include "cond.h"
+#include "embed.h"
+#include "geom/mesh.h"
+#include "solver.h"
