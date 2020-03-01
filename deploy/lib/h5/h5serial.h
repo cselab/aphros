994b545a88c56cd7ca831b8315f806ba21a995cc
Sergey Litvinov <slitvinov@gmail.com>
2020-03-01 12:34:50 +0100

    lib/h5: minor

--- deploy/lib/h5/h5serial.h
+++ deploy/lib/h5/h5serial.h
@@ -5,5 +5,5 @@ int h5_silence(void);
 int h5_serial_hdf(const char *path, const int size[3], const double* data);
 int h5_read_hdf(const char *path, /**/ int size[3], double**);
 int h5_read_xmf(
-    const char*, /**/ char* name, double origin[3], double* spacing,
+    const char*, /**/ char* name, double origin[3], double spacing[3],
     int size[3]);
