ed10b1e5cbf0ffc0a7af15fe54aec0c048643d37
Sergey Litvinov <slitvinov@gmail.com>
2020-03-03 13:55:18 +0100

    poc/08_contact_line: add main.c

---
+++ poc/08_contact_line/main.c
@@ -0,0 +1,153 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+const char* me = "main";
+#define USED(x) \
+  if (x)        \
+    ;           \
+  else {        \
+  }
+
+static void usg(void) {
+  fprintf(stderr, "%s -r ratio -p degree -x float -y float> vtk\n", me);
+  exit(2);
+}
+
+static double sq(double);
+static double cube(double);
+
+int main(int argc, char** argv) {
+  USED(argc);
+  double R;
+  double h;
+  double pi;
+  double aA, bA, cA, dA, aB, bB, cB, dB;
+  double S, C;
+  double vr;
+  double vt;
+  double t;
+  double x;
+  double y;
+  double vx;
+  double vy;
+  int Rflag;
+  int Pflag;
+  int Xflag;
+  int Yflag;
+
+  Rflag = Pflag = Xflag = Yflag = 0;
+  while (*++argv != NULL && argv[0][0] == '-')
+    switch (argv[0][1]) {
+      case 'h':
+        usg();
+        break;
+      case 'p':
+        argv++;
+        if (*argv == NULL) {
+          fprintf(stderr, "%s: -p needs an argument\n", me);
+          exit(1);
+        }
+        h = atof(*argv);
+        Pflag = 1;
+        break;
+      case 'x':
+        argv++;
+        if (*argv == NULL) {
+          fprintf(stderr, "%s: -x needs an argument\n", me);
+          exit(1);
+        }
+        x = atof(*argv);
+        Xflag = 1;
+        break;
+      case 'y':
+        argv++;
+        if (*argv == NULL) {
+          fprintf(stderr, "%s: -y needs an argument\n", me);
+          exit(1);
+        }
+        y = atof(*argv);
+        Yflag = 1;
+        break;	
+      case 'r':
+        argv++;
+        if (*argv == NULL) {
+          fprintf(stderr, "%s: -r needs an argument\n", me);
+          exit(1);
+        }
+        R = atof(*argv);
+        Rflag = 1;
+        break;
+      default:
+        fprintf(stderr, "%s: unknown option '%s'\n", me, argv[0]);
+        exit(2);
+    }
+  if (!Rflag) {
+    fprintf(stderr, "%s: -r is not set\n", me);
+    exit(2);
+  }
+  if (!Pflag) {
+    fprintf(stderr, "%s: -p is not set\n", me);
+    exit(2);
+  }
+  if (!Xflag) {
+    fprintf(stderr, "%s: -x is not set\n", me);
+    exit(2);
+  }
+  if (!Yflag) {
+    fprintf(stderr, "%s: -y is not set\n", me);
+    exit(2);
+  }  
+  
+  if (!(0 <= h && h <= 90)) {
+    fprintf(stderr, "%s: h = %g is not in [0, 90]\n", me, h);
+    exit(2);
+  }
+
+  h *= 0.0174532925199433; /* to radian */
+  pi = 3.141592653589793;
+  S = sin(h);
+  C = cos(h);
+
+  dA = -(S * ((-C * h * pi) + S * pi - C * R * sq(h) + C * sq(h) +
+              C * R * sq(S) - C * sq(S))) /
+       ((-h * sq(pi)) + C * S * sq(pi) - R * sq(h) * pi + 2 * sq(h) * pi -
+        2 * C * S * h * pi + R * sq(S) * pi - C * R * S * sq(h) +
+        C * S * sq(h) + R * cube(h) - cube(h) - R * sq(S) * h + sq(S) * h +
+        C * R * cube(S) - C * cube(S));
+  bA = -dA * pi;
+  cA = (C * dA * pi - C * dA * h + S * dA + S) / (S * (h - pi));
+  aA = (-cA * pi) - dA - 1;
+  bB = 0;
+  dB = (dA * h * sq(pi) - 2 * dA * sq(h) * pi + sq(C) * pi - pi + dA * cube(h) +
+        sq(C) * dA * h - dA * h) /
+       ((sq(h) + sq(C) - 1) * (h - pi));
+  aB = (-dB) - 1;
+  cB = ((-C * dB * h) + S * dB + S) / (S * h);
+
+  t = atan2(y, x);
+  S = sin(t);
+  C = cos(t);
+  if (t > h) {
+      vt = (cA*t+aA)*S+(dA*t+bA)*C;      
+      vr = (dA*t-cA+bA)*S-(cA*t+dA+aA)*C;
+  } else {
+      vt = (cB*t+aB)*S+(dB*t+bB)*C;
+      vr = (dB*t-cB+bB)*S-(cB*t+dB+aB)*C;
+  }
+  vx = vr * C - vt * S;
+  vy = vr * S + vt * C;
+  printf("%g %g %g %g\n", x, y, vr, vt);
+}
+
+static double
+sq(double x)
+{
+    return x * x;
+}
+
+static double
+cube(double x)
+{
+    return x * x * x;
+}
