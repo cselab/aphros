03ae096cc1413f3463b39980c66254ec600aec4d
Petr Karnakov <kpetr@ethz.ch>
2020-03-04 16:05:17 +0100

    sim29: add bcg

---
+++ sim/sim29_order/bcg/hook.cpp
@@ -0,0 +1,33 @@
+// Created by Petr Karnakov on 04.03.2020
+// Copyright 2020 ETH Zurich
+
+#include <iostream>
+
+#include <util/posthook.h>
+
+template <class M>
+void InitVelHook(
+    FieldCell<typename M::Vect>& fcvel, const Vars& var, const M& m) {
+  using Scal = typename M::Scal;
+  using Vect = typename M::Vect;
+  for (auto c : m.AllCells()) {
+    // Bell,Colella,Glaz 1989
+    // psi = 1/pi * sin**2(pi*x) * sin**2(pi*y)
+    const Vect xx = m.GetCenter(c);
+    const Scal x = xx[0];
+    const Scal y = xx[1];
+    Scal& u = fcvel[c][0];
+    Scal& v = fcvel[c][1];
+    const Scal pi = M_PI;
+    using std::sin;
+    using std::cos;
+    u = -2 * sqr(sin(pi * x)) * sin(pi * y) * cos(pi * y);
+    v = 2 * sqr(sin(pi * y)) * sin(pi * x) * cos(pi * x);
+  }
+}
+
+using M = MeshStructured<double, 3>;
+using Scal = typename M::Scal;
+using Vect = typename M::Vect;
+
+template void InitVelHook(FieldCell<Vect>&, const Vars&, const M&);
