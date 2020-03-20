957b3a950cf0fccb4cbc956936d79aca5b5e090a
Sergey Litvinov <lisergey@debian>
2020-03-20 17:41:49 +0100

    initial import of inside

---
+++ poc/18_inside/main.c
@@ -0,0 +1,11 @@
+#include <assert.h>
+#include <limits.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "err.h"
+#include "memory.h"
+#include "inside.h"
+
+static char me[] = "inside";
