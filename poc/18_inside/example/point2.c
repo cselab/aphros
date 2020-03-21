7240f7aae3a7e575410002248c71ea549f055163
Sergey Litvinov <lisergey@debian>
2020-03-21 23:16:02 +0100

    poc/18_inside: grid

---
+++ poc/18_inside/example/point2.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inside.h>
+
+const char* me = "point2";
+
+static void usg(void) {
+  fprintf(stderr, "%s -p float float float < off\n", me);
+  exit(1);
+}
+
+int
+main(int argc, const char** argv) {
+  enum {X, Y, Z};
+  int nt;
+  int nv;
+  int *tri;
+  double *ver;
+  int status;
+  double p[3];
+  FILE *file;
+  struct Inside *inside;
+  while (*++argv != NULL && argv[0][0] == '-')
+    switch (argv[0][1]) {
+    case 'h':
+      usg();
+      break;
+    default:
+      fprintf(stderr, "%s: unknown option '%s'\n", me, argv[0]);
+      exit(2);
+    }
+  if (argv[0] == NULL) {
+    fprintf(stderr, "%s: off file is missing\n", me);
+    exit(2);
+  }
+  if ((file = fopen(argv[0], "r")) == NULL) {
+    fprintf(stderr, "%s: failed to open '%s'\n", me, argv[0]);
+    exit(2);    
+  }
+  if (off_read(file, &status, &nt, &tri, &nv, &ver) != 0) {
+    fprintf(stderr, "%s: off_read failed\n", me);
+    exit(2);
+  }
+  fclose(file);
+  if (status != 0) {
+    fprintf(stderr, "%s: not an off file\n", me);
+    exit(2);
+  }
+  inside_ini(nt, tri, ver, &inside);
+  while (scanf("%lf %lf %lf", &p[X], &p[Y], &p[Z]) == 3)
+    printf("%d\n", inside_inside(inside, p));
+  inside_fin(inside);
+  off_fin(tri, ver);
+}
