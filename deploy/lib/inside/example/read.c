44239b9e537e7cec373b6d279a29b6cf0772af4b
Sergey Litvinov <lisergey@debian>
2020-03-22 13:51:53 +0100

    deploy/lib/inside: wip

---
+++ deploy/lib/inside/example/read.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#include <inside.h>
+
+const char* me = "read";
+
+static void usg(void) {
+  fprintf(stderr, "%s < off > off\n", me);
+  exit(1);
+}
+
+int
+main(int argc, const char** argv) {
+  int nt;
+  int nv;
+  int *tri;
+  double *ver;
+
+  while (*++argv != NULL && argv[0][0] == '-')
+    switch (argv[0][1]) {
+    case 'h':
+      usg();
+      break;
+    default:
+      fprintf(stderr, "%s: unknown option '%s'\n", me, argv[0]);
+      exit(2);
+    }
+  if (argv[0] == NULL) {
+    fprintf(stderr, "%s: mesh file is missing\n", me);
+    exit(2);
+  }
+  if (inside_mesh_read(argv[0], &nt, &tri, &nv, &ver) != 0) {
+    fprintf(stderr, "%s: fail to read mesh '%s'\n", me, argv[0]);
+    exit(2);
+  }
+  off_write(nt, tri, nv, ver, stdout);
+  inside_mesh_fin(tri, ver);
+}
