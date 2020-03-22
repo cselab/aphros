1cebed6a9bc64dfee83f049c4d20f093e25348ef
Petr Karnakov <kpetr@ethz.ch>
2020-03-22 18:40:15 +0100

    lib/inside/example/aphros: add dummy hook

--- deploy/lib/inside/example/aphros/hook.cpp
+++ deploy/lib/inside/example/aphros/hook.cpp
@@ -1,4 +1,4 @@
-// Created by Petr Karnakov on 04.03.2020
+// Created by Petr Karnakov on 22.03.2020
 // Copyright 2020 ETH Zurich
 
 #include <iostream>
@@ -6,25 +6,16 @@
 #include <util/posthook.h>
 
 template <class M>
-void InitVelHook(
-    FieldCell<typename M::Vect>& fcvel, const Vars&, const M& m) {
+void InitEmbedHook(
+    FieldNode<typename M::Scal>& fn_levelset, const Vars&, const M& m) {
   using Scal = typename M::Scal;
   using Vect = typename M::Vect;
   if (m.IsRoot()) {
-    std::cout << "initial velocity: Bell,Colella,Glaz 1989" << std::endl;
+    std::cout << "Embedded boundaries from lib/inside"
+              << std::endl;
   }
-  for (auto c : m.AllCells()) {
-    // psi = 1/pi * sin**2(pi*x) * sin**2(pi*y)
-    const Vect xx = m.GetCenter(c);
-    const Scal x = xx[0];
-    const Scal y = xx[1];
-    Scal& u = fcvel[c][0];
-    Scal& v = fcvel[c][1];
-    const Scal pi = M_PI;
-    using std::sin;
-    using std::cos;
-    u = -2 * sqr(sin(pi * x)) * sin(pi * y) * cos(pi * y);
-    v = 2 * sqr(sin(pi * y)) * sin(pi * x) * cos(pi * x);
+  for (auto n : m.AllNodes()) {
+    (void) n;
   }
 }
 
@@ -32,4 +23,4 @@ using M = MeshStructured<double, 3>;
 using Scal = typename M::Scal;
 using Vect = typename M::Vect;
 
-template void InitVelHook(FieldCell<Vect>&, const Vars&, const M&);
+template void InitEmbedHook(FieldNode<Scal>&, const Vars&, const M&);
