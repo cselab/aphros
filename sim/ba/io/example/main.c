55c3106817b7606f90600f3a2c19d93ed90dc47d
Sergey Litvinov <slitvinov@gmail.com>
2020-04-15 12:13:38 +0200

    initial import of ba/io/example

---
+++ sim/ba/io/example/main.c
@@ -0,0 +1,20 @@
+#include <h5.h>
+#include <tgmath.h>
+#include "grid/multigrid3D.h"
+#include ".u/io.h"
+
+#define LVL (6)
+
+int
+main(void)
+{
+  init_grid(1 << LVL);
+  scalar a[], b[];
+  char *names[] = {"pressure", "density"};
+  scalar *scalars = {a, b};
+  foreach() {
+    a[] = (x - X0) * z;
+    b[] = (x - X0);
+  }
+  io_write({a, b}, stdout);
+}
