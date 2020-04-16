c9fda7ace129821679e7f0b13a4f6e1aa5604642
Sergey Litvinov <slitvinov@gmail.com>
2020-04-16 15:21:01 +0200

    poc/32_baview: minor

--- poc/32_baview/distance.c
+++ poc/32_baview/distance.c
@@ -58,7 +58,7 @@ int main(int argc, char **argv)
   foreach_dimension()
     if (max.x - min.x > maxl)
       maxl = max.x - min.x;
-  init_grid (8);
+  init_grid (4);
   size (1.2*maxl);
   origin ((max.x + min.x)/2. - L0/2,
           (max.y + min.y)/2. - L0/2,
@@ -71,7 +71,7 @@ int main(int argc, char **argv)
   view (fov = 15.65, quat = {-0.52,0.31,0.38,-0.7},
         tx = -0.045, ty = 0.015, width = 640, height = 480, bg = {1,1,1});
   isosurface ("d", 0, color = "level", min = 5, max = 10);
-  save ("horse.png");
+  save ("level.png");
 
   vertex scalar phi[];
   foreach_vertex()
