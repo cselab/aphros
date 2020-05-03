2db995830c8160d2a1291de8d780470a75131551
Petr Karnakov <pkarnakov@gmail.com>
2020-05-03 17:46:33 +0200

    test/hook: add config to hook.cpp

---
+++ src/test/hook/hook/hook.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include <util/posthook.h>
+#include <parse/config.h>
+
+
+template <class M>
+void InitVelHook(FieldCell<typename M::Vect>&, const Vars& var, const M&) {
+  struct : public ConfigBase {
+    VAR_INT(var0);
+    VAR_DOUBLE(var1);
+  } conf;
+  conf.Read(var);
+
+  std::cout << "[hook]";
+  std::cout << " var0=" << conf.var0;
+  std::cout << " var1=" << conf.var1;
+  std::cout << std::endl;
+}
+
+using M = MeshStructured<double, 3>;
+template void InitVelHook(FieldCell<typename M::Vect>&, const Vars&, const M&);
