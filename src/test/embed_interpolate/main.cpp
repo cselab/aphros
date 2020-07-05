0592a03f7eacd01ed58f18eb35a5b98c03227e99
Petr Karnakov <kpetr@ethz.ch>
2020-07-06 00:43:48 +0200

    embed_interpolate: add omz interpolation

--- src/test/embed_interpolate/main.cpp
+++ src/test/embed_interpolate/main.cpp
@@ -55,13 +55,15 @@ void EmbedInterpolate<M>::Run() {
     FieldCell<Scal> fcvx;
     FieldCell<Scal> fcvy;
     FieldCell<Scal> fcvz;
-    FieldEmbed<Scal> feomz;
+    FieldCell<Scal> fcomz;
 
     std::vector<Vect> px;
     std::vector<Scal> pomz;
+    std::vector<Scal> pomz_interp;
   } * ctx(sem);
   auto& px = ctx->px;
   auto& pomz = ctx->pomz;
+  auto& pomz_interp = ctx->pomz_interp;
   if (sem("ctor")) {
     eb_.reset(new Embed<M>(m, var.Double["embed_gradlim"]));
     ctx->fnl = UEmbed<M>::InitEmbed(m, var, m.IsRoot());
@@ -81,7 +83,18 @@ void EmbedInterpolate<M>::Run() {
   if (sem.Nested()) {
     Hdf<M>::Read(ctx->fcvy, var.String["input_hdf_vy"], m);
   }
-  if (sem("interpolate")) {
+  if (sem.Nested()) {
+    Hdf<M>::Read(ctx->fcomz, var.String["input_hdf_omz"], m);
+  }
+  if (sem("reduce_x")) {
+    auto& eb = *eb_;
+    for (auto c : eb.CFaces()) {
+      px.push_back(eb.GetFaceCenter(c));
+    }
+    using TV = typename M::template OpCatT<Vect>;
+    m.Reduce(std::make_shared<TV>(&px));
+  }
+  if (sem("calc_omz")) {
     auto& eb = *eb_;
     MapEmbed<BCond<Scal>> mebc;
     for (auto c : eb.CFaces()) {
@@ -89,32 +102,44 @@ void EmbedInterpolate<M>::Run() {
     }
     const auto fegx = UEmbed<M>::Gradient(ctx->fcvx, mebc, eb);
     const auto fegy = UEmbed<M>::Gradient(ctx->fcvy, mebc, eb);
-    ctx->feomz.Reinit(m, 0);
+    FieldEmbed<Scal> feomz(m, 0);
     for (auto c : eb.CFaces()) {
       const Vect n = -eb.GetNormal(c);
       const Vect t{n[1], -n[0], 0.};
-      ctx->feomz[c] = fegx[c] * t[0] + fegy[c] * t[1];
+      feomz[c] = fegx[c] * t[0] + fegy[c] * t[1];
     }
     for (auto c : eb.CFaces()) {
-      px.push_back(eb.GetFaceCenter(c));
-      pomz.push_back(ctx->feomz[c]);
+      pomz.push_back(feomz[c]);
     }
-    using TV = typename M::template OpCatT<Vect>;
-    m.Reduce(std::make_shared<TV>(&px));
     using TS = typename M::template OpCatT<Scal>;
     m.Reduce(std::make_shared<TS>(&pomz));
   }
+  if (sem("interpolate_omz")) {
+    auto& eb = *eb_;
+    MapEmbed<BCond<Scal>> mebc;
+    for (auto c : eb.CFaces()) {
+      mebc[c] = BCond<Scal>(BCondType::neumann, 0);
+    }
+    const auto feomz = UEmbed<M>::Interpolate(ctx->fcomz, mebc, eb);
+    for (auto c : eb.CFaces()) {
+      pomz_interp.push_back(feomz[c]);
+    }
+    using TS = typename M::template OpCatT<Scal>;
+    m.Reduce(std::make_shared<TS>(&pomz_interp));
+  }
   if (sem("write") && m.IsRoot()) {
     fassert_equal(px.size(), pomz.size());
+    fassert_equal(px.size(), pomz_interp.size());
     std::ofstream o(var.String["output_csv_omz"]);
     o.precision(16);
     // header
-    o << "x,y,z,omz";
+    o << "x,y,z,omz,omz_interp";
     o << std::endl;
     // content
     for (size_t i = 0; i < px.size(); ++i) {
       o << px[i][0] << ',' << px[i][1] << ',' << px[i][2];
       o << ',' << pomz[i];
+      o << ',' << pomz_interp[i];
       o << "\n";
     }
   }
