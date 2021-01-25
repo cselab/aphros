9978f662f809e700f937d6db546d3f706f1bc812
Petr Karnakov <pkarnakov@gmail.com>
2021-01-25 11:46:21 +0100

    add CONTRIBUTING.md

--- README.py
+++ README.py
@@ -65,33 +65,6 @@ container
     docker build github.com/cselab/aphros --tag aphros
     docker run -i aphros
 
-### Code formatting
-
-#### C++
-
-Run clang-format for C/C++ files found recursively in current directory
-
-```
-ap.format
-```
-
-#### Python
-
-Format all python files found recursively in current directory
-
-```
-yapf -ir .
-```
-
-### Add copyright notice
-
-Add copyright notice to C/C++ source files found recursively in current
-directory (if `copyright` is not found in first 10 lines of the file)
-
-```
-ap.applycopyright $(ap.findsource)
-```
-
 ## Videos
 
 Examples of simulations visualized using
