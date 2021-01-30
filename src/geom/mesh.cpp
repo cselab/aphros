d0e32f917eafa8d9e4824532fdc28c5f22560239
Petr Karnakov <pkarnakov@gmail.com>
2021-01-30 21:33:04 +0100

    macros: add MULTIDIMX, cmake: add use_avx, use_dims, mesh: use MULTIDIMX

--- src/geom/mesh.cpp
+++ src/geom/mesh.cpp
@@ -3,19 +3,17 @@
 
 #include "mesh.ipp"
 
-template class MeshCartesian<double, 3>;
-using M = MeshCartesian<double, 3>;
-template M InitUniformMesh(
-    Rect<typename M::Vect> domain, typename M::MIdx begin, typename M::MIdx s,
-    int halos, bool isroot, bool islead, typename M::MIdx gs, int id);
-template void M::ApplyNanFaces(FieldCell<typename M::Scal>& fc);
-template void M::ApplyNanFaces(FieldCell<typename M::Vect>& fc);
+#define XX(M)                                                      \
+  template class M;                                                \
+  template M InitUniformMesh(                                      \
+      Rect<typename M::Vect> domain, typename M::MIdx begin,       \
+      typename M::MIdx s, int halos, bool isroot, bool islead,     \
+      typename M::MIdx gs, int id);                                \
+  template void M::ApplyNanFaces(FieldCell<typename M::Scal>& fc); \
+  template void M::ApplyNanFaces(FieldCell<typename M::Vect>& fc);
 
+#define COMMA ,
 
-template class MeshCartesian<double, 4>;
-using M4 = MeshCartesian<double, 4>;
-template M4 InitUniformMesh(
-    Rect<typename M4::Vect> domain, typename M4::MIdx begin, typename M4::MIdx s,
-    int halos, bool isroot, bool islead, typename M4::MIdx gs, int id);
-template void M4::ApplyNanFaces(FieldCell<typename M4::Scal>& fc);
-template void M4::ApplyNanFaces(FieldCell<typename M4::Vect>& fc);
+#define X(dim) XX(MeshCartesian<double COMMA dim>)
+
+MULTIDIMX
