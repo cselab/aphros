d27b085142b818cd58299c24906e1f46b302d50a
Petr Karnakov <kpetr@ethz.ch>
2021-02-03 13:15:44 +0100

    cmake: cleanup client.cmake, fix test/hook

--- src/client.cmake
+++ src/client.cmake
@@ -24,8 +24,11 @@ set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 
-# MPI
-if (USE_MPI)
+if (APHROS_USE_MPI)
+  find_package(OpenMP REQUIRED)
+endif()
+
+if (APHROS_USE_MPI)
   find_package(MPI REQUIRED)
   set(CMAKE_C_COMPILER ${MPI_C_COMPILER})
   set(CMAKE_CXX_COMPILER ${MPI_CXX_COMPILER})
@@ -33,45 +36,3 @@ endif()
 
 # warnings
 add_compile_options(-Wall -pedantic -Wextra)
-
-if (USE_HDF)
-  set(T "hdf")
-  if (USE_MPI)
-    set(HDF5_PREFER_PARALLEL ON)
-  endif()
-  set(HDF5_NO_FIND_PACKAGE_CONFIG_FILE ON)
-  find_package(HDF5 REQUIRED COMPONENTS C HL)
-  if (USE_MPI AND NOT HDF5_IS_PARALLEL)
-    message(FATAL_ERROR "No parallel HDF5")
-  endif()
-  add_library(${T} INTERFACE IMPORTED)
-  set_property(TARGET ${T} APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${HDF5_INCLUDE_DIRS})
-  set_property(TARGET ${T} APPEND PROPERTY INTERFACE_LINK_LIBRARIES ${HDF5_LIBRARIES} ${HDF5_HL_LIBRARIES})
-endif()
-
-if (USE_HYPRE)
-  set(T "hypreext")
-  set(HYPRE_DIR ${APHROS_PREFIX})
-  add_library(${T} INTERFACE IMPORTED)
-  set_property(TARGET ${T} APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${HYPRE_DIR}/include)
-  set_property(TARGET ${T} APPEND PROPERTY INTERFACE_LINK_LIBRARIES -L${HYPRE_DIR}/lib -lHYPRE -lm)
-endif()
-
-set(T "openmp")
-add_library(${T} INTERFACE IMPORTED)
-if (USE_OPENMP)
-  find_package(OpenMP REQUIRED)
-  find_package(Threads REQUIRED)
-  set_property(TARGET ${T} PROPERTY 
-      INTERFACE_COMPILE_OPTIONS ${OpenMP_CXX_FLAGS})
-  set_property(TARGET ${T} PROPERTY 
-      INTERFACE_LINK_LIBRARIES ${OpenMP_CXX_FLAGS} Threads::Threads)
-endif()
-
-if (USE_FPZIP)
-  find_package(FPZIP REQUIRED) # floating point compressor
-endif()
-
-if (USE_AMGX)
-  find_package(CUDAToolkit)
-endif()
