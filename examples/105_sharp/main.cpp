8263b7002a224675ce713768ce4c1fc745cc47c2
Petr Karnakov <pkarnakov@gmail.com>
2021-02-03 15:09:28 +0100

    examples/105_sharp: add raw format

--- examples/105_sharp/main.cpp
+++ examples/105_sharp/main.cpp
@@ -13,11 +13,13 @@
 
 #include <distr/distrbasic.h>
 #include "dump/hdf.h"
+#include "dump/raw.h"
 #include "parse/argparse.h"
 #include "parse/vars.h"
 #include "parse/vof.h"
 #include "solver/vof.h"
 #include "util/distr.h"
+#include "util/filesystem.h"
 #include "util/hydro.h"
 #include "util/vof.h"
 
@@ -25,6 +27,20 @@ using M = MeshStructured<double, 3>;
 using Scal = typename M::Scal;
 using Vect = typename M::Vect;
 using MIdx = typename M::MIdx;
+
+std::string GetFormat(std::string path, std::string format) {
+  const auto ext = util::SplitExt(path)[1];
+  if (format == "auto") {
+    if (ext == ".h5") {
+      format = "h5";
+    } else if (ext == ".raw") {
+      format = "raw";
+    } else {
+      fassert(false, util::Format("Unknown extension '{}' of '{}'", ext, path));
+    }
+  }
+  return format;
+}
 
 void Run(M& m, Vars& var) {
   const bool verbose = var.Int["VERBOSE"];
@@ -51,9 +67,49 @@ void Run(M& m, Vars& var) {
     std::vector<std::vector<Scal>> table;
   } * ctx(sem);
   auto& t = *ctx;
-  if (sem.Nested()) {
-    Hdf<M>::Read(t.fcu, var.String["hdf_in"], m);
-  }
+
+  auto read = [&](FieldCell<Scal>& fc_buf, std::string path) {
+    if (sem.Nested("read")) {
+      auto format = GetFormat(path, var.String["format"]);
+      if (format == "h5") {
+        Hdf<M>::Read(fc_buf, path, m);
+      } else if (format == "raw") {
+        dump::Raw<M>::Meta meta;
+        meta.size = m.GetGlobalSize();
+        meta.count = m.GetGlobalSize();
+        using Raw = dump::Raw<M>;
+        Raw::Read(fc_buf, meta, path, m);
+      } else {
+        fassert(false, "Unkown format=" + format);
+      }
+    }
+  };
+  auto write = [&](auto fc_buf, std::string path) {
+    auto format = GetFormat(path, var.String["format"]);
+    if (format == "h5") {
+      if (sem.Nested("write")) {
+        Hdf<M>::Write(fc_buf(), path, m);
+      }
+      if (sem("writexmf")) {
+        Hdf<M>::WriteXmf(util::SplitExt(path)[0] + ".xmf", "u", path, m);
+      }
+    } else if (format == "raw") {
+      dump::Raw<M>::Meta meta;
+      meta.size = m.GetGlobalSize();
+      meta.count = m.GetGlobalSize();
+      using Raw = dump::Raw<M>;
+      if (sem.Nested("write")) {
+        Raw::Write(fc_buf(), meta, path, m);
+      }
+      if (sem("writexmf")) {
+        Raw::WriteXmf(
+            util::SplitExt(path)[0] + ".xmf", "u", Raw::Type::Float64, path, m);
+      }
+    } else {
+      fassert(false, "Unkown format=" + format);
+    }
+  };
+  read(t.fcu, var.String["inputpath"]);
   if (sem("ctor")) {
     t.fe_flux.Reinit(m, 0);
     t.fc_src.Reinit(m, 0);
@@ -90,10 +146,9 @@ void Run(M& m, Vars& var) {
   if (sem.Nested("finish")) {
     t.solver->FinishStep();
   }
-  if (sem.Nested()) {
-    Hdf<M>::Write(
-        t.solver->GetField(Step::iter_curr), var.String["hdf_out"], m);
-  }
+  write(
+      [&]() { return t.solver->GetField(Step::iter_curr); },
+      var.String["outputpath"]);
   const std::string* vtk_out = var.String.Find("vtk_out");
   if (vtk_out && sem.Nested()) {
     t.solver->DumpInterface(*vtk_out);
@@ -113,14 +168,14 @@ void Run(M& m, Vars& var) {
     auto plic = t.solver->GetPlic();
     t.uvof.Recolor(
         plic.layers, plic.vfcu, &t.fccl, &t.fccl, t.par.clfixed,
-        t.par.clfixed_x, t.par.coalth, {}, t.par.verb,
-        t.par.recolor_unionfind, t.par.recolor_reduce, t.par.recolor_grid, m);
+        t.par.clfixed_x, t.par.coalth, {}, t.par.verb, t.par.recolor_unionfind,
+        t.par.recolor_reduce, t.par.recolor_grid, m);
   }
   if (csv_out && sem.Nested()) {
     auto plic = t.solver->GetPlic();
     CalcTraj<M>(
-        m, plic.layers, plic.vfcu, &t.fccl, plic.vfcim, t.fc_zero,
-        t.fc_zerov, t.column_names, t.row_colors, t.table);
+        m, plic.layers, plic.vfcu, &t.fccl, plic.vfcim, t.fc_zero, t.fc_zerov,
+        t.column_names, t.row_colors, t.table);
   }
   if (csv_out && sem()) {
     if (m.IsRoot()) {
@@ -159,28 +214,40 @@ int main(int argc, const char** argv) {
       .Help("CFL number for advection, valid values between 0 and 1");
   parser.AddVariable<std::string>("--vtk_out_march")
       .Help("Path to output VTK with surface from marching cubes");
-  parser.AddVariable<std::string>("--vtk_out").Help(
-      "Path to output VTK with piecewise linear surface");
-  parser.AddVariable<std::string>("--csv_out").Help(
-      "Path to output CSV with centroids of connected components");
+  parser.AddVariable<std::string>("--vtk_out")
+      .Help("Path to output VTK with piecewise linear surface");
+  parser.AddVariable<std::string>("--csv_out")
+      .Help("Path to output CSV with centroids of connected components");
   parser.AddVariable<int>("--steps", 5).Help("Number of sharpening steps");
   parser.AddVariable<std::string>("--extra", "")
       .Help("Extra configuration (commands 'set ... ')");
+  parser.AddVariable<std::string>({"--format", "-f"}, "auto")
+      .Help("File format")
+      .Options({"auto", "h5", "raw", "dat"});
 
-  parser.AddVariable<std::string>("hdf_in").Help(
+  parser.AddVariable<std::string>("input").Help(
       "Path to input image as HDF5 array of floats between 0 and 1");
-  parser.AddVariable<std::string>("hdf_out").Help(
-      "Path to output image, can be the same as HDF_IN");
+  parser.AddVariable<std::string>("output").Help(
+      "Path to output image, can be the same as input");
 
   auto args = parser.ParseArgs(argc, argv);
   if (const int* p = args.Int.Find("EXIT")) {
     return *p;
   }
 
-  const auto shape = Hdf<M>::GetShape(args.String["hdf_in"]);
-  const int nx = shape[2];
-  const int ny = shape[1];
-  const int nz = shape[0];
+  int nx, ny, nz;
+  const auto inputformat =
+      GetFormat(args.String.GetStr("input"), args.String.GetStr("format"));
+  if (inputformat == "h5") {
+    const auto shape = Hdf<M>::GetShape(args.String["input"]);
+    nx = shape[2];
+    ny = shape[1];
+    nz = shape[0];
+  } else {
+    nx = 0;
+    ny = 0;
+    nz = 0;
+  }
 
   auto checkdiv = [](int n, int b, std::string name) {
     if (n % b) {
@@ -221,8 +288,8 @@ int main(int argc, const char** argv) {
 
   conf << "set int steps " << args.Int["steps"] << '\n';
 
-  conf << "set string hdf_in " << args.String["hdf_in"] << '\n';
-  conf << "set string hdf_out " << args.String["hdf_out"] << '\n';
+  conf << "set string inputpath " << args.String["input"] << '\n';
+  conf << "set string outputpath " << args.String["output"] << '\n';
   if (auto* p = args.String.Find("vtk_out")) {
     conf << "set string vtk_out " << *p << '\n';
   }
@@ -234,6 +301,7 @@ int main(int argc, const char** argv) {
   }
   conf << "set double cfl " << args.Double["cfl"] << '\n';
   conf << "set int VERBOSE " << args.Int["verbose"] << '\n';
+  conf << "set string format " << args.String["format"] << '\n';
 
   conf << args.String["extra"] << '\n';
 
