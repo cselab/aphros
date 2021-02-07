c37be8192e75061f822a43d06546232203968e29
Petr Karnakov <pkarnakov@gmail.com>
2021-02-07 14:06:11 +0100

    march: revert moving MARCH_O

--- src/march/march.h
+++ src/march/march.h
@@ -3,6 +3,10 @@ extern "C" {
 #endif
 
 enum { MARCH_NTRI = 48 };
+static double MARCH_O[][3] = {
+    {0, 0, 0}, {1, 0, 0}, {0, 1, 0}, {1, 1, 0},
+    {0, 0, 1}, {1, 0, 1}, {0, 1, 1}, {1, 1, 1},
+};
 int march_cube(double u[8], /**/ int* ntri, double* tri);
 int march_cube_location(
     double u[8], /**/ int* ntri, double* tri, int* x, int* y, double* a);
