ce427eeb68cdafa27a3be01782d9709ad0bdc89e
Petr Karnakov <pkarnakov@gmail.com>
2021-02-14 01:57:16 +0100

    geom/blockface: cleanup

--- src/geom/blockface.h
+++ src/geom/blockface.h
@@ -19,23 +19,25 @@ class GBlock<IdxFace, dim_> {
   using Dir = GDir<dim>;
 
   class iterator {
-    const GBlock* o_; // owner
+    const GBlock* owner_;
     MIdx w_;
-    Dir d_;
+    Dir dir_;
 
    public:
-    explicit iterator(const GBlock* o, MIdx w, Dir d) : o_(o), w_(w), d_(d) {}
+    explicit iterator(const GBlock* owner, MIdx w, Dir d)
+        : owner_(owner), w_(w), dir_(d) {}
     iterator& operator++() {
-      MIdx wd(d_);
+      MIdx wd(dir_);
       for (size_t n = 0; n < dim; ++n) {
         ++w_[n]; // increment current
-        if (w_[n] == o_->b_[n] + o_->cs_[n] + wd[n]) { // if end reached
+        if (w_[n] ==
+            owner_->start_[n] + owner_->ncells_[n] + wd[n]) { // if end reached
           if (n < dim - 1) {
-            w_[n] = o_->b_[n]; // reset to begin
+            w_[n] = owner_->start_[n]; // reset to begin
           } else { // if end reached for last dim
-            if (size_t(d_) < dim - 1) {
-              d_ = Dir(size_t(d_) + 1);
-              w_ = o_->b_;
+            if (size_t(dir_) < dim - 1) {
+              dir_ = Dir(size_t(dir_) + 1);
+              w_ = owner_->start_;
             }
           }
         } else {
@@ -45,21 +47,20 @@ class GBlock<IdxFace, dim_> {
       return *this;
     }
     bool operator==(const iterator& o) const {
-      return w_ == o.w_ && d_ == o.d_;
+      return w_ == o.w_ && dir_ == o.dir_;
     }
     bool operator!=(const iterator& o) const {
       return !(*this == o);
     }
     std::pair<MIdx, Dir> operator*() const {
-      return std::make_pair(w_, d_);
+      return std::make_pair(w_, dir_);
     }
     // Returns number of calls ++i_ for which
     // GetIdx(*++i_).GetRaw() == GetIdx(*i_).GetRaw() + 1
     // (i.e. increment of iterator equivalent to increment of index)
     size_t GetNumLite() const {
-      const MIdx wd(d_);
-      assert(w_[0] + 1 <= o_->b_[0] + o_->cs_[0] + wd[0]);
-      return o_->b_[0] + o_->cs_[0] + wd[0] - w_[0] - 1;
+      return owner_->start_[0] + owner_->ncells_[0] +
+             (dir_.raw() == 0 ? 1 : 0) - w_[0] - 1;
     }
     void LiteInc(size_t a) {
       assert(a <= GetNumLite());
@@ -67,74 +68,36 @@ class GBlock<IdxFace, dim_> {
     }
   };
 
-  // b: begin, lower corner cell index
-  // cs: cells size
-  GBlock(MIdx b, MIdx cs) : b_(b), cs_(cs) {
-    // number of faces
-    nfa_ = 0;
-    for (size_t d = 0; d < dim; ++d) {
-      nf_[d] = CalcNumFaces(d);
-      nfa_ += nf_[d];
-    }
-  }
-  GBlock(MIdx cs) : GBlock(MIdx(0), cs) {}
+  // start: begin, lower corner cell index
+  // ncells: cells size
+  GBlock(MIdx start, MIdx ncells)
+      : start_(start)
+      , ncells_(ncells)
+      , count_(MIdx(ncells_.prod()) + MIdx(ncells_.prod()) / ncells_) {}
+  GBlock(MIdx ncells) : GBlock(MIdx(0), ncells) {}
   GBlock() : GBlock(MIdx(0), MIdx(0)) {}
   size_t size() const {
-    return nfa_;
+    return count_.sum();
   }
   operator GRange<Idx>() const {
     return GRange<Idx>(0, size());
   }
   MIdx GetSize() const {
-    return cs_;
+    return ncells_;
   }
   iterator begin() const {
-    return iterator(this, b_, Dir(0));
+    return iterator(this, start_, Dir(0));
   }
   iterator end() const {
-    MIdx w = b_;
-    auto ld = dim - 1; // last direction
-    w[ld] = (b_ + cs_)[ld] + 1;
-    return iterator(this, w, Dir(ld));
+    MIdx w = start_;
+    w[dim - 1] = (start_ + ncells_)[dim - 1] + 1;
+    return iterator(this, w, Dir(dim - 1));
   }
 
  private:
-  const MIdx b_;
-  const MIdx cs_; // cells size
-  MIdx nf_; // number of faces in each direction
-  size_t nfa_; // total number of faces
-  size_t GetNumFaces(size_t d) const {
-    return nf_[d];
-  }
-  size_t CalcNumFaces(size_t d) const {
-    MIdx bcs = cs_;
-    ++bcs[d];
-    return bcs.prod();
-  }
-  size_t GetFlat(MIdx w, size_t d) const {
-    w -= b_;
-    MIdx bcs = cs_;
-    ++bcs[d];
-    size_t r = 0;
-    for (size_t i = dim; i != 0;) {
-      --i;
-      r *= bcs[i];
-      r += w[i];
-    }
-    return r;
-  }
-  // r: offset from first face with direction d
-  // d: direction
-  MIdx GetMIdxFromOffset(size_t r, size_t d) const {
-    MIdx bcs = cs_;
-    ++bcs[d];
-    MIdx w;
-    for (size_t i = 0; i < dim; ++i) {
-      w[i] = r % bcs[i];
-      r /= bcs[i];
-    }
-    return b_ + w;
-  }
+  const MIdx start_;
+  const MIdx ncells_; // cells size
+  const MIdx count_; // number of faces in each direction
 };
 
 // Specialization for IdxFace
@@ -147,81 +110,58 @@ class GIndex<IdxFace, dim_> {
   using MIdx = generic::MIdx<dim>;
   using Dir = GDir<dim>;
 
-  // b: begin, lower corner
-  // rs: raw size (underlying block that fits all faces_)
-  GIndex(MIdx b, MIdx rs) : b_(b), rs_(rs), br_(b_, rs_) {
-    // number of faces in each direction
-    nfa_ = 0;
-    for (size_t d = 0; d < dim; ++d) {
-      nf_[d] = CalcNumFaces(d);
-      nfa_ += nf_[d];
-    }
-    // cumulative number of faces
-    nfc_[0] = 0;
-    for (size_t d = 1; d < dim; ++d) {
-      nfc_[d] = nfc_[d - 1] + nf_[d - 1];
-    }
-  }
-  GIndex(MIdx rs) : GIndex(MIdx(0), rs) {}
+  // start: begin, lower corner
+  // rawsize: raw size (underlying block that fits all faces_)
+  GIndex(MIdx start, MIdx rawsize)
+      : indexc_(start, rawsize), ncells_(indexc_.size()) {}
+  GIndex(MIdx rawsize) : GIndex(MIdx(0), rawsize) {}
   GIndex() : GIndex(MIdx(0), MIdx(0)) {}
   size_t size() const {
-    return nfa_;
+    return ncells_ * dim;
   }
   operator GRange<Idx>() const {
     return GRange<Idx>(0, size());
   }
   Idx GetIdx(MIdx w, Dir d) const {
-    size_t r = GetBase(size_t(d)) + br_.GetIdx(w);
-    return Idx(r);
+    return Idx(d.raw() * ncells_ + indexc_.GetIdx(w));
   }
   Idx GetIdx(std::pair<MIdx, Dir> p) const {
     return GetIdx(p.first, p.second);
   }
   Dir GetDir(Idx f) const {
-    size_t r = f.GetRaw();
-    size_t d = 0;
-    while (r >= GetNumFaces(d) && d < dim) {
-      r -= GetNumFaces(d);
-      ++d;
+    const auto r = f.raw();
+    const auto n = ncells_;
+    for (size_t d = 0; d < dim; ++d) {
+      if (r < n * (d + 1)) {
+        return Dir(d);
+      }
     }
-    return Dir(d);
+    return {};
   }
   std::pair<MIdx, Dir> GetMIdxDir(Idx f) const {
-    size_t r = f.GetRaw();
-    size_t d = 0;
-    while (r >= GetNumFaces(d) && d < dim) {
-      r -= GetNumFaces(d);
-      ++d;
+    const auto r = f.raw();
+    const auto n = ncells_;
+    for (size_t d = 0; d < dim; ++d) {
+      if (r < n * (d + 1)) {
+        return {indexc_.GetMIdx(r - n * d), Dir(d)};
+      }
     }
-    return {br_.GetMIdx(r), Dir(d)};
+    return {};
   }
   MIdx GetMIdx(Idx f) const {
-    size_t r = f.GetRaw();
-    size_t d = 0;
-    while (r >= GetNumFaces(d) && d < dim) {
-      r -= GetNumFaces(d);
-      ++d;
+    const auto r = f.raw();
+    const auto n = ncells_;
+    for (size_t d = 0; d < dim; ++d) {
+      if (r < n * (d + 1)) {
+        return indexc_.GetMIdx(r - n * d);
+      }
     }
-    return br_.GetMIdx(r);
+    return {};
   }
 
  private:
-  const MIdx b_;
-  const MIdx rs_; // size of cells with padding
-  const GIndex<size_t, dim> br_; // index cells
-  MIdx nf_; // number of faces in each direction with padding
-  size_t nfa_; // total number of indices with padding
-  MIdx nfc_; // cumulative number of faces up to direction: sum(nf_[0:d])
-             // (base index for faces of direction d)
-  size_t GetNumFaces(size_t d) const {
-    return nf_[d];
-  }
-  size_t GetBase(size_t d) const {
-    return nfc_[d];
-  }
-  size_t CalcNumFaces(size_t /*d*/) const {
-    return br_.size();
-  }
+  const GIndex<size_t, dim> indexc_; // indexer over cells
+  const size_t ncells_; // total number of cells with padding
 };
 
 template <size_t dim>
