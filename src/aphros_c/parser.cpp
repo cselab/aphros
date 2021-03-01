252f67fed4445bbef40005367f1c11fd0d19ea48
Sergey Litvinov <slitvinov@gmail.com>
2021-03-01 16:35:30 +0100

    lib/c: wip aphros_c/parser.cpp

--- src/aphros_c/parser.cpp
+++ src/aphros_c/parser.cpp
@@ -1,18 +1,27 @@
 #include "aphros_c.h"
-#include <stdlib.h>
+#include <iostream>
 #include "parse/parser.h"
 
 struct aphros_Parser {
-  Vars par;
-  Parser parser(par);
-  parser.ParseFile("a.conf");
+  Vars *par;
+  Parser *parser;
+  int status;
 };
 
 struct aphros_Parser *aphros_parser_file_ini(const char *path) {
   struct aphros_Parser *q;
   if ((q = (struct aphros_Parser*)malloc(sizeof *q)) == NULL)
     return NULL;
+  q->par = new Vars();
+  q->parser = new Parser(*q->par);
+  q->parser->ParseFile(path);
+  q->status = 0;
   return q;
+}
+
+int aphros_parser_print_vars(struct aphros_Parser *q) {
+  q->parser->PrintVars(std::cout);
+  return 0;
 }
 
 int aphros_parser_int(struct aphros_Parser *q, const char *name) {
@@ -20,11 +29,13 @@ int aphros_parser_int(struct aphros_Parser *q, const char *name) {
 }
 
 int aphros_parser_status(struct aphros_Parser *q) {
-  return 0;
+  return q->status;
 }
 
 int aphros_parser_fin(struct aphros_Parser *q)
 {
+  delete q->par;
+  delete q->parser;
   free(q);
   return 0;
 }
