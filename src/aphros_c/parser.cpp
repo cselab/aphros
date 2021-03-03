3e543622e35b5734f68acbf60166c602a5b8f18e
Sergey Litvinov <slitvinov@gmail.com>
2021-03-03 14:18:41 +0100

    lib/c: add aphros_ParserGetString

--- src/aphros_c/parser.cpp
+++ src/aphros_c/parser.cpp
@@ -1,23 +1,28 @@
 #include "parse/parser.h"
+#include <float.h>
+#include <limits.h>
+#include <stdio.h>
+#include <string.h>
 #include <iostream>
 #include "aphros_c.h"
 
 struct aphros_Parser {
   Vars* par;
   Parser* parser;
+  char* string;
   int status;
 };
 
 struct aphros_Parser* aphros_ParserFileIni(const char* path) {
   struct aphros_Parser* q;
   if ((q = (struct aphros_Parser*)malloc(sizeof *q)) == NULL) return NULL;
+  q->string = NULL;
   q->status = 0;
   q->par = new Vars();
   q->parser = new Parser(*q->par);
   try {
     q->parser->ParseFile(path);
   } catch (const std::runtime_error& e) {
-    fprintf(stderr, "%s\n", e.what());
     return NULL;
   }
   return q;
@@ -33,7 +38,7 @@ int aphros_ParserGetInt(struct aphros_Parser* q, const char* name) {
     return q->par->Int[name];
   } catch (const std::runtime_error& e) {
     q->status = 1;
-    return 0;
+    return INT_MAX;
   }
 }
 
@@ -42,7 +47,25 @@ double aphros_ParserGetDouble(struct aphros_Parser* q, const char* name) {
     return q->par->Double[name];
   } catch (const std::runtime_error& e) {
     q->status = 1;
-    return 0.0;
+    return DBL_MAX;
+  }
+}
+
+char* aphros_ParserGetString(struct aphros_Parser* q, const char* name) {
+  std::string s;
+  try {
+    s = q->par->String[name];
+    if (q->string == NULL)
+      free(q->string);
+    if ((q->string = strdup(s.c_str())) == NULL) {
+      fprintf(stderr, "%s:%d: strdup failed\n", __FILE__, __LINE__);
+      q->status = 2;
+      return NULL;
+    }
+    return q->string;
+  } catch (const std::runtime_error& e) {
+    q->status = 1;
+    return NULL;
   }
 }
 
@@ -56,6 +79,7 @@ int aphros_ParserStatus(struct aphros_Parser* q) {
 int aphros_ParserFin(struct aphros_Parser* q) {
   delete q->par;
   delete q->parser;
+  free(q->string);
   free(q);
   return 0;
 }
