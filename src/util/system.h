ed49344181bbc3b47aa7c22952719b06e05b293b
Sergey Litvinov <slitvinov@gmail.com>
2021-03-08 18:38:25 +0100

    initial import of src/util/system.h

---
+++ src/util/system.h
@@ -0,0 +1,9 @@
+char *SystemRealPath(const char *, char *resolved);
+char *SystemDirName(char*);
+char *SystemBaseName(char*);
+char *SystemMakeDir(char*, int parent);
+int SystemIsFile(char*);
+int SystemIsDir(char*);
+int SystemGetHostName(char *, size_t size);
+size_t SystemGetMem(void);
+int SystemHasHyperthreads(void);
