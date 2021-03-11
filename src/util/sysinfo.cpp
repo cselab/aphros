705a677d09673b7bdc504b67c10c8b9ea6ce7235
Sergey Litvinov <slitvinov@gmail.com>
2021-03-11 17:06:08 +0100

    use system.h in src/util/sysinfo.cpp

--- src/util/sysinfo.cpp
+++ src/util/sysinfo.cpp
@@ -6,7 +6,8 @@
 #include <sstream>
 
 #include "sysinfo.h"
-#include "util/logger.h"
+#include "system.h"
+#include "logger.h"
 #include "util/mpi.h"
 
 #ifdef _OPENMP
@@ -19,50 +20,12 @@
 
 namespace sysinfo {
 
-// Returns value from /proc/self/status found by key k
-size_t GetProcStat(std::string k) {
-  std::ifstream f("/proc/self/status");
-
-  f >> std::skipws;
-  while (f) {
-    std::string s;
-    f >> s;
-    if (s == k + ":") {
-      size_t r = 0;
-      f >> r;
-      return r;
-    }
-  }
-  return 0;
-}
-
 size_t GetMem() {
-  return GetProcStat("VmRSS") << 10;
+  return SystemGetMem();
 }
 
 bool HasHyperthreads() {
-  bool has_ht = false;
-  std::ifstream f("/proc/cpuinfo");
-  f >> std::skipws;
-  while (f) {
-    std::string s;
-    f >> s;
-    if (s == "flags") {
-      break;
-    }
-  }
-  std::string line;
-  std::getline(f, line);
-  std::istringstream is(line);
-  while (!is.eof()) {
-    std::string s;
-    is >> s;
-    if (s == "ht") {
-      has_ht = true;
-      break;
-    }
-  }
-  return has_ht;
+  return SystemHasHyperthreads();
 }
 
 std::string GetHostname() {
