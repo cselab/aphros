9aec8fd01de18cffeeb39fb3c0a090dc9655e2e0
Petr Karnakov <pkarnakov@gmail.com>
2021-03-14 11:58:47 +0100

    ex205/clustering: fix parameter name bubgen_dt

--- doc/codeocean/code/clustering.sh
+++ doc/codeocean/code/clustering.sh
@@ -8,8 +8,6 @@ set string list_path inline 0 0.03 0 1e5 0.015 1e5
 set int dumppoly 0
 set double dump_field_dt 0.01
 set double tmax 0.1
-set double bubgen_per 0.02
-set double cflst 1
 EOF
 
 make m="32 24 32" bs="8 8 8" np=8 run
