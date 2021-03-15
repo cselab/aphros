ca570ed2b7c3d8aceb5963616e8e882e1930d024
Petr Karnakov <pkarnakov@gmail.com>
2021-03-15 21:39:42 +0100

    circleci: fix flags

--- .circleci/main.bat
+++ .circleci/main.bat
@@ -1,5 +1,5 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-nmake /DCXXFLAGS="/O2 /nologo" /DCFLAGS="/O2 /nologo" /k /f Makefile_nmake
+nmake "CXXFLAGS=/O2 /nologo" "CFLAGS=/O2 /nologo" /k /f Makefile_nmake
 main.exe --logo --exit
 
 
