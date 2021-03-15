56b4947eef5869943764bf6d17f00896f1e72a0a
Sergey Litvinov <slitvinov@gmail.com>
2021-03-15 10:17:19 +0100

    make: add option USE_MPI

---
+++ make/0mpi.mk
@@ -0,0 +1,8 @@
+1_MPI = \
+-D_USE_MPI_=0 \
+-D_USE_HDF_=0 \
+
+CXXFLAGS_HDF =
+LDFLAGS_HDF =
+CXX = g++
+CC = c99
---
+++ make/1mpi.mk
@@ -0,0 +1,8 @@
+1_MPI = \
+-D_USE_MPI_=1 \
+-D_USE_HDF_=1 \
+
+CXXFLAGS_HDF = `pkg-config --cflags hdf5-openmpi`
+LDFLAGS_HDF = `pkg-config --libs hdf5-openmpi`
+CXX = mpic++
+CC = mpicc
