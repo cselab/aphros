5d7974f2351fc2f7c4135bae1d9cd8778901a23b
Sergey Litvinov <slitvinov@gmail.com>
2021-03-22 13:21:25 +0100

    nmake: Makefile_nmake / NMake, no \f in .circleci

--- .circleci/main.bat
+++ .circleci/main.bat
@@ -1,5 +1,5 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-nmake "CXXFLAGS=/O2 /nologo" "CFLAGS=/O2 /nologo" /k /f Makefile_nmake
+nmake "CXXFLAGS=/O2 /nologo" "CFLAGS=/O2 /nologo" /k
 main.exe --logo --exit
 
 
