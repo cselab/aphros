d8843830a1f5f54c9ac72f8ffa3dbff34ae89e01
Sergey Litvinov <slitvinov@gmail.com>
2021-08-03 17:24:41 +0200

    README: rm Vofi

--- README.py
+++ README.py
@@ -49,7 +49,6 @@ Bundled optional dependencies:
 [hypre](https://github.com/hypre-space/hypre),
 [eigen](https://gitlab.com/libeigen/eigen),
 [overlap](https://github.com/severinstrobl/overlap),
-[vofi](https://github.com/VOFTracking/Vofi),
 [fpzip](https://github.com/LLNL/fpzip)
 
 ### Clone and build
