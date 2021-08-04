54647349a2836f578da8f07f4b8251633a39315d
Sergey Litvinov <slitvinov@gmail.com>
2021-08-04 17:17:26 +0200

    deploy: rm deploy/lib; overlap is in git:aphros-dev

--- deploy/lib/overlap/example/c/fail.c
+++
@@ -1,8 +0,0 @@
-#include <overlap.h>
-#include <stdio.h>
-
-int main() {
-  double v;
-  v = overlap_2d(1.5913659557700157e+01, 2.4201182103715837e+01, 2.8272534986026585e+01);
-  printf("%.16e\n", v);
-}
--- deploy/lib/overlap/overlap.h
+++
@@ -1,2 +0,0 @@
-double overlap_3d(double, double, double, double r);
-double overlap_2d(double, double, double r);
