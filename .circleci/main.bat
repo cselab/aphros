98fadff45f4d459e9c73c7cff20815481e7c2e75
Sergey Litvinov <slitvinov@gmail.com>
2021-08-07 00:55:26 +0200

    make: no /f clean on windows

--- .circleci/main.bat
+++ .circleci/main.bat
@@ -1,5 +1,4 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-nmake /f NMakefile clean
 nmake "CXXFLAGS=/O2 /nologo" "CFLAGS=/O2 /nologo" /k /f NMakefile
 main.exe --logo --exit
 
