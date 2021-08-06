59f7158eabbd5c3a1ad9fd177bae4e787a7b8550
Sergey Litvinov <slitvinov@gmail.com>
2021-08-07 01:09:32 +0200

    make: missing .exe

--- .circleci/main.bat
+++ .circleci/main.bat
@@ -2,5 +2,5 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary
 nmake "CXXFLAGS=/O2 /nologo" "CFLAGS=/O2 /nologo" /k /f NMakefile
 main.exe --logo --exit
 
-nmake /f NMakefile test\reconst\main
+nmake /f NMakefile test\reconst\main.exe
 test\reconst\main.exe
