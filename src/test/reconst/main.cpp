6c0c7debf389d952d15bc5f7980080670a5dc538
Sergey Litvinov <slitvinov@gmail.com>
2021-08-06 16:11:40 +0200

    use fassert

--- src/test/reconst/main.cpp
+++ src/test/reconst/main.cpp
@@ -443,10 +443,10 @@ void TestOverlap() {
   Vect h(0.1);
   Vect c(0.);
   Scal r = 1.;
-  std::cout << GetSphereOverlap(Vect(0), h, c, r) << std::endl;
-  std::cout << GetSphereOverlap(Vect(1), h, c, r) << std::endl;
-  std::cout << GetSphereOverlap(Vect(1., 0., 0.), h, c, r) << std::endl;
-  std::cout << GetSphereOverlap(Vect(0.7, 0.7, 0.), h, c, r) << std::endl;
+  fassert(GetSphereOverlap(Vect(0), h, c, r) == 1);
+  fassert(GetSphereOverlap(Vect(1), h, c, r) == 0);
+  fassert(GetSphereOverlap(Vect(1., 0., 0.), h, c, r) == 4.9166179884618788e-01);
+  fassert(GetSphereOverlap(Vect(0.7, 0.7, 0.), h, c, r) == 6.1897116844761280e-01);
 }
 
 void TestArea() {
