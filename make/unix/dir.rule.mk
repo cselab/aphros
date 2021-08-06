727ae84701c3bc452a228a760d184533a0822ba7
Sergey Litvinov <slitvinov@gmail.com>
2021-08-06 21:30:25 +0200

    make: simplify ./bootstrap: rm make/rule

--- make/unix/dir.rule.mk
+++ make/unix/dir.rule.mk
@@ -4,17 +4,14 @@ $(WRK)/color\
 $(WRK)/distr\
 $(WRK)/dump\
 $(WRK)/func\
-$(WRK)/gen\
 $(WRK)/geom\
 $(WRK)/inside\
 $(WRK)/kernel\
 $(WRK)/linear\
 $(WRK)/march\
-$(WRK)/opencl\
 $(WRK)/overlap\
 $(WRK)/parse\
 $(WRK)/solver\
-$(WRK)/test\
 $(WRK)/util\
 $(WRK)/young\
 
