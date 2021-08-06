e670715872ba5abf53f2b98dda3e57898fa3f7ee
Sergey Litvinov <slitvinov@gmail.com>
2021-08-07 01:44:04 +0200

    make: use CXXFLAGS on windows

--- .circleci/main.bat
+++ .circleci/main.bat
@@ -1,5 +1,4 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 nmake "CXXFLAGS=/O2 /nologo" "CFLAGS=/O2 /nologo" /k /f NMakefile
 main.exe --logo --exit
-
-nmake /f NMakefile test\reconst\main.exe test\sysinfo\main.exe
+nmake "CXXFLAGS=/O2 /nologo" "CFLAGS=/O2 /nologo" /k /f NMakefile test\reconst\main.exe test\sysinfo\main.exe
