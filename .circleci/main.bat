9b5b7431d3106010091d1e8a89c1a6e614e87c56
Sergey Litvinov <slitvinov@gmail.com>
2021-08-07 00:19:39 +0200

    make: try test\sysinfo on windows

--- .circleci/main.bat
+++ .circleci/main.bat
@@ -2,4 +2,5 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary
 nmake "CXXFLAGS=/O2 /nologo" "CFLAGS=/O2 /nologo" /k /f NMakefile
 main.exe --logo --exit
 
-
+nmake /f NMakefile test\sysinfo\main.exe
+test\sysinfo\main.exe
