dae412e48eaa2487fa70621bc4e7c394ca18cff4
Sergey Litvinov <slitvinov@gmail.com>
2021-08-08 12:58:41 +0200

    .circleci: fail better on windows

--- .circleci/main.bat
+++ .circleci/main.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-nmake "CXXFLAGS=/O2 /nologo" "CFLAGS=/O2 /nologo" /k /f NMakefile
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat" || goto :eof
+nmake "CXXFLAGS=/O2 /nologo" "CFLAGS=/O2 /nologo" /k /f NMakefile || goto :eof
+nmake "CXXFLAGS=/O2 /nologo" "CFLAGS=/O2 /nologo" /k /f NMakefile test\reconst\main.exe test\sysinfo\main.exe || goto :eof
 main.exe --logo --exit
-nmake "CXXFLAGS=/O2 /nologo" "CFLAGS=/O2 /nologo" /k /f NMakefile test\reconst\main.exe test\sysinfo\main.exe
